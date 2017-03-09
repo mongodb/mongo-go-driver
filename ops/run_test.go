f301dea32997aceebb49f124347c6ab1d42b7aad
Wisdom Omuya <deafgoat@gmail.com>
2017-03-09 11:16:06 -0500

    add Run command helper

---
+++ ops/run_test.go
@@ -0,0 +1,41 @@
+package ops_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/10gen/mongo-go-driver/bson"
+	. "github.com/10gen/mongo-go-driver/ops"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+
+	server := getServer()
+
+	ctx := context.Background()
+	result := bson.M{}
+
+	err := Run(
+		ctx,
+		server,
+		bson.D{{"getnonce", 1}},
+		result,
+	)
+	require.Nil(t, err)
+	require.Equal(t, float64(1), result["ok"])
+	require.NotEqual(t, "", result["nonce"], "MongoDB returned empty nonce")
+
+	result = bson.M{}
+	err = Run(
+		ctx,
+		server,
+		bson.D{{"ping", 1}},
+		result,
+	)
+
+	require.Nil(t, err)
+	require.Equal(t, float64(1), result["ok"], "Unable to ping MongoDB")
+
+}
