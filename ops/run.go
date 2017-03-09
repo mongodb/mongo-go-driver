933e1e5f83a42ec90c2b3aa32360e46e95cfbfc3
Wisdom Omuya <deafgoat@gmail.com>
2017-03-09 11:16:06 -0500

    add Run command helper

---
+++ ops/run.go
@@ -0,0 +1,32 @@
+package ops
+
+import (
+	"context"
+
+	"github.com/10gen/mongo-go-driver/conn"
+	"github.com/10gen/mongo-go-driver/internal"
+	"github.com/10gen/mongo-go-driver/msg"
+)
+
+// Run executes an arbitrary command against the admin database.
+func Run(ctx context.Context, s *SelectedServer, command interface{}, result interface{}) error {
+	request := msg.NewCommand(
+		msg.NextRequestID(),
+		"admin",
+		slaveOk(s.ReadPref),
+		command,
+	)
+
+	c, err := s.Connection(ctx)
+	if err != nil {
+		return internal.WrapError(err, "unable to get a connection to execute run command")
+	}
+	defer c.Close()
+
+	err = conn.ExecuteCommand(ctx, c, request, result)
+	if err != nil {
+		return internal.WrapError(err, "failed to execute run command")
+	}
+
+	return nil
+}
