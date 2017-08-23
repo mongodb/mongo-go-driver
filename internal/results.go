deb51c6072e89a7f02b8d4a6f77efc899e477fe1
Wisdom Omuya <deafgoat@gmail.com>
2017-08-23 12:35:02 -0400

    GODRIVER-71: add support for read preference tags

--- internal/results.go
+++ internal/results.go
@@ -9,28 +9,28 @@ import (
 // IsMasterResult is the result of executing this
 // ismaster command.
 type IsMasterResult struct {
-	Arbiters            []string      `bson:"arbiters,omitempty"`
-	ArbiterOnly         bool          `bson:"arbiterOnly,omitempty"`
-	ElectionID          bson.ObjectId `bson:"electionId,omitempty"`
-	Hidden              bool          `bson:"hidden,omitempty"`
-	Hosts               []string      `bson:"hosts,omitempty"`
-	IsMaster            bool          `bson:"ismaster,omitempty"`
-	IsReplicaSet        bool          `bson:"isreplicaset,omitempty"`
-	LastWriteTimestamp  time.Time     `bson:"lastWriteDate,omitempty"`
-	MaxBSONObjectSize   uint32        `bson:"maxBsonObjectSize,omitempty"`
-	MaxMessageSizeBytes uint32        `bson:"maxMessageSizeBytes,omitempty"`
-	MaxWriteBatchSize   uint16        `bson:"maxWriteBatchSize,omitempty"`
-	Me                  string        `bson:"me,omitempty"`
-	MaxWireVersion      uint8         `bson:"maxWireVersion,omitempty"`
-	MinWireVersion      uint8         `bson:"minWireVersion,omitempty"`
-	Msg                 string        `bson:"msg,omitempty"`
-	OK                  bool          `bson:"ok"`
-	Passives            []string      `bson:"passives,omitempty"`
-	ReadOnly            bool          `bson:"readOnly,omitempty"`
-	Secondary           bool          `bson:"secondary,omitempty"`
-	SetName             string        `bson:"setName,omitempty"`
-	SetVersion          uint32        `bson:"setVersion,omitempty"`
-	Tags                []bson.D      `bson:"tags,omitempty"`
+	Arbiters            []string          `bson:"arbiters,omitempty"`
+	ArbiterOnly         bool              `bson:"arbiterOnly,omitempty"`
+	ElectionID          bson.ObjectId     `bson:"electionId,omitempty"`
+	Hidden              bool              `bson:"hidden,omitempty"`
+	Hosts               []string          `bson:"hosts,omitempty"`
+	IsMaster            bool              `bson:"ismaster,omitempty"`
+	IsReplicaSet        bool              `bson:"isreplicaset,omitempty"`
+	LastWriteTimestamp  time.Time         `bson:"lastWriteDate,omitempty"`
+	MaxBSONObjectSize   uint32            `bson:"maxBsonObjectSize,omitempty"`
+	MaxMessageSizeBytes uint32            `bson:"maxMessageSizeBytes,omitempty"`
+	MaxWriteBatchSize   uint16            `bson:"maxWriteBatchSize,omitempty"`
+	Me                  string            `bson:"me,omitempty"`
+	MaxWireVersion      uint8             `bson:"maxWireVersion,omitempty"`
+	MinWireVersion      uint8             `bson:"minWireVersion,omitempty"`
+	Msg                 string            `bson:"msg,omitempty"`
+	OK                  bool              `bson:"ok"`
+	Passives            []string          `bson:"passives,omitempty"`
+	ReadOnly            bool              `bson:"readOnly,omitempty"`
+	Secondary           bool              `bson:"secondary,omitempty"`
+	SetName             string            `bson:"setName,omitempty"`
+	SetVersion          uint32            `bson:"setVersion,omitempty"`
+	Tags                map[string]string `bson:"tags,omitempty"`
 }
 
 // BuildInfoResult is the result of executing the
