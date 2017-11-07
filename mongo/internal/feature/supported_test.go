a8ee0755eb0fc452546243c1e08c9425f875db33
Saghm Rossi <saghmrossi@gmail.com>
2017-11-07 15:16:51 -0500

    GODRIVER-25 Implement Max Staleness Specification tests

--- mongo/internal/feature/supported_test.go
+++ mongo/internal/feature/supported_test.go
@@ -15,21 +15,29 @@ import (
 )
 
 func TestMaxStaleness(t *testing.T) {
+	wireVersionSupported := model.NewRange(0, 5)
+	wireVersionUnsupported := model.NewRange(0, 4)
+
 	tests := []struct {
 		version  model.Version
+		wire     *model.Range
 		expected bool
 	}{
-		{model.Version{Parts: []uint8{2, 4, 0}}, false},
-		{model.Version{Parts: []uint8{3, 3, 99}}, false},
-		{model.Version{Parts: []uint8{3, 4, 0}}, true},
-		{model.Version{Parts: []uint8{3, 4, 1}}, true},
+		{model.Version{Parts: []uint8{2, 4, 0}}, nil, false},
+		{model.Version{Parts: []uint8{3, 3, 99}}, nil, false},
+		{model.Version{Parts: []uint8{3, 4, 0}}, nil, true},
+		{model.Version{Parts: []uint8{3, 4, 1}}, nil, true},
+		{model.Version{Parts: []uint8{2, 4, 0}}, &wireVersionSupported, false},
+		{model.Version{Parts: []uint8{2, 4, 0}}, &wireVersionUnsupported, false},
+		{model.Version{Parts: []uint8{3, 4, 1}}, &wireVersionSupported, true},
+		{model.Version{Parts: []uint8{3, 4, 1}}, &wireVersionUnsupported, false},
 	}
 
 	for _, test := range tests {
 		t.Run(test.version.String(), func(t *testing.T) {
 			t.Parallel()
 
-			err := MaxStaleness(test.version)
+			err := MaxStaleness(test.version, test.wire)
 			if test.expected {
 				require.NoError(t, err)
 			} else {
