b1e05143b0c4f8a4840f1cae87986d883d43611a
Saghm Rossi <saghmrossi@gmail.com>
2018-02-20 17:10:03 -0500

    GODRIVER-237 Make mongo.Cursor its own interface
    Change-Id: I38df0960072fdcaa20be2d29543475d67be07d2d

--- mongo/cursor.go
+++ mongo/cursor.go
@@ -6,18 +6,51 @@
 
 package mongo
 
-import "github.com/mongodb/mongo-go-driver/mongo/private/ops"
+import (
+	"context"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+)
 
 // Cursor instances iterate a stream of documents. Each document is
 // decoded into the result according to the rules of the bson package.
 //
 // A typical usage of the Cursor interface would be:
 //
-//		cursor := ...    // get a cursor from some operation
-//		ctx := ...       // create a context for the operation
-//		var doc bson.D
-//		for cursor.Next(ctx, &doc) {
-//			...
+//		var cur Cursor
+//		ctx := context.Background()
+//		defer cur.Close(ctx)
+//
+// 		for cur.Next(ctx) {
+//			elem := bson.NewDocument()
+//			if err := cur.Decode(elem); err != nil {
+// 				log.Fatal(err)
+// 			}
+//
+// 			// do something with elem....
 //		}
-//		err := cursor.Close(ctx)
-type Cursor ops.Cursor
+//
+// 		if err := cur.Err(); err != nil {
+//			log.Fatal(err)
+//		}
+//
+type Cursor interface {
+	// NOTE: Whenever ops.Cursor changes, this must be changed to match it.
+
+	// Get the ID of the cursor.
+	ID() int64
+
+	// Get the next result from the cursor.
+	// Returns true if there were no errors and there is a next result.
+	Next(context.Context) bool
+
+	Decode(interface{}) error
+
+	DecodeBytes() (bson.Reader, error)
+
+	// Returns the error status of the cursor
+	Err() error
+
+	// Close the cursor.
+	Close(context.Context) error
+}
