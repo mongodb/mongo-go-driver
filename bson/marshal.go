2b9a818c5f1532c31ce4410f60bdf8a1742729e2
Saghm Rossi <saghmrossi@gmail.com>
2018-02-21 14:34:33 -0500

    GODRIVER-236 Add Marshal, Unmarshal, and UnmarshalDocument functions to BSON library
    Change-Id: I1455e15db07352cf2cc2318e7db14cc72431315c

---
+++ bson/marshal.go
@@ -0,0 +1,41 @@
+// Copyright (C) MongoDB, Inc. 2017-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+package bson
+
+import "bytes"
+
+// Marshal converts a BSON type to bytes.
+//
+// TODO(GODRIVER-257): Document which types are valid for value.
+func Marshal(value interface{}) ([]byte, error) {
+	out := bytes.NewBuffer(make([]byte, 0, 5))
+
+	err := NewEncoder(out).Encode(value)
+	if err != nil {
+		return nil, err
+	}
+
+	return out.Bytes(), nil
+}
+
+// Unmarshal converts bytes into a BSON type.
+//
+// TODO(GODRIVER-257): Document which types are valid for value.
+func Unmarshal(in []byte, out interface{}) error {
+	return NewDecoder(bytes.NewReader(in)).Decode(out)
+}
+
+// UnmarshalDocument converts bytes into a *bson.Document.
+func UnmarshalDocument(bson []byte) (*Document, error) {
+	doc := NewDocument()
+	err := NewDecoder(bytes.NewReader(bson)).Decode(doc)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
+}
