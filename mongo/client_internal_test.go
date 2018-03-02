ec1c87fcd7434daebd4078422e3670a97faa7145
Saghm Rossi <saghmrossi@gmail.com>
2018-03-02 14:20:01 -0500

    GODRIVER-2 Implement X.509 authentication
    Change-Id: Ib87791b4f708e40c454f629874cdb0cfb3fdb0b5

--- mongo/client_internal_test.go
+++ mongo/client_internal_test.go
@@ -9,7 +9,10 @@ package mongo
 import (
 	"context"
 	"os"
+	"path"
 	"testing"
+
+	"fmt"
 
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo/internal/testutil"
@@ -73,4 +76,91 @@ func TestClient_TLSConnection(t *testing.T) {
 	_, found = security.Value().ReaderDocument().Lookup("SSLServerHasCertificateAuthority")
 	require.Nil(t, found)
 
+}
+
+func TestClient_X509Auth(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip()
+	}
+
+	caFile := os.Getenv("MONGO_GO_DRIVER_CA_FILE")
+
+	if len(caFile) == 0 || os.Getenv("AUTH") == "auth" {
+		t.Skip()
+	}
+
+	const user = "C=US,ST=New York,L=New York City,O=MongoDB,OU=other,CN=external"
+
+	c := createTestClient(t)
+	db := c.Database("$external")
+
+	// We don't care if the user doesn't already exist.
+	_, _ = db.RunCommand(
+		context.Background(),
+		bson.NewDocument(
+			bson.EC.String("dropUser", user),
+		),
+	)
+
+	_, err := db.RunCommand(
+		context.Background(),
+		bson.NewDocument(
+			bson.EC.String("createUser", user),
+			bson.EC.ArrayFromElements("roles",
+				bson.VC.DocumentFromElements(
+					bson.EC.String("role", "readWrite"),
+					bson.EC.String("db", "test"),
+				),
+			),
+		),
+	)
+	require.NoError(t, err)
+
+	basePath := path.Join("..", "data", "certificates")
+	baseConnString := testutil.ConnString(t)
+	cs := fmt.Sprintf(
+		"%s&sslClientCertificateKeyFile=%s&authMechanism=MONGODB-X509",
+		baseConnString.String(),
+		path.Join(basePath, "client.pem"),
+	)
+
+	authClient, err := NewClient(cs)
+	require.NoError(t, err)
+
+	db = authClient.Database("test")
+	rdr, err := db.RunCommand(
+		context.Background(),
+		bson.NewDocument(
+			bson.EC.Int32("connectionStatus", 1),
+		),
+	)
+	require.NoError(t, err)
+
+	users, err := rdr.Lookup("authInfo", "authenticatedUsers")
+	require.NoError(t, err)
+
+	array := users.Value().MutableArray()
+
+	for i := uint(0); i < uint(array.Len()); i++ {
+		v, err := array.Lookup(i)
+		require.NoError(t, err)
+
+		rdr := v.ReaderDocument()
+		var u struct {
+			User string
+			DB   string
+		}
+
+		if err := bson.Unmarshal(rdr, &u); err != nil {
+			continue
+		}
+
+		if u.User == user && u.DB == "$external" {
+			return
+		}
+	}
+
+	t.Error("unable to find authenticated user")
 }
