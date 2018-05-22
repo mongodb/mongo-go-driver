01a9a6c938c57e2063dc273dbdee62f911225ffe
Sam Kleinman <sam@10gen.com>
2018-05-22 14:14:28 -0400

    GODRIVER-209: deprecate mongodb-cr
    Change-Id: Iacb18a9aa3a8613ef99131fecdad30065a715f59

--- core/auth/mongodbcr.go
+++ core/auth/mongodbcr.go
@@ -20,6 +20,8 @@ import (
 )
 
 // MONGODBCR is the mechanism name for MONGODB-CR.
+//
+// The MONGODB-CR authentication mechanism is deprecated in MongoDB 4.0.
 const MONGODBCR = "MONGODB-CR"
 
 func newMongoDBCRAuthenticator(cred *Cred) (Authenticator, error) {
@@ -31,6 +33,8 @@ func newMongoDBCRAuthenticator(cred *Cred) (Authenticator, error) {
 }
 
 // MongoDBCRAuthenticator uses the MONGODB-CR algorithm to authenticate a connection.
+//
+// The MONGODB-CR authentication mechanism is deprecated in MongoDB 4.0.
 type MongoDBCRAuthenticator struct {
 	DB       string
 	Username string
@@ -38,6 +42,8 @@ type MongoDBCRAuthenticator struct {
 }
 
 // Auth authenticates the connection.
+//
+// The MONGODB-CR authentication mechanism is deprecated in MongoDB 4.0.
 func (a *MongoDBCRAuthenticator) Auth(ctx context.Context, desc description.Server, rw wiremessage.ReadWriter) error {
 
 	// Arbiters cannot be authenticated
