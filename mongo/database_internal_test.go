e9052e92cec0abceb54347e877d46992b4192733
Divjot Arora <divjot.arora@10gen.com>
2018-05-31 15:01:42 -0400

    Add test cases for ListCollections
    GODRIVER-31
    
    Change-Id: I6087b16dc2429af5f5fc228138e67d45e53632a2

--- mongo/database_internal_test.go
+++ mongo/database_internal_test.go
@@ -10,10 +10,13 @@ import (
 	"context"
 	"testing"
 
+	"fmt"
 	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/bson/objectid"
 	"github.com/mongodb/mongo-go-driver/core/option"
 	"github.com/mongodb/mongo-go-driver/internal/testutil"
 	"github.com/stretchr/testify/require"
+	"os"
 )
 
 func createTestDatabase(t *testing.T, name *string) *Database {
@@ -138,7 +141,7 @@ func TestDatabase_ListCollectionsOptions(t *testing.T) {
 			t.FailNow()
 		}
 
-		//Because we run it with name only parameter we should check that there are no other parameters
+		// Because we run it with name only parameter we should check that there are no other parameters
 		_, err = next.LookupErr("type")
 		require.Error(t, err)
 	}
@@ -146,4 +149,254 @@ func TestDatabase_ListCollectionsOptions(t *testing.T) {
 		err := db.Drop(context.Background())
 		require.NoError(t, err)
 	}()
+}
+
+// creates 1 normal collection and 1 capped collection of size 64*1024
+func setupListCollectionsDb(db *Database) (uncappedName string, cappedName string, err error) {
+	uncappedName, cappedName = "listcoll_uncapped", "listcoll_capped"
+	uncappedColl := db.Collection(uncappedName)
+
+	_, err = db.RunCommand(
+		context.Background(),
+		bson.NewDocument(
+			bson.EC.String("create", cappedName),
+			bson.EC.Boolean("capped", true),
+			bson.EC.Int32("size", 64*1024),
+		),
+	)
+	if err != nil {
+		return "", "", err
+	}
+	cappedColl := db.Collection(cappedName)
+
+	id := objectid.New()
+	want := bson.EC.ObjectID("_id", id)
+	doc := bson.NewDocument(want, bson.EC.Int32("x", 1))
+
+	_, err = uncappedColl.InsertOne(context.Background(), doc)
+	if err != nil {
+		return "", "", err
+	}
+
+	_, err = cappedColl.InsertOne(context.Background(), doc)
+	if err != nil {
+		return "", "", err
+	}
+
+	return uncappedName, cappedName, nil
+}
+
+// verifies both collection names are found in cursor, cursor does not have extra collections, and cursor has no
+// duplicates
+func verifyListCollections(cursor Cursor, uncappedName string, cappedName string, cappedOnly bool) (err error) {
+	next := bson.NewDocument()
+	uncappedFound, cappedFound := false, false
+
+	for cursor.Next(context.Background()) {
+		err = cursor.Decode(next)
+		if err != nil {
+			return err
+		}
+
+		elem, err := next.LookupErr("name")
+		if err != nil {
+			return err
+		}
+
+		if elem.Type() != bson.TypeString {
+			return fmt.Errorf("incorrect type for 'name'. got %v. want %v", elem.Type(), bson.TypeString)
+		}
+
+		elemName := elem.StringValue()
+
+		if elemName != uncappedName && elemName != cappedName {
+			return fmt.Errorf("incorrect collection name. got: %s. wanted: %s or %s", elemName, uncappedName,
+				cappedName)
+		}
+
+		if elemName == uncappedName && !uncappedFound {
+			if cappedOnly {
+				return fmt.Errorf("found uncapped collection %s. expected only capped collections", uncappedName)
+			}
+
+			uncappedFound = true
+			continue
+		}
+
+		if elemName == cappedName && !cappedFound {
+			cappedFound = true
+			continue
+		}
+
+		// duplicate found
+		return fmt.Errorf("found duplicate collection %s", elemName)
+	}
+
+	if !cappedFound {
+		return fmt.Errorf("did not find collection %s", cappedName)
+	}
+
+	if !cappedOnly && !uncappedFound {
+		return fmt.Errorf("did not find collection %s", uncappedName)
+	}
+
+	return nil
+}
+
+func listCollectionsTest(db *Database, cappedOnly bool) error {
+	uncappedName, cappedName, err := setupListCollectionsDb(db)
+	if err != nil {
+		return err
+	}
+
+	var filter *bson.Document
+	if cappedOnly {
+		filter = bson.NewDocument(
+			bson.EC.Boolean("options.capped", true),
+		)
+	}
+
+	cursor, err := db.ListCollections(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+
+	return verifyListCollections(cursor, uncappedName, cappedName, cappedOnly)
+}
+
+func TestDatabase_ListCollections_Standalone_NoFilter(t *testing.T) {
+	t.Parallel()
+	dbName := "db_list_collections_nofilter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, false)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Standalone_Filter(t *testing.T) {
+	t.Parallel()
+	dbName := "db_list_collections_filter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, true)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Primary_NoFilter(t *testing.T) {
+	// TODO(GODRIVER-272): become primary using read preference
+	if os.Getenv("TOPOLOGY") != "replica_set" {
+		t.Skip()
+	}
+
+	t.Parallel()
+	dbName := "db_list_collections_primary_nofilter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, false)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Primary_Filter(t *testing.T) {
+	if os.Getenv("TOPOLOGY") != "replica_set" {
+		t.Skip()
+	}
+
+	t.Parallel()
+	dbName := "db_list_collections_primary_filter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, true)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Secondary_NoFilter(t *testing.T) {
+	if os.Getenv("TOPOLOGY") != "replica_set" {
+		t.Skip()
+	}
+
+	t.Parallel()
+	dbName := "db_list_collections_secondary_nofilter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, false)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Secondary_Filter(t *testing.T) {
+	if os.Getenv("TOPOLOGY") != "replica_set" {
+		t.Skip()
+	}
+
+	t.Parallel()
+	dbName := "db_list_collections_secondary_filter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, true)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Sharded_Nofilter(t *testing.T) {
+	if os.Getenv("TOPOLOGY") != "sharded_cluster" {
+		t.Skip()
+	}
+
+	t.Parallel()
+	dbName := "db_list_collections_sharded_nofilter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, true)
+	require.NoError(t, err)
+}
+
+func TestDatabase_ListCollections_Sharded_Filter(t *testing.T) {
+	if os.Getenv("TOPOLOGY") != "sharded_cluster" {
+		t.Skip()
+	}
+
+	t.Parallel()
+	dbName := "db_list_collections_sharded_filter"
+	db := createTestDatabase(t, &dbName)
+
+	defer func() {
+		err := db.Drop(context.Background())
+		require.NoError(t, err)
+	}()
+
+	err := listCollectionsTest(db, true)
+	require.NoError(t, err)
 }
