7dee59ac7855a11b42b6d8cd117a00ed040ddd0c
Sam Kleinman <sam@10gen.com>
2018-06-08 13:25:10 -0400

    GODRIVER-115: driver benchmarks and bson benchmarks
    Change-Id: I8d8f0ab8c9ca37db39c31855489b792eb255ab4d

---
+++ benchmark/bson.go
@@ -0,0 +1,49 @@
+package benchmark
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/pkg/errors"
+)
+
+const (
+	perfDataDir  = "perf"
+	bsonDataDir  = "extended_bson"
+	flatBSONData = "flat_bson.json"
+	deepBSONData = "deep_bson.json"
+	fullBSONData = "full_bson.json"
+)
+
+// utility functions for the bson benchmarks
+
+func loadSourceDocument(pathParts ...string) (*bson.Document, error) {
+	data, err := ioutil.ReadFile(filepath.Join(pathParts...))
+	if err != nil {
+		return nil, err
+	}
+	doc, err := bson.ParseExtJSONObject(string(data))
+	if err != nil {
+		return nil, err
+	}
+
+	if doc.Len() == 0 {
+		return nil, errors.New("empty bson document")
+	}
+
+	return doc, nil
+}
+
+func loadSourceReader(pathParts ...string) (bson.Reader, error) {
+	doc, err := loadSourceDocument(pathParts...)
+	if err != nil {
+		return nil, err
+	}
+	raw, err := doc.MarshalBSON()
+	if err != nil {
+		return nil, err
+	}
+
+	return bson.Reader(raw), nil
+}
