d0d57be028899d94b15f26bfc67e435df01affca
Sam Kleinman <sam@10gen.com>
2018-06-08 13:25:10 -0400

    GODRIVER-115: driver benchmarks and bson benchmarks
    Change-Id: I8d8f0ab8c9ca37db39c31855489b792eb255ab4d

---
+++ benchmark/bson_reader.go
@@ -0,0 +1,66 @@
+package benchmark
+
+import (
+	"context"
+	"errors"
+)
+
+func BSONFlatReaderDecoding(ctx context.Context, tm TimerManager, iters int) error {
+	doc, err := loadSourceReader(getProjectRoot(), perfDataDir, bsonDataDir, flatBSONData)
+	if err != nil {
+		return err
+	}
+
+	tm.ResetTimer()
+
+	for i := 0; i < iters; i++ {
+		keys, err := doc.Keys(true)
+		if err != nil {
+			return err
+		}
+		if len(keys) != 145 {
+			return errors.New("reader parsing error")
+		}
+	}
+	return nil
+}
+
+func BSONDeepReaderDecoding(ctx context.Context, tm TimerManager, iters int) error {
+	doc, err := loadSourceReader(getProjectRoot(), perfDataDir, bsonDataDir, deepBSONData)
+	if err != nil {
+		return err
+	}
+
+	tm.ResetTimer()
+
+	for i := 0; i < iters; i++ {
+		keys, err := doc.Keys(true)
+		if err != nil {
+			return err
+		}
+		if len(keys) != 126 {
+			return errors.New("incomplete bson")
+		}
+	}
+	return nil
+}
+
+func BSONFullReaderDecoding(ctx context.Context, tm TimerManager, iters int) error {
+	doc, err := loadSourceReader(getProjectRoot(), perfDataDir, bsonDataDir, fullBSONData)
+	if err != nil {
+		return err
+	}
+
+	tm.ResetTimer()
+
+	for i := 0; i < iters; i++ {
+		keys, err := doc.Keys(true)
+		if err != nil {
+			return err
+		}
+		if len(keys) != 145 {
+			return errors.New("incomplete bson")
+		}
+	}
+	return nil
+}
---
+++ benchmark/harness_case.go
@@ -0,0 +1,121 @@
+package benchmark
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
+)
+
+type CaseDefinition struct {
+	Bench   BenchCase
+	Count   int
+	Size    int
+	Runtime time.Duration
+
+	startAt time.Time
+}
+
+// TimerManager is a subset of the testing.B tool, used to manage
+// setup code.
+type TimerManager interface {
+	ResetTimer()
+	StartTimer()
+	StopTimer()
+}
+
+func (c *CaseDefinition) ResetTimer() {
+	c.startAt = time.Now()
+	c.Runtime = 0
+}
+
+func (c *CaseDefinition) StartTimer() {
+	c.startAt = time.Now()
+}
+
+func (c *CaseDefinition) StopTimer() {
+	if c.startAt.IsZero() {
+		return
+	}
+
+	c.startAt = time.Time{}
+	c.Runtime += time.Since(c.startAt)
+}
+
+func (c *CaseDefinition) Run(ctx context.Context) *BenchResult {
+	out := &BenchResult{
+		Trials:     1,
+		DataSize:   c.Size,
+		Name:       c.Name(),
+		Operations: c.Count,
+	}
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(ctx, ExecutionTimeout)
+	defer cancel()
+
+	fmt.Println("=== RUN", out.Name)
+	c.startAt = time.Now()
+	for {
+		if time.Since(c.startAt) > c.Runtime {
+			if out.Trials >= MinIterations {
+				break
+			} else if ctx.Err() != nil {
+				break
+			}
+		}
+
+		res := Result{
+			Iterations: c.Count,
+		}
+		c.StartTimer()
+		res.Error = c.Bench(ctx, c, c.Count)
+		c.StopTimer()
+		res.Duration = c.Runtime
+
+		if res.Error == context.Canceled {
+			break
+		}
+
+		out.Trials++
+		out.Raw = append(out.Raw, res)
+	}
+
+	out.Duration = out.totalDuration()
+	if out.HasErrors() {
+		fmt.Printf("--- FAIL: %s (%s)\n", out.Name, out.Duration.Round(time.Millisecond))
+	} else {
+		fmt.Printf("--- PASS: %s (%s)\n", out.Name, out.Duration.Round(time.Millisecond))
+	}
+
+	return out
+
+}
+
+func (c *CaseDefinition) String() string {
+	return fmt.Sprintf("name=%s, count=%d, runtime=%s timeout=%s",
+		c.Name(), c.Count, c.Runtime, ExecutionTimeout)
+}
+
+func (c *CaseDefinition) Name() string { return getName(c.Bench) }
+
+func getName(i interface{}) string {
+	n := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+	parts := strings.Split(n, ".")
+	if len(parts) > 1 {
+		return parts[len(parts)-1]
+	}
+
+	return n
+
+}
+
+func getProjectRoot() string { return filepath.Dir(getDirectoryOfFile()) }
+
+func getDirectoryOfFile() string {
+	_, file, _, _ := runtime.Caller(1)
+
+	return filepath.Dir(file)
+}
