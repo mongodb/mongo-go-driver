6ef364fc467daaa8ddee45dc4bc4b420e2f36e3f
Roland Fong <rfblue2@gmail.com>
2018-06-11 09:59:55 -0400

    Throw an error if IndexModel Keys is nil
    GODRIVER-456
    
    Change-Id: I89843f6fcbfd23bffa46cef6c7abe3949ccc1c4c

--- mongo/index_view_internal_test.go
+++ mongo/index_view_internal_test.go
@@ -245,6 +245,25 @@ func TestIndexView_CreateOneWithCollationOption(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestIndexView_CreateOneWithNilKeys(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip()
+	}
+
+	_, coll := getIndexableCollection(t)
+	indexView := coll.Indexes()
+
+	_, err := indexView.CreateOne(
+		context.Background(),
+		IndexModel{
+			Keys: nil,
+		},
+	)
+	require.Error(t, err)
+}
+
 func TestIndexView_CreateMany(t *testing.T) {
 	t.Parallel()
 
