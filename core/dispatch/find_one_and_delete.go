a0264daa54d5eb6be186af5735fcf52ad4b59306
Divjot Arora <divjot.arora@10gen.com>
2018-06-15 14:52:04 -0400

    Add mongo/findopt
    GODRIVER-272
    
    Change-Id: I6acc5c25b3ffeb1accfe8d14e7125c46d35792c5

--- core/dispatch/find_one_and_delete.go
+++ core/dispatch/find_one_and_delete.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/mongodb/mongo-go-driver/core/command"
 	"github.com/mongodb/mongo-go-driver/core/description"
-	"github.com/mongodb/mongo-go-driver/core/option"
 	"github.com/mongodb/mongo-go-driver/core/result"
 	"github.com/mongodb/mongo-go-driver/core/topology"
 	"github.com/mongodb/mongo-go-driver/core/writeconcern"
@@ -40,19 +39,7 @@ func FindOneAndDelete(
 		cmd.Opts = append(cmd.Opts, opt)
 	}
 
-	// NOTE: We iterate through the options because the user may have provided
-	// an option explicitly and that needs to override the provided write concern.
-	// We put this here because it would complicate the methods that call this to
-	// parse out the option.
-	acknowledged := true
-	for _, opt := range cmd.Opts {
-		wc, ok := opt.(option.OptWriteConcern)
-		if !ok {
-			continue
-		}
-		acknowledged = wc.Acknowledged
-		break
-	}
+	acknowledged := wc.Acknowledged()
 
 	desc := ss.Description()
 	conn, err := ss.Connection(ctx)
