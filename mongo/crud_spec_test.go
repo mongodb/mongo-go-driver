5cae0795564acbec40e8b2fa62eec61481906470
Divjot Arora <divjot.arora@10gen.com>
2018-06-19 09:44:35 -0400

    Add mongo/distinctopt
    GODRIVER-272
    
    Change-Id: Ic909c0e9930e822699540ca14d1bd5ba0b6b5bc7

--- mongo/crud_spec_test.go
+++ mongo/crud_spec_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/mongodb/mongo-go-driver/core/option"
 	"github.com/mongodb/mongo-go-driver/internal/testutil/helpers"
 	"github.com/mongodb/mongo-go-driver/mongo/aggregateopt"
+	"github.com/mongodb/mongo-go-driver/mongo/distinctopt"
 	"github.com/mongodb/mongo-go-driver/mongo/findopt"
 	"github.com/stretchr/testify/require"
 )
@@ -318,10 +319,11 @@ func distinctTest(t *testing.T, coll *Collection, test *testCase) {
 			filter = filterArg.(map[string]interface{})
 		}
 
-		var opts []option.DistinctOptioner
+		var opts []distinctopt.Distinct
 
 		if collation, found := test.Operation.Arguments["collation"]; found {
-			opts = append(opts, Opt.Collation(collationFromMap(collation.(map[string]interface{}))))
+			mapCollation := collationFromMap(collation.(map[string]interface{}))
+			opts = append(opts, distinctopt.Collation(mapCollation))
 		}
 
 		actual, err := coll.Distinct(context.Background(), fieldName, filter, opts...)
