a63103701b49c980e62def7e13cb7674d9348f16
Divjot Arora <divjot.arora@10gen.com>
2018-06-22 16:37:09 -0400

    Add mongo/countopt
    GODRIVER-272
    
    Change-Id: Iee3633f16e540fd0a97260ada6575dc47c0049b4

--- mongo/collection.go
+++ mongo/collection.go
@@ -20,6 +20,7 @@ import (
 	"github.com/mongodb/mongo-go-driver/core/readpref"
 	"github.com/mongodb/mongo-go-driver/core/writeconcern"
 	"github.com/mongodb/mongo-go-driver/mongo/aggregateopt"
+	"github.com/mongodb/mongo-go-driver/mongo/countopt"
 	"github.com/mongodb/mongo-go-driver/mongo/findopt"
 )
 
@@ -455,7 +456,7 @@ func (coll *Collection) Aggregate(ctx context.Context, pipeline interface{},
 // *bson.Document. See TransformDocument for the list of valid types for
 // filter.
 func (coll *Collection) Count(ctx context.Context, filter interface{},
-	opts ...option.CountOptioner) (int64, error) {
+	opts ...countopt.Count) (int64, error) {
 
 	if ctx == nil {
 		ctx = context.Background()
@@ -466,11 +467,16 @@ func (coll *Collection) Count(ctx context.Context, filter interface{},
 		return 0, err
 	}
 
+	countOpts, err := countopt.BundleCount(opts...).Unbundle(true)
+	if err != nil {
+		return 0, err
+	}
+
 	oldns := coll.namespace()
 	cmd := command.Count{
 		NS:       command.Namespace{DB: oldns.DB, Collection: oldns.Collection},
 		Query:    f,
-		Opts:     opts,
+		Opts:     countOpts,
 		ReadPref: coll.readPreference,
 	}
 	return dispatch.Count(ctx, cmd, coll.client.topology, coll.readSelector, coll.readConcern)
