30fc98a364fdabbd3043e0e9e909dc2b3c3e4786
Divjot Arora <divjot.arora@10gen.com>
2018-06-29 14:20:15 -0400

    Add ClientSession, ServerSession, and Pool structs
    GODRIVER-52
    
    Change-Id: Ib61ee298d151408d766e1ba9a79dc3ddc32f0c75

---
+++ core/session/session.go
@@ -0,0 +1,37 @@
+package session
+
+import (
+	"time"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+)
+
+// ServerSession is an open session with the server.
+type ServerSession struct {
+	SessionID *bson.Document
+	LastUsed  time.Time
+}
+
+// ClientSession is a session for clients to run commands.
+type ClientSession struct {
+	ClusterTime   *bson.Document
+	SessionID     *bson.Document
+	serverSession *ServerSession
+}
+
+// NewClientSession creates a ClientSession.
+func NewClientSession(servSess *ServerSession) *ClientSession {
+	return &ClientSession{
+		serverSession: servSess,
+	}
+}
+
+// AdvanceClusterTime updates the session's cluster time.
+func (cs *ClientSession) AdvanceClusterTime(clusterTime *bson.Document) {
+
+}
+
+// EndSession closes the session.
+func (cs *ClientSession) EndSession() {
+
+}
