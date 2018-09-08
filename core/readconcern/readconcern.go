4b810a7f25fd7a16377b744714290e4a104a87e2
Igor Kryvenko <kryvenko7igor@gmail.com>
2018-09-08 16:35:49 +0300

    GODRIVER-523: Support new readConcern level "available"

--- core/readconcern/readconcern.go
+++ core/readconcern/readconcern.go
@@ -40,6 +40,12 @@ func Linearizable() *ReadConcern {
 	return New(Level("linearizable"))
 }
 
+// Available specifies that the query should return data from the instance with no guarantee
+// that the data has been written to a majority of the replica set members (i.e. may be rolled back).
+func Available() *ReadConcern {
+	return New(Level("available"))
+}
+
 // New constructs a new read concern from the given string.
 func New(options ...Option) *ReadConcern {
 	concern := &ReadConcern{}
