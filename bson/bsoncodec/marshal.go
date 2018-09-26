266cb82a3c6233d71b7d15cdadf9f7abd4411447
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2018-09-26 09:13:24 -0400

    Implement new extended JSON parser
    GODRIVER-514
    
    Change-Id: I59e18824178c36ab37af5c3cc97f0575c0c2f709

--- bson/bsoncodec/marshal.go
+++ bson/bsoncodec/marshal.go
@@ -6,6 +6,8 @@ import (
 	"github.com/mongodb/mongo-go-driver/bson"
 )
 
+const defaultDstCap = 256
+
 // Marshaler is an interface implemented by types that can marshal themselves
 // into a BSON document represented as bytes. The bytes returned must be a valid
 // BSON document if the error is nil.
@@ -38,7 +40,7 @@ func MarshalAppend(dst []byte, val interface{}) ([]byte, error) {
 
 // MarshalWithRegistry returns the BSON encoding of val using Registry r.
 func MarshalWithRegistry(r *Registry, val interface{}) ([]byte, error) {
-	dst := make([]byte, 0, 256) // TODO: make the default cap a constant
+	dst := make([]byte, 0, defaultDstCap)
 	return MarshalAppendWithRegistry(r, dst, val)
 }
 
@@ -46,8 +48,6 @@ func MarshalWithRegistry(r *Registry, val interface{}) ([]byte, error) {
 // Registry r. If dst is not large enough to hold the BSON encoding of val, dst
 // will be grown.
 func MarshalAppendWithRegistry(r *Registry, dst []byte, val interface{}) ([]byte, error) {
-	// w := writer(dst)
-	// vw := newValueWriter(&w)
 	vw := vwPool.Get().(*valueWriter)
 	defer vwPool.Put(vw)
 
@@ -71,6 +71,53 @@ func MarshalAppendWithRegistry(r *Registry, dst []byte, val interface{}) ([]byte
 	}
 
 	return vw.buf, nil
+}
+
+// MarshalExtJSON returns the extended JSON encoding of val.
+func MarshalExtJSON(val interface{}, canonical, escapeHTML bool) ([]byte, error) {
+	return MarshalExtJSONWithRegistry(defaultRegistry, val, canonical, escapeHTML)
+}
+
+// MarshalExtJSONAppend will append the extended JSON encoding of val to dst.
+// If dst is not large enough to hold the extended JSON encoding of val, dst
+// will be grown.
+func MarshalExtJSONAppend(dst []byte, val interface{}, canonical, escapeHTML bool) ([]byte, error) {
+	return MarshalExtJSONAppendWithRegistry(defaultRegistry, dst, val, canonical, escapeHTML)
+}
+
+// MarshalExtJSONWithRegistry returns the extended JSON encoding of val using Registry r.
+func MarshalExtJSONWithRegistry(r *Registry, val interface{}, canonical, escapeHTML bool) ([]byte, error) {
+	dst := make([]byte, 0, defaultDstCap)
+	return MarshalExtJSONAppendWithRegistry(r, dst, val, canonical, escapeHTML)
+}
+
+// MarshalExtJSONAppendWithRegistry will append the extended JSON encoding of
+// val to dst using Registry r. If dst is not large enough to hold the BSON
+// encoding of val, dst will be grown.
+func MarshalExtJSONAppendWithRegistry(r *Registry, dst []byte, val interface{}, canonical, escapeHTML bool) ([]byte, error) {
+	ejvw := ejvwPool.Get().(*extJSONValueWriter)
+	defer ejvwPool.Put(ejvw)
+
+	ejvw.reset(dst, canonical, escapeHTML)
+
+	enc := encPool.Get().(*Encoder)
+	defer encPool.Put(enc)
+
+	err := enc.Reset(ejvw)
+	if err != nil {
+		return nil, err
+	}
+	err = enc.SetRegistry(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = enc.Encode(val)
+	if err != nil {
+		return nil, err
+	}
+
+	return ejvw.buf, nil
 }
 
 func marshalElement(r *Registry, dst []byte, key string, val interface{}) ([]byte, error) {
