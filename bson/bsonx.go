3a438ec2f915243e06eb7af9cc304ee4dabccafc
Kris Brandow <kris@mongodb.com>
2018-11-13 17:53:17 -0500

    Move the Doc type family outside of stable
    Change-Id: I6ec8f06f91db1d8450d20a8cfe1d08c77aa019f1

---
+++ bson/bsonx.go
@@ -0,0 +1,65 @@
+package bson
+
+import "github.com/mongodb/mongo-go-driver/x/bson"
+
+var ErrNilDocument = bsonx.ErrNilDocument
+
+type KeyNotFound = bsonx.KeyNotFound
+type Doc = bsonx.Doc
+type MDoc = bsonx.MDoc
+type Arr = bsonx.Arr
+
+var ReadDoc = bsonx.ReadDoc
+var ReadMDoc = bsonx.ReadMDoc
+
+type Val = bsonx.Val
+
+type ElementTypeError = bsonx.ElementTypeError
+
+type Elem = bsonx.Elem
+
+type IDoc = bsonx.IDoc
+
+var Double = bsonx.Double
+
+var String = bsonx.String
+
+var Document = bsonx.Document
+
+var Array = bsonx.Array
+
+var Binary = bsonx.Binary
+
+var Undefined = bsonx.Undefined
+
+var ObjectID = bsonx.ObjectID
+
+var Boolean = bsonx.Boolean
+
+var DateTime = bsonx.DateTime
+
+var Time = bsonx.Time
+
+var Null = bsonx.Null
+
+var Regex = bsonx.Regex
+
+var DBPointer = bsonx.DBPointer
+
+var JavaScript = bsonx.JavaScript
+
+var Symbol = bsonx.Symbol
+
+var CodeWithScope = bsonx.CodeWithScope
+
+var Int32 = bsonx.Int32
+
+var Timestamp = bsonx.Timestamp
+
+var Int64 = bsonx.Int64
+
+var Decimal128 = bsonx.Decimal128
+
+var MinKey = bsonx.MinKey
+
+var MaxKey = bsonx.MaxKey
