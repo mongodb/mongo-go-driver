376f1f4150aa5df594f6d391fc6f77c9e84e4bd7
Kris Brandow <kris@mongodb.com>
2018-11-26 15:00:30 -0500

    Update documentation for mongo package doc.go file
    Change-Id: Ia4ff155ed93f6e2f3418c7dd06b6532ce2637984

---
+++ mongo/database_test.go
@@ -0,0 +1,19 @@
+package mongo
+
+import (
+	"context"
+	"time"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+)
+
+// Individual commands can be sent to the server and response retrieved via run command.
+func ExampleDatabase_RunCommand(db *Database) (bson.Raw, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	result, err := db.RunCommand(ctx, bson.D{{"ping", 1}})
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
