aab8cf48fb5908381ddf0675df4431bad3199f93
Kris Brandow <kris@mongodb.com>
2018-11-30 01:30:17 -0500

    Fix list_test_pkgs.sh to work properly on Windows
    Change-Id: I3799d93dcb5db28a693477e63ffbf0573aeab958

--- etc/list_test_pkgs.sh
+++ etc/list_test_pkgs.sh
@@ -4,4 +4,9 @@ directory="$1"
 if [ -z "$directory" ]; then
     directory="."
 fi
-go list -test -f '{{.ForTest}}' $directory/... | sed -e "s/^github.com\/mongodb\/mongo-go-driver/./"
+
+if [ "Windows_NT" = "$OS" ]; then
+    find "$directory" -iname "*_test.go" | perl -ple 's{(.*)/[^/]+}{$1}' | sort -u
+else
+    go list -test -f '{{.ForTest}}' $directory/... | sed -e "s/^github.com\/mongodb\/mongo-go-driver/./"
+fi
