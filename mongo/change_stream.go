8c4d8df3cb16b75221f1ab12f782e895feea3fbb
Divjot Arora <divjot.arora@10gen.com>
2019-01-07 14:58:21 -0500

    Call Next() recursively after resume-able change stream errors.
    GODRIVER-746
    
    Change-Id: I4b62daffa4443a3af1775f51bdb63487dd9f571a

--- mongo/change_stream.go
+++ mongo/change_stream.go
@@ -205,42 +205,13 @@ func (cs *changeStream) runCommand(ctx context.Context, replaceOptions bool) err
 	}
 	cs.cursor = cursor
 
-	// can get resume token from initial aggregate command if non-empty batch
-	// operationTime from aggregate saved in the session
 	cursorValue, err := rdr.LookupErr("cursor")
 	if err != nil {
 		return err
 	}
 	cursorDoc := cursorValue.Document()
-
 	cs.ns = command.ParseNamespace(cursorDoc.Lookup("ns").StringValue())
 
-	batchVal := cursorDoc.Lookup("firstBatch")
-	if err != nil {
-		return err
-	}
-
-	batch := batchVal.Array()
-	elements, err := batch.Elements()
-	if err != nil {
-		return err
-	}
-
-	if len(elements) == 0 {
-		return nil // no resume token
-	}
-
-	firstElem, err := batch.IndexErr(0)
-	if err != nil {
-		return err
-	}
-
-	tokenDoc, err := bsonx.ReadDoc(firstElem.Value().Document().Lookup("_id").Document())
-	if err != nil {
-		return err
-	}
-
-	cs.resumeToken = tokenDoc
 	return nil
 }
 
@@ -398,6 +369,34 @@ func newClientChangeStream(ctx context.Context, client *Client, pipeline interfa
 	return cs, nil
 }
 
+func (cs *changeStream) storeResumeToken() error {
+	br, err := cs.cursor.DecodeBytes()
+	if err != nil {
+		return err
+	}
+
+	idVal, err := br.LookupErr("_id")
+	if err != nil {
+		_ = cs.Close(context.Background())
+		return ErrMissingResumeToken
+	}
+
+	var idDoc bson.Raw
+	idDoc, ok := idVal.DocumentOK()
+	if !ok {
+		_ = cs.Close(context.Background())
+		return ErrMissingResumeToken
+	}
+	tokenDoc, err := bsonx.ReadDoc(idDoc)
+	if err != nil {
+		_ = cs.Close(context.Background())
+		return ErrMissingResumeToken
+	}
+
+	cs.resumeToken = tokenDoc
+	return nil
+}
+
 func (cs *changeStream) ID() int64 {
 	if cs.cursor == nil {
 		return 0
@@ -407,38 +406,44 @@ func (cs *changeStream) ID() int64 {
 }
 
 func (cs *changeStream) Next(ctx context.Context) bool {
-	if cs.cursor == nil {
-		return false
-	}
+	for {
+		if cs.cursor == nil {
+			return false
+		}
 
-	if cs.cursor.Next(ctx) {
-		return true
-	}
+		if cs.cursor.Next(ctx) {
+			err := cs.storeResumeToken()
+			if err != nil {
+				cs.err = err
+				return false
+			}
 
-	err := cs.cursor.Err()
-	if err == nil {
-		return false
-	}
+			return true
+		}
 
-	switch t := err.(type) {
-	case command.Error:
-		if t.Code == errorInterrupted || t.Code == errorCappedPositionLost || t.Code == errorCursorKilled {
+		err := cs.cursor.Err()
+		if err == nil {
+			return false
+		}
+
+		switch t := err.(type) {
+		case command.Error:
+			if t.Code == errorInterrupted || t.Code == errorCappedPositionLost || t.Code == errorCursorKilled {
+				return false
+			}
+		}
+
+		killCursors := command.KillCursors{
+			NS:  cs.ns,
+			IDs: []int64{cs.ID()},
+		}
+
+		_, _ = driver.KillCursors(ctx, killCursors, cs.client.topology, cs.db.writeSelector)
+		cs.err = cs.runCommand(ctx, true)
+		if cs.err != nil {
 			return false
 		}
 	}
-
-	killCursors := command.KillCursors{
-		NS:  cs.ns,
-		IDs: []int64{cs.ID()},
-	}
-
-	_, _ = driver.KillCursors(ctx, killCursors, cs.client.topology, cs.db.writeSelector)
-	cs.err = cs.runCommand(ctx, true)
-	if cs.err != nil {
-		return false
-	}
-
-	return true
 }
 
 func (cs *changeStream) Decode(out interface{}) error {
@@ -458,32 +463,11 @@ func (cs *changeStream) DecodeBytes() (bson.Raw, error) {
 	if cs.cursor == nil {
 		return nil, ErrNilCursor
 	}
-
-	br, err := cs.cursor.DecodeBytes()
-	if err != nil {
-		return nil, err
+	if cs.err != nil {
+		return nil, cs.err
 	}
 
-	idVal, err := br.LookupErr("_id")
-	if err != nil {
-		_ = cs.Close(context.Background())
-		return nil, ErrMissingResumeToken
-	}
-
-	var idDoc bson.Raw
-	idDoc, ok := idVal.DocumentOK()
-	if !ok {
-		_ = cs.Close(context.Background())
-		return nil, ErrMissingResumeToken
-	}
-	tokenDoc, err := bsonx.ReadDoc(idDoc)
-	if err != nil {
-		_ = cs.Close(context.Background())
-		return nil, ErrMissingResumeToken
-	}
-
-	cs.resumeToken = tokenDoc
-	return br, nil
+	return cs.cursor.DecodeBytes()
 }
 
 func (cs *changeStream) Err() error {
