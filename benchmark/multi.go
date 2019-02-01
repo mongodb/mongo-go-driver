10b524505649cf4d6d2d3824a3bbf8eb537eeda8
Kris Brandow <kris@mongodb.com>
2019-02-01 12:20:25 -0500

    Fix performance benchmarks
    Change-Id: I71f18923ee5d952d481a3910d605b48559c90e63

--- benchmark/multi.go
+++ benchmark/multi.go
@@ -10,7 +10,6 @@ import (
 	"context"
 	"errors"
 
-	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/x/bsonx"
 )
 
@@ -59,9 +58,7 @@ func MultiFindMany(ctx context.Context, tm TimerManager, iters int) error {
 		if err != nil {
 			return err
 		}
-		var r bson.Raw
-		r = cursor.DecodeBytes()
-		if len(r) == 0 {
+		if len(cursor.Current) == 0 {
 			return errors.New("error retrieving document")
 		}
 
