562abe8baccd12c6a7364470228c8fb4f428e199
Kris Brandow <kris@mongodb.com>
2019-02-12 18:01:24 -0500

    Add transactions examples for docs
    GODRIVER-422
    GODRIVER-828
    GODRIVER-839
    
    Change-Id: I5927c58c4ecf89ab8124c87066b394ca6a6b8dc8

--- mongo/session.go
+++ mongo/session.go
@@ -105,7 +105,7 @@ func (s *sessionImpl) AbortTransaction(ctx context.Context) error {
 	_, err = driver.AbortTransaction(ctx, cmd, s.topo, description.WriteSelector())
 
 	_ = s.Client.AbortTransaction()
-	return err
+	return replaceErrors(err)
 }
 
 // CommitTransaction commits the sesson's transaction.
@@ -140,7 +140,7 @@ func (s *sessionImpl) CommitTransaction(ctx context.Context) error {
 	if err == nil {
 		return s.Client.CommitTransaction()
 	}
-	return err
+	return replaceErrors(err)
 }
 
 func (s *sessionImpl) ClusterTime() bson.Raw {
