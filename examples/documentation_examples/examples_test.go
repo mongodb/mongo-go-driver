3ac9c5dd4f46728392bc3ba511f661d176b59e5f
Isabella Siu <isabella.siu@10gen.com>
2019-02-14 17:21:19 -0500

    GODRIVER-194 add ChangeStream examples for docs
    Change-Id: Ifd84577c877ef2738f555eb8eadc4cb192fda42c

--- examples/documentation_examples/examples_test.go
+++ examples/documentation_examples/examples_test.go
@@ -52,13 +52,28 @@ func TestTransactionExamples(t *testing.T) {
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(cs.String()))
 	require.NoError(t, err)
 	defer client.Disconnect(ctx)
-
 	ver, err := getServerVersion(ctx, client)
 	if err != nil || testutil.CompareVersions(t, ver, "4.0") < 0 || os.Getenv("TOPOLOGY") != "replica_set" {
 		t.Skip("server does not support transactions")
 	}
 	err = documentation_examples.TransactionsExamples(ctx, client)
 	require.NoError(t, err)
+}
+
+func TestChangeStreamExamples(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	cs := testutil.ConnString(t)
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(cs.String()))
+	require.NoError(t, err)
+	defer client.Disconnect(ctx)
+
+	db := client.Database("changestream_examples")
+	ver, err := getServerVersion(ctx, client)
+	if err != nil || testutil.CompareVersions(t, ver, "3.6") < 0 || os.Getenv("TOPOLOGY") != "replica_set" {
+		t.Skip("server does not support changestreams")
+	}
+	documentation_examples.ChangeStreamExamples(t, db)
 }
 
 func getServerVersion(ctx context.Context, client *mongo.Client) (string, error) {
