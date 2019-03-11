1e7f5521af8d500c6aebd793f32e9b09d6f72e62
Isabella Siu <isabella.siu@10gen.com>
2019-03-11 10:50:09 -0400

    add recovery token and unpinning to sharded transactions
    GODRIVER-637
    GODRIVER-690
    
    Change-Id: I86d7a96c872700b328a662390dfe9acccb13b00d

--- x/mongo/driver/session/client_session.go
+++ x/mongo/driver/session/client_session.go
@@ -87,7 +87,8 @@ type Client struct {
 
 	pool           *Pool
 	state          state
-	PinnedSelector description.ServerSelector
+	PinnedSelector *description.Server
+	RecoveryToken  bson.Raw
 }
 
 func getClusterTime(clusterTime bson.Raw) (uint32, uint32) {
@@ -198,6 +199,27 @@ func (c *Client) UpdateUseTime() error {
 	return nil
 }
 
+// UpdateRecoveryToken updates the session's recovery token from the server response.
+func (c *Client) UpdateRecoveryToken(response bson.Raw) {
+	if c == nil {
+		return
+	}
+
+	token, err := response.LookupErr("recoveryToken")
+	if err != nil {
+		return
+	}
+
+	c.RecoveryToken = token.Document()
+}
+
+// ClearPinnedSelector sets the PinnedSelector to nil.
+func (c *Client) ClearPinnedSelector() {
+	if c != nil {
+		c.PinnedSelector = nil
+	}
+}
+
 // EndSession ends the session.
 func (c *Client) EndSession() {
 	if c.Terminated {
@@ -298,7 +320,6 @@ func (c *Client) CommitTransaction() error {
 		return err
 	}
 	c.state = Committed
-	c.PinnedSelector = nil
 	return nil
 }
 
@@ -315,7 +336,7 @@ func (c *Client) CheckAbortTransaction() error {
 	return nil
 }
 
-// AbortTransaction updates the state for a successfully committed transaction and returns
+// AbortTransaction updates the state for a successfully aborted transaction and returns
 // an error if not permissible.  It does not actually perform the abort.
 func (c *Client) AbortTransaction() error {
 	err := c.CheckAbortTransaction()
@@ -337,7 +358,7 @@ func (c *Client) ApplyCommand(desc description.Server) {
 		c.state = InProgress
 		// If this is in a transaction and the server is a mongos, pin it
 		if desc.Kind == description.Mongos {
-			c.PinnedSelector = desc
+			c.PinnedSelector = &desc
 		}
 	} else if c.state == Committed || c.state == Aborted {
 		c.clearTransactionOpts()
@@ -353,4 +374,5 @@ func (c *Client) clearTransactionOpts() {
 	c.CurrentRp = nil
 	c.CurrentRc = nil
 	c.PinnedSelector = nil
+	c.RecoveryToken = nil
 }
