229a9c94a4735eccfc431ea183e0942de7569f58
Divjot Arora <divjot.arora@10gen.com>
2019-04-30 11:31:29 -0400

    Allow SingleResult Decode and DecodeBytes methods to be called more than once.
    GODRIVER-1004
    
    Change-Id: Ia1a7471dda022414acccba2b81e13822a24285fb

--- mongo/single_result.go
+++ mongo/single_result.go
@@ -39,27 +39,11 @@ func (sr *SingleResult) Decode(v interface{}) error {
 	if sr.reg == nil {
 		return bson.ErrNilRegistry
 	}
-	switch {
-	case sr.rdr != nil:
-		if v == nil {
-			return nil
-		}
-		return bson.UnmarshalWithRegistry(sr.reg, sr.rdr, v)
-	case sr.cur != nil:
-		defer sr.cur.Close(context.TODO())
-		if !sr.cur.Next(context.TODO()) {
-			if err := sr.cur.Err(); err != nil {
-				return err
-			}
-			return ErrNoDocuments
-		}
-		if v == nil {
-			return nil
-		}
-		return sr.cur.Decode(v)
-	}
 
-	return ErrNoDocuments
+	if sr.err = sr.setRdrContents(); sr.err != nil {
+		return sr.err
+	}
+	return bson.UnmarshalWithRegistry(sr.reg, sr.rdr, v)
 }
 
 // DecodeBytes will return a copy of the document as a bson.Raw. If there was an
@@ -67,23 +51,37 @@ func (sr *SingleResult) Decode(v interface{}) error {
 // will be returned. If there were no returned documents, ErrNoDocuments is
 // returned.
 func (sr *SingleResult) DecodeBytes() (bson.Raw, error) {
+	if sr.err != nil {
+		return nil, sr.err
+	}
+
+	if sr.err = sr.setRdrContents(); sr.err != nil {
+		return nil, sr.err
+	}
+	return sr.rdr, nil
+}
+
+// setRdrContents will set the contents of rdr by iterating the underlying cursor if necessary.
+func (sr *SingleResult) setRdrContents() error {
 	switch {
 	case sr.err != nil:
-		return nil, sr.err
+		return sr.err
 	case sr.rdr != nil:
-		return sr.rdr, nil
+		return nil
 	case sr.cur != nil:
 		defer sr.cur.Close(context.TODO())
 		if !sr.cur.Next(context.TODO()) {
 			if err := sr.cur.Err(); err != nil {
-				return nil, err
+				return err
 			}
-			return nil, ErrNoDocuments
+
+			return ErrNoDocuments
 		}
-		return sr.cur.Current, nil
+		sr.rdr = sr.cur.Current
+		return nil
 	}
 
-	return nil, ErrNoDocuments
+	return ErrNoDocuments
 }
 
 // Err will return the error from the operation that created this SingleResult.
