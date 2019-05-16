70a78023176fe757fc5155ad1bf43664dc26a882
iwysiu <isabella.siu@10gen.com>
2019-05-16 13:41:22 -0400

    GODRIVER-1047 generate distinct operation
    Change-Id: If89f903436b26ac4b7021a5b1d813ec185a77560

--- x/mongo/driver/operation/operation.go
+++ x/mongo/driver/operation/operation.go
@@ -2,3 +2,4 @@ package operation
 
 //go:generate operationgen insert.toml operation insert.go
 //go:generate operationgen find.toml operation find.go
+//go:generate operationgen distinct.toml operation distinct.go
