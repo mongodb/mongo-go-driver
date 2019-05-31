916f685312a87e5a23849f772c3f536a05caabe1
Connie Yu <connie.yu@10gen.com>
2019-05-31 13:06:06 -0400

    Add Compare method to primitive.Timestamp
    GODRIVER-736
    
    Change-Id: I89dfaeb3cb6e9d17ffe1d4574be483e2e115f3cd

---
+++ bson/primitive/primitive_test.go
@@ -0,0 +1,33 @@
+// Copyright (C) MongoDB, Inc. 2017-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+package primitive
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTimestampCompare(t *testing.T) {
+	testcases := []struct {
+		name     string
+		tp       Timestamp
+		tp2      Timestamp
+		expected int
+	}{
+		{"equal", Timestamp{T: 12345, I: 67890}, Timestamp{T: 12345, I: 67890}, 0},
+		{"greater than", Timestamp{T: 12345, I: 67890}, Timestamp{T: 2345, I: 7890}, 1},
+		{"less than", Timestamp{T: 12345, I: 67890}, Timestamp{T: 112345, I: 167890}, -1},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Compare(tc.tp, tc.tp2)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
