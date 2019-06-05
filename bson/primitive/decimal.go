4b5512b16d602bea8385a16b734b7554099d15e6
ZHOULIREN <254244460@qq.com>
2019-06-05 17:58:36 +0800

    Enhance Decimal128 with support parse from or to big.Int.

--- bson/primitive/decimal.go
+++ bson/primitive/decimal.go
@@ -11,8 +11,14 @@ package primitive
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
 	"strings"
+)
+
+const (
+	MaxDecimal128Exp = 6111
+	MinDecimal128Exp = -6176
 )
 
 // Decimal128 holds decimal128 BSON values.
@@ -25,7 +31,7 @@ func NewDecimal128(h, l uint64) Decimal128 {
 	return Decimal128{h: h, l: l}
 }
 
-// GetBytes returns the underlying bytes of the BSON decimal value as two uint16 values. The first
+// GetBytes returns the underlying bytes of the BSON decimal value as two uint64 values. The first
 // contains the most first 8 bytes of the value and the second contains the latter.
 func (d Decimal128) GetBytes() (uint64, uint64) {
 	return d.h, d.l
@@ -52,13 +58,13 @@ func (d Decimal128) String() string {
 	if d.h>>61&3 == 3 {
 		// Bits: 1*sign 2*ignored 14*exponent 111*significand.
 		// Implicit 0b100 prefix in significand.
-		e = int(d.h>>47&(1<<14-1)) - 6176
+		e = int(d.h>>47&(1<<14-1)) + MinDecimal128Exp
 		//h = 4<<47 | d.h&(1<<47-1)
 		// Spec says all of these values are out of range.
 		h, l = 0, 0
 	} else {
 		// Bits: 1*sign 14*exponent 113*significand
-		e = int(d.h>>49&(1<<14-1)) - 6176
+		e = int(d.h>>49&(1<<14-1)) + MinDecimal128Exp
 		h = d.h & (1<<49 - 1)
 	}
 
@@ -114,6 +120,80 @@ Loop:
 	return string(repr[last+pos:])
 }
 
+// BigInt returns significand as big.Int and exponent, bi * 10 ^ exp.
+func (d Decimal128) BigInt() (bi *big.Int, exp int, err error) {
+	h, l := d.GetBytes()
+	var pos int // positive sign
+
+	if h>>63&1 == 0 {
+		pos = 1
+	}
+
+	switch h >> 58 & (1<<5 - 1) {
+	case 0x1F:
+		return nil, 0, fmt.Errorf("cannot parse NaN as a *big.Int")
+	case 0x1E:
+		return nil, 0, fmt.Errorf("cannot parse %s as a *big.Int", "-Infinity"[pos:])
+	}
+
+	if h>>61&3 == 3 {
+		// Bits: 1*sign 2*ignored 14*exponent 111*significand.
+		// Implicit 0b100 prefix in significand.
+		exp = int(h>>47&(1<<14-1)) + MinDecimal128Exp
+		//h = 4<<47 | d.h&(1<<47-1)
+		// Spec says all of these values are out of range.
+		h, l = 0, 0
+	} else {
+		// Bits: 1*sign 14*exponent 113*significand
+		exp = int(h>>49&(1<<14-1)) + MinDecimal128Exp
+		h = h & (1<<49 - 1)
+	}
+
+	// Would be handled by the logic below, but that's trivial and common.
+	if h == 0 && l == 0 && exp == 0 {
+		if pos == 1 {
+			return new(big.Int), 0, nil
+		}
+		return new(big.Int), 0, nil
+	}
+
+	bi = big.NewInt(0)
+	const host32bit = ^uint(0)>>32 == 0
+	if host32bit {
+		bi.SetBits([]big.Word{big.Word(l), big.Word(l >> 32), big.Word(h), big.Word(h >> 32)})
+	} else {
+		bi.SetBits([]big.Word{big.Word(l), big.Word(h)})
+	}
+
+	if pos == 0 {
+		return bi.Neg(bi), exp, nil
+	}
+	return
+}
+
+// IsNaN returns whether d is NaN.
+func (d Decimal128) IsNaN() bool {
+	return d.h>>58&(1<<5-1) == 0x1F
+}
+
+// IsInf returns:
+//
+//   +1 d == Infinity
+//    0 other case
+//   -1 d == -Infinity
+//
+func (d Decimal128) IsInf() int {
+	if d.h>>58&(1<<5-1) != 0x1E {
+		return 0
+	}
+
+	if d.h>>63&1 == 0 {
+		return 1
+	} else {
+		return -1
+	}
+}
+
 func divmod(h, l uint64, div uint32) (qh, ql uint64, rem uint32) {
 	div64 := uint64(div)
 	a := h >> 32
@@ -139,7 +219,7 @@ func dErr(s string) (Decimal128, error) {
 	return dNaN, fmt.Errorf("cannot parse %q as a decimal128", s)
 }
 
-//ParseDecimal128 takes the given string and attempts to parse it into a valid
+// ParseDecimal128 takes the given string and attempts to parse it into a valid
 // Decimal128 value.
 func ParseDecimal128(s string) (Decimal128, error) {
 	orig := s
@@ -250,10 +330,10 @@ func ParseDecimal128(s string) (Decimal128, error) {
 			n = -n
 		}
 		e += n
-		for e < -6176 {
+		for e < MinDecimal128Exp {
 			// Subnormal.
 			var div uint32 = 1
-			for div < 1e9 && e < -6176 {
+			for div < 1e9 && e < MinDecimal128Exp {
 				div *= 10
 				e++
 			}
@@ -263,10 +343,10 @@ func ParseDecimal128(s string) (Decimal128, error) {
 				return dErr(orig)
 			}
 		}
-		for e > 6111 {
+		for e > MaxDecimal128Exp {
 			// Clamped.
 			var mul uint32 = 1
-			for mul < 1e9 && e > 6111 {
+			for mul < 1e9 && e > MaxDecimal128Exp {
 				mul *= 10
 				e--
 			}
@@ -275,7 +355,7 @@ func ParseDecimal128(s string) (Decimal128, error) {
 				return dErr(orig)
 			}
 		}
-		if e < -6176 || e > 6111 {
+		if e < MinDecimal128Exp || e > MaxDecimal128Exp {
 			return dErr(orig)
 		}
 	}
@@ -284,7 +364,7 @@ func ParseDecimal128(s string) (Decimal128, error) {
 		return dErr(orig)
 	}
 
-	h |= uint64(e+6176) & uint64(1<<14-1) << 49
+	h |= uint64(e-MinDecimal128Exp) & uint64(1<<14-1) << 49
 	if neg {
 		h |= 1 << 63
 	}
@@ -304,4 +384,64 @@ func muladd(h, l uint64, mul uint32, add uint32) (resh, resl uint64, overflow ui
 	d = d&(1<<32-1) + c>>32
 
 	return (d<<32 | c&(1<<32-1)), (b<<32 | a&(1<<32-1)), uint32(d >> 32)
+}
+
+var (
+	ten  = big.NewInt(10)
+	zero = new(big.Int)
+	maxS = new(big.Int).SetBytes([]byte{0x1, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}) // 14 bits
+)
+
+// ParseDecimal128FromBigInt attempts to parse the given significand and exponent into a valid Decimal128 value.
+func ParseDecimal128FromBigInt(bi *big.Int, exp int) (Decimal128, bool) {
+	//copy
+	bi = new(big.Int).Set(bi)
+
+	q := new(big.Int)
+	r := new(big.Int)
+
+	for bi.CmpAbs(maxS) == 1 {
+		bi, _ = q.QuoRem(bi, ten, r)
+		if r.Cmp(zero) != 0 {
+			return Decimal128{}, false
+		}
+		exp++
+		if exp > MaxDecimal128Exp {
+			return Decimal128{}, false
+		}
+	}
+
+	for exp < MinDecimal128Exp {
+		// Subnormal.
+		bi, _ = q.QuoRem(bi, ten, r)
+		if r.Cmp(zero) != 0 {
+			return Decimal128{}, false
+		}
+		exp++
+	}
+	for exp > MaxDecimal128Exp {
+		// Clamped.
+		bi.Mul(bi, ten)
+		if bi.CmpAbs(maxS) == 1 {
+			return Decimal128{}, false
+		}
+		exp--
+	}
+
+	b := bi.Bytes()
+	var h, l uint64
+	for i := 0; i < len(b); i++ {
+		if i < len(b)-8 {
+			h = h<<8 | uint64(b[i])
+		} else {
+			l = l<<8 | uint64(b[i])
+		}
+	}
+
+	h |= uint64(exp-MinDecimal128Exp) & uint64(1<<14-1) << 49
+	if bi.Sign() == -1 {
+		h |= 1 << 63
+	}
+
+	return Decimal128{h: h, l: l}, true
 }
