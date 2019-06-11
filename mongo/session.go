fef578da42499d675d06707ec9049dce553b041a
Connie Yu <connie.yu@10gen.com>
2019-06-11 10:56:48 -0400

    Generate AbortTransaction operation
    GODRIVER-1043
    
    Change-Id: Ic7a1501c06d3cc6a3aa4b30c1e45e43a5f1dbfa2

--- mongo/session.go
+++ mongo/session.go
@@ -14,7 +14,10 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
+	"go.mongodb.org/mongo-driver/x/mongo/driver"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/description"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/operation"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/session"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/topology"
 	"go.mongodb.org/mongo-driver/x/mongo/driverlegacy"
@@ -180,15 +183,25 @@ func (s *sessionImpl) AbortTransaction(ctx context.Context) error {
 		return s.clientSession.AbortTransaction()
 	}
 
-	cmd := command.AbortTransaction{
-		Session: s.clientSession,
+	var selector description.ServerSelectorFunc = func(t description.Topology, svrs []description.Server) ([]description.Server, error) {
+		if s.clientSession.PinnedServer != nil {
+			return s.clientSession.PinnedServer.SelectServer(t, svrs)
+		}
+		return description.WriteSelector().SelectServer(t, svrs)
 	}
 
 	s.clientSession.Aborting = true
-	_, err = driverlegacy.AbortTransaction(ctx, cmd, s.topo, description.WriteSelector())
+	err = operation.NewAbortTransaction().Session(s.clientSession).ClusterClock(s.client.clock).Database("admin").
+		Deployment(s.topo).WriteConcern(s.clientSession.CurrentWc).ServerSelector(selector).
+		Retry(driver.RetryOncePerCommand).CommandMonitor(s.client.monitor).RecoveryToken(bsoncore.Document(s.clientSession.RecoveryToken)).Execute(ctx)
 
-	_ = s.clientSession.AbortTransaction()
-	return replaceErrors(err)
+	s.clientSession.Aborting = false
+	abortErr := s.clientSession.AbortTransaction()
+
+	if err != nil {
+		return replaceErrors(err)
+	}
+	return abortErr
 }
 
 // CommitTransaction commits the sesson's transaction.
