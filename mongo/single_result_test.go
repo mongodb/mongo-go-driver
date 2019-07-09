8d233214999ebbdb0fe6d33a7ea544d86febe204
Connie Yu <connie.yu@10gen.com>
2019-07-09 15:55:25 -0400

    Return results from runCommand in error cases
    GODRIVER-832
    
    Change-Id: I5dc892d7cab5e3d8cf03dc3acda3bc77787a0a90

--- mongo/single_result_test.go
+++ mongo/single_result_test.go
@@ -8,6 +8,7 @@ package mongo
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -51,6 +52,18 @@ func TestSingleResult(t *testing.T) {
 		err := sr.Err()
 		if err != ErrNoDocuments {
 			t.Fatalf("Error returned by SingleResult.Err() was %v when ErrNoDocuments was expected", err)
+		}
+	})
+
+	t.Run("TestDecodeWithErr", func(t *testing.T) {
+		r := []byte("foo")
+		sr := &SingleResult{rdr: r, err: errors.New("DecodeBytes error")}
+		res, err := sr.DecodeBytes()
+		if !bytes.Equal(res, r) {
+			t.Fatalf("DecodeBytes contents do not match; expected %v, returned %v", res, r)
+		}
+		if err != sr.err {
+			t.Fatalf("Error returned by DecodeBytes was %v when %v was expected", err, sr.err)
 		}
 	})
 }
