7225ad02c843c34ff250276b25a521514dcae8e5
Emmanuel Eppinger <manny.eppinger@mongodb.com>
2019-07-10 13:53:49 -0400

    Implement Connection Pooling to follow CMAP Spec
    GODRIVER-622
    
    Change-Id: Ied6e03c1dc2337fc9fa6b86ab42ef61740308681

--- mongo/options/clientoptions_test.go
+++ mongo/options/clientoptions_test.go
@@ -6,6 +6,7 @@ import (
 	"crypto/x509"
 	"errors"
 	"fmt"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/topology"
 	"net"
 	"os"
 	"reflect"
@@ -45,6 +46,7 @@ func TestClientOptions(t *testing.T) {
 		}
 	})
 	t.Run("Set", func(t *testing.T) {
+		var poolMonitor = func(_ topology.PoolEvent) {}
 		testCases := []struct {
 			name        string
 			fn          interface{} // method to be run
@@ -61,7 +63,9 @@ func TestClientOptions(t *testing.T) {
 			{"Hosts", (*ClientOptions).SetHosts, []string{"localhost:27017", "localhost:27018", "localhost:27019"}, "Hosts", true},
 			{"LocalThreshold", (*ClientOptions).SetLocalThreshold, 5 * time.Second, "LocalThreshold", true},
 			{"MaxConnIdleTime", (*ClientOptions).SetMaxConnIdleTime, 5 * time.Second, "MaxConnIdleTime", true},
-			{"MaxPoolSize", (*ClientOptions).SetMaxPoolSize, uint16(250), "MaxPoolSize", true},
+			{"MaxPoolSize", (*ClientOptions).SetMaxPoolSize, uint64(250), "MaxPoolSize", true},
+			{"MinPoolSize", (*ClientOptions).SetMinPoolSize, uint64(10), "MinPoolSize", true},
+			{"PoolMonitor", (*ClientOptions).SetPoolMonitor, &poolMonitor, "PoolMonitor", false},
 			{"Monitor", (*ClientOptions).SetMonitor, &event.CommandMonitor{}, "Monitor", false},
 			{"ReadConcern", (*ClientOptions).SetReadConcern, readconcern.Majority(), "ReadConcern", false},
 			{"ReadPreference", (*ClientOptions).SetReadPreference, readpref.SecondaryPreferred(), "ReadPreference", false},
@@ -132,6 +136,7 @@ func TestClientOptions(t *testing.T) {
 					cmp.AllowUnexported(readconcern.ReadConcern{}, writeconcern.WriteConcern{}, readpref.ReadPref{}),
 					cmp.Comparer(func(r1, r2 *bsoncodec.Registry) bool { return r1 == r2 }),
 					cmp.Comparer(func(cfg1, cfg2 *tls.Config) bool { return cfg1 == cfg2 }),
+					cmp.Comparer(func(fp1, fp2 *topology.PoolMonitor) bool { return fp1 == fp2 }),
 				) {
 					t.Errorf("Field not set properly. got %v; want %v", got.Interface(), want.Interface())
 				}
@@ -145,6 +150,7 @@ func TestClientOptions(t *testing.T) {
 				cmp.AllowUnexported(readconcern.ReadConcern{}, writeconcern.WriteConcern{}, readpref.ReadPref{}),
 				cmp.Comparer(func(r1, r2 *bsoncodec.Registry) bool { return r1 == r2 }),
 				cmp.Comparer(func(cfg1, cfg2 *tls.Config) bool { return cfg1 == cfg2 }),
+				cmp.Comparer(func(fp1, fp2 *topology.PoolMonitor) bool { return fp1 == fp2 }),
 				cmp.AllowUnexported(ClientOptions{}),
 			); diff != "" {
 				t.Errorf("diff:\n%s", diff)
--- x/mongo/driver/topology/connection_test.go
+++ x/mongo/driver/topology/connection_test.go
@@ -166,7 +166,7 @@ func TestConnection(t *testing.T) {
 						t.Errorf("errors do not match. got %v; want %v", got, want)
 					}
 					if !tnc.closed {
-						t.Errorf("failed to close net.Conn after error writing bytes.")
+						t.Errorf("failed to closeConnection net.Conn after error writing bytes.")
 					}
 				})
 				tnc := &testNetConn{}
@@ -248,7 +248,7 @@ func TestConnection(t *testing.T) {
 					t.Errorf("errors do not match. got %v; want %v", got, want)
 				}
 				if !tnc.closed {
-					t.Errorf("failed to close net.Conn after error writing bytes.")
+					t.Errorf("failed to closeConnection net.Conn after error writing bytes.")
 				}
 			})
 			t.Run("Read (wire message)", func(t *testing.T) {
@@ -261,7 +261,7 @@ func TestConnection(t *testing.T) {
 					t.Errorf("errors do not match. got %v; want %v", got, want)
 				}
 				if !tnc.closed {
-					t.Errorf("failed to close net.Conn after error writing bytes.")
+					t.Errorf("failed to closeConnection net.Conn after error writing bytes.")
 				}
 			})
 			t.Run("Read (success)", func(t *testing.T) {
@@ -469,8 +469,9 @@ func (nc *netconn) Close() error {
 
 type dialer struct {
 	Dialer
-	opened map[*netconn]struct{}
-	closed map[*netconn]struct{}
+	opened        map[*netconn]struct{}
+	closed        map[*netconn]struct{}
+	closeCallBack func()
 	sync.Mutex
 }
 
@@ -494,6 +495,9 @@ func (d *dialer) connclosed(nc *netconn) {
 	d.Lock()
 	defer d.Unlock()
 	d.closed[nc] = struct{}{}
+	if d.closeCallBack != nil {
+		d.closeCallBack()
+	}
 }
 
 func (d *dialer) lenopened() int {
