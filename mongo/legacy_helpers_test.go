2e4ee12c0c87409245a7ee9109c54f12c1598c3c
Divjot Arora <divjot.arora@10gen.com>
2019-08-29 15:19:44 -0400

    Use testing library for change stream prose tests.
    GODRIVER-1267
    
    Change-Id: I3c42728ba4abf03cd38fd54152f57e72a05bd5a8

--- mongo/legacy_helpers_test.go
+++ mongo/legacy_helpers_test.go
@@ -9,17 +9,25 @@ package mongo
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/internal/testutil"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/mongo/writeconcern"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/session"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 )
 
 // temporary file to hold test helpers as we are porting tests to the new integration testing framework.
+
+var wcMajority = writeconcern.New(writeconcern.WMajority())
+
+var doc1 = bsonx.Doc{
+	{"x", bsonx.Int32(1)},
+}
 
 func createTestClient(t *testing.T) *Client {
 	id, _ := uuid.New()
@@ -58,4 +66,22 @@ func createTestDatabase(t *testing.T, name *string, opts ...*options.DatabaseOpt
 	dbOpts := []*options.DatabaseOptions{options.Database().SetWriteConcern(writeconcern.New(writeconcern.WMajority()))}
 	dbOpts = append(dbOpts, opts...)
 	return client.Database(*name, dbOpts...)
+}
+
+func skipIfBelow36(t *testing.T) {
+	serverVersion, err := getServerVersion(createTestDatabase(t, nil))
+	require.NoError(t, err, "unable to get server version of database")
+
+	if compareVersions(t, serverVersion, "3.6") < 0 {
+		t.Skip()
+	}
+}
+
+func skipIfBelow32(t *testing.T) {
+	serverVersion, err := getServerVersion(createTestDatabase(t, nil))
+	require.NoError(t, err)
+
+	if compareVersions(t, serverVersion, "3.2") < 0 {
+		t.Skip()
+	}
 }
