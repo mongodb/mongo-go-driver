ba4f05cf5aecba3dc3983d9ca6494cc446a4dc78
Divjot Arora <divjot.arora@10gen.com>
2019-08-31 16:23:03 -0400

    Use testing library for collection tests.
    GODRIVER-1271
    
    Change-Id: Ia2e3477167f46beca2c070b34359a75290802001

--- mongo/legacy_helpers_test.go
+++ mongo/legacy_helpers_test.go
@@ -7,13 +7,16 @@
 package mongo
 
 import (
+	"context"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/internal/testutil"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/mongo/writeconcern"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/session"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
@@ -58,4 +61,42 @@ func createTestDatabase(t *testing.T, name *string, opts ...*options.DatabaseOpt
 	dbOpts := []*options.DatabaseOptions{options.Database().SetWriteConcern(writeconcern.New(writeconcern.WMajority()))}
 	dbOpts = append(dbOpts, opts...)
 	return client.Database(*name, dbOpts...)
+}
+
+func createTestCollection(t *testing.T, dbName *string, collName *string, opts ...*options.CollectionOptions) *Collection {
+	if collName == nil {
+		coll := testutil.ColName(t)
+		collName = &coll
+	}
+
+	db := createTestDatabase(t, dbName)
+	db.RunCommand(
+		context.Background(),
+		bsonx.Doc{{"create", bsonx.String(*collName)}},
+	)
+
+	collOpts := []*options.CollectionOptions{options.Collection().SetWriteConcern(writeconcern.New(writeconcern.WMajority()))}
+	collOpts = append(collOpts, opts...)
+	return db.Collection(*collName, collOpts...)
+}
+
+func skipIfBelow34(t *testing.T, db *Database) {
+	versionStr, err := getServerVersion(db)
+	if err != nil {
+		t.Fatalf("error getting server version: %s", err)
+	}
+	if compareVersions(t, versionStr, "3.4") < 0 {
+		t.Skip("skipping collation test for server version < 3.4")
+	}
+}
+
+func initCollection(t *testing.T, coll *Collection) {
+	docs := []interface{}{}
+	var i int32
+	for i = 1; i <= 5; i++ {
+		docs = append(docs, bsonx.Doc{{"x", bsonx.Int32(i)}})
+	}
+
+	_, err := coll.InsertMany(ctx, docs)
+	require.Nil(t, err)
 }
