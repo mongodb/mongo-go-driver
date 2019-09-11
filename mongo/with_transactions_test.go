fa6fc1d46f38dc10884935eef2b1f1714786e591
Divjot Arora <divjot.arora@10gen.com>
2019-09-11 19:08:20 -0400

    Use assertion library for convenient transactions tests.
    GODRIVER-1310
    
    Change-Id: Ife98f363ae63397e085d3cf81a383f4e84364bf8

--- mongo/with_transactions_test.go
+++ mongo/with_transactions_test.go
@@ -9,154 +9,180 @@ package mongo
 import (
 	"context"
 	"errors"
-	"os"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/internal/testutil"
+	"go.mongodb.org/mongo-driver/internal/testutil/assert"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 	"go.mongodb.org/mongo-driver/x/mongo/driver"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/description"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/topology"
 )
 
-const convenientTransactionTestsDir = "../data/convenient-transactions"
+func setupConvenientTransactions(t *testing.T) *Client {
+	cs := testutil.ConnString(t)
+	clientOpts := options.Client().ApplyURI(cs.Original).SetReadPreference(readpref.Primary()).
+		SetWriteConcern(writeconcern.New(writeconcern.WMajority()))
+	client, err := Connect(ctx, clientOpts)
+	assert.Nil(t, err, "Connect error: %v", err)
+
+	version, err := getServerVersion(client.Database("admin"))
+	assert.Nil(t, err, "getServerVersion error: %v", err)
+	topoKind := client.deployment.(*topology.Topology).Kind()
+	if compareVersions(t, version, "4.1") < 0 || topoKind == description.Single {
+		t.Skip("skipping standalones and versions < 4.1")
+	}
+
+	// pin to a single mongos if necessary
+	if topoKind != description.Sharded {
+		return client
+	}
+	client, err = Connect(ctx, clientOpts.SetHosts([]string{cs.Hosts[0]}))
+	assert.Nil(t, err, "Connect error: %v", err)
+	return client
+}
 
 func TestConvenientTransactions(t *testing.T) {
-	cs := testutil.ConnString(t)
-	opts := options.Client().ApplyURI(cs.String())
-	if os.Getenv("TOPOLOGY") == "sharded_cluster" {
-		opts.SetHosts([]string{opts.Hosts[0]})
-	}
-	client, err := Connect(context.Background(), opts)
-	require.NoError(t, err)
-	defer func() { _ = client.Disconnect(context.Background()) }()
-	dbName := "TestConvenientTransactions"
-	db := client.Database(dbName)
-
+	client := setupConvenientTransactions(t)
+	db := client.Database("TestConvenientTransactions")
 	dbAdmin := client.Database("admin")
-	version, err := getServerVersion(dbAdmin)
-	require.NoError(t, err)
 
-	if compareVersions(t, version, "4.1") < 0 || os.Getenv("TOPOLOGY") == "server" {
-		t.Skip()
-	}
+	defer func() {
+		err := dbAdmin.RunCommand(ctx, bson.D{
+			{"killAllSessions", bson.A{}},
+		}).Err()
+		if err != nil {
+			if ce, ok := err.(CommandError); !ok || ce.Code != errorInterrupted {
+				t.Fatalf("killAllSessions error: %v", err)
+			}
+		}
 
-	t.Run("CallbackRaisesCustomError", func(t *testing.T) {
-		collName := "unpinForNextTransaction"
-		db.RunCommand(
-			context.Background(),
-			bson.D{{"drop", collName}},
-		)
+		_ = db.Drop(ctx)
+		_ = client.Disconnect(ctx)
+	}()
 
-		coll := db.Collection(collName)
-		_, err = coll.InsertOne(ctx, bson.D{{"x", 1}})
-		testErr := errors.New("Test Error")
+	t.Run("callback raises custom error", func(t *testing.T) {
+		coll := db.Collection(t.Name())
+		_, err := coll.InsertOne(ctx, bson.D{{"x", 1}})
+		assert.Nil(t, err, "InsertOne error: %v", err)
 
 		sess, err := client.StartSession()
-		require.NoError(t, err)
+		assert.Nil(t, err, "StartSession error: %v", err)
 		defer sess.EndSession(context.Background())
+
+		testErr := errors.New("test error")
 		_, err = sess.WithTransaction(context.Background(), func(sessCtx SessionContext) (interface{}, error) {
 			return nil, testErr
 		})
-		require.Error(t, err)
-		require.Equal(t, err, testErr)
+		assert.Equal(t, testErr, err, "expected error %v, got %v", testErr, err)
 	})
-	t.Run("CallbackReturnsAValue", func(t *testing.T) {
-		collName := "CallbackReturnsAValue"
-		db.RunCommand(
-			context.Background(),
-			bson.D{{"drop", collName}},
-		)
-
-		coll := db.Collection(collName)
-		_, err = coll.InsertOne(ctx, bson.D{{"x", 1}})
+	t.Run("callback returns value", func(t *testing.T) {
+		coll := db.Collection(t.Name())
+		_, err := coll.InsertOne(ctx, bson.D{{"x", 1}})
+		assert.Nil(t, err, "InsertOne error: %v", err)
 
 		sess, err := client.StartSession()
-		require.NoError(t, err)
+		assert.Nil(t, err, "StartSession error: %v", err)
 		defer sess.EndSession(context.Background())
+
 		res, err := sess.WithTransaction(context.Background(), func(sessCtx SessionContext) (interface{}, error) {
 			return false, nil
 		})
-		require.NoError(t, err)
+		assert.Nil(t, err, "WithTransaction error: %v", err)
 		resBool, ok := res.(bool)
-		require.True(t, ok)
-		require.False(t, resBool)
+		assert.True(t, ok, "expected result type %T, got %T", false, res)
+		assert.False(t, resBool, "expected result false, got %v", resBool)
 	})
-	t.Run("RetryTimeoutEnforced", func(t *testing.T) {
+	t.Run("retry timeout enforced", func(t *testing.T) {
 		withTransactionTimeout = time.Second
 
-		collName := "RetryTimeoutEnforced"
-		db.RunCommand(
-			context.Background(),
-			bson.D{{"drop", collName}},
-		)
+		coll := db.Collection(t.Name())
+		_, err := coll.InsertOne(ctx, bson.D{{"x", 1}})
+		assert.Nil(t, err, "InsertOne error: %v", err)
 
-		coll := db.Collection(collName)
-		_, err = coll.InsertOne(ctx, bson.D{{"x", 1}})
-
-		t.Run("CallbackWithTransientTransactionError", func(t *testing.T) {
+		t.Run("transient transaction error", func(t *testing.T) {
 			sess, err := client.StartSession()
-			require.NoError(t, err)
+			assert.Nil(t, err, "StartSession error: %v", err)
 			defer sess.EndSession(context.Background())
+
 			_, err = sess.WithTransaction(context.Background(), func(sessCtx SessionContext) (interface{}, error) {
 				return nil, CommandError{Name: "test Error", Labels: []string{driver.TransientTransactionError}}
 			})
-			require.Error(t, err)
+			assert.NotNil(t, err, "expected WithTransaction error, got nil")
 			cmdErr, ok := err.(CommandError)
-			require.True(t, ok)
-			require.True(t, cmdErr.HasErrorLabel(driver.TransientTransactionError))
+			assert.True(t, ok, "expected error type %T, got %T", CommandError{}, err)
+			assert.True(t, cmdErr.HasErrorLabel(driver.TransientTransactionError),
+				"expected error with label %v, got %v", driver.TransientTransactionError, cmdErr)
 		})
-		t.Run("UnknownTransactionCommitResult", func(t *testing.T) {
+		t.Run("unknown transaction commit result", func(t *testing.T) {
 			//set failpoint
 			failpoint := bson.D{{"configureFailPoint", "failCommand"},
 				{"mode", "alwaysOn"},
-				{"data", bson.D{{"failCommands", bson.A{"commitTransaction"}}, {"closeConnection", true}}}}
-			require.NoError(t, dbAdmin.RunCommand(ctx, failpoint).Err())
-			defer func() {
-				require.NoError(t, dbAdmin.RunCommand(ctx, bson.D{
-					{"configureFailPoint", "failCommand"},
-					{"mode", "off"},
-				}).Err())
-			}()
-
-			sess, err := client.StartSession()
-			require.NoError(t, err)
-			defer sess.EndSession(context.Background())
-			_, err = sess.WithTransaction(context.Background(), func(sessCtx SessionContext) (interface{}, error) {
-				_, err := sessCtx.Client().Database(dbName).Collection(collName).InsertOne(sessCtx, bson.D{{"x", 1}})
-				return nil, err
-			})
-			require.Error(t, err)
-			cmdErr, ok := err.(CommandError)
-			require.True(t, ok)
-			require.True(t, cmdErr.HasErrorLabel(driver.UnknownTransactionCommitResult))
-		})
-		t.Run("CommitWithTransientTransactionError", func(t *testing.T) {
-			//set failpoint
-			failpoint := bson.D{{"configureFailPoint", "failCommand"},
-				{"mode", "alwaysOn"},
-				{"data", bson.D{{"failCommands", bson.A{"commitTransaction"}}, {"errorCode", 251}}}}
+				{"data", bson.D{
+					{"failCommands", bson.A{"commitTransaction"}},
+					{"closeConnection", true},
+				}},
+			}
 			err = dbAdmin.RunCommand(ctx, failpoint).Err()
-			require.NoError(t, err)
+			assert.Nil(t, err, "error setting failpoint: %v", err)
 			defer func() {
-				require.NoError(t, dbAdmin.RunCommand(ctx, bson.D{
+				err = dbAdmin.RunCommand(ctx, bson.D{
 					{"configureFailPoint", "failCommand"},
 					{"mode", "off"},
-				}).Err())
+				}).Err()
+				assert.Nil(t, err, "error turning off failpoint: %v", err)
 			}()
 
 			sess, err := client.StartSession()
-			require.NoError(t, err)
+			assert.Nil(t, err, "StartSession error: %v", err)
 			defer sess.EndSession(context.Background())
+
 			_, err = sess.WithTransaction(context.Background(), func(sessCtx SessionContext) (interface{}, error) {
-				_, err := sessCtx.Client().Database(dbName).Collection(collName).InsertOne(sessCtx, bson.D{{"x", 1}})
+				_, err := coll.InsertOne(sessCtx, bson.D{{"x", 1}})
 				return nil, err
 			})
-			require.Error(t, err)
+			assert.NotNil(t, err, "expected WithTransaction error, got nil")
 			cmdErr, ok := err.(CommandError)
-			require.True(t, ok)
-			require.True(t, cmdErr.HasErrorLabel(driver.TransientTransactionError))
+			assert.True(t, ok, "expected error type %T, got %T", CommandError{}, err)
+			assert.True(t, cmdErr.HasErrorLabel(driver.UnknownTransactionCommitResult),
+				"expected error with label %v, got %v", driver.UnknownTransactionCommitResult, cmdErr)
+		})
+		t.Run("commit transient transaction error", func(t *testing.T) {
+			//set failpoint
+			failpoint := bson.D{{"configureFailPoint", "failCommand"},
+				{"mode", "alwaysOn"},
+				{"data", bson.D{
+					{"failCommands", bson.A{"commitTransaction"}},
+					{"errorCode", 251},
+				}},
+			}
+			err = dbAdmin.RunCommand(ctx, failpoint).Err()
+			assert.Nil(t, err, "error setting failpoint: %v", err)
+			defer func() {
+				err = dbAdmin.RunCommand(ctx, bson.D{
+					{"configureFailPoint", "failCommand"},
+					{"mode", "off"},
+				}).Err()
+				assert.Nil(t, err, "error turning off failpoint: %v", err)
+			}()
+
+			sess, err := client.StartSession()
+			assert.Nil(t, err, "StartSession error: %v", err)
+			defer sess.EndSession(context.Background())
+
+			_, err = sess.WithTransaction(context.Background(), func(sessCtx SessionContext) (interface{}, error) {
+				_, err := coll.InsertOne(sessCtx, bson.D{{"x", 1}})
+				return nil, err
+			})
+			assert.NotNil(t, err, "expected WithTransaction error, got nil")
+			cmdErr, ok := err.(CommandError)
+			assert.True(t, ok, "expected error type %T, got %T", CommandError{}, err)
+			assert.True(t, cmdErr.HasErrorLabel(driver.TransientTransactionError),
+				"expected error with label %v, got %v", driver.TransientTransactionError, cmdErr)
 		})
 	})
 }
