0d5963d282b57e611e5cb3aaa64420f3eb5479f4
iwysiu <isabella.siu@10gen.com>
2019-11-14 14:52:06 -0500

    fix copyright header

--- bson/mgocompat/bson_test.go
+++ bson/mgocompat/bson_test.go
@@ -3,6 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Based on gopkg.in/mgo.v2/bson by Gustavo Niemeyer
+// See THIRD-PARTY-NOTICES for original license terms.
 
 package mgocompat
 
