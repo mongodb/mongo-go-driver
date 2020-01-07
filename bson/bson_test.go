21d600473efeade017b59fd84e98a14134f5c713
iwysiu <isabella.siu@10gen.com>
2020-01-07 17:31:13 -0500

    GODRIVER-1456 make extJSONValueWriter escape keys (#270)

--- bson/bson_test.go
+++ bson/bson_test.go
@@ -111,3 +111,21 @@ func TestD(t *testing.T) {
 		}
 	})
 }
+
+func TestExtJSONEscapeKey(t *testing.T) {
+	doc := D{{Key: "\\usb#", Value: int32(1)}}
+	b, err := MarshalExtJSON(&doc, false, false)
+	noerr(t, err)
+
+	want := "{\"\\\\usb#\":1}"
+	if diff := cmp.Diff(want, string(b)); diff != "" {
+		t.Errorf("Marshaled documents do not match. got %v, want %v", string(b), want)
+	}
+
+	var got D
+	err = UnmarshalExtJSON(b, false, &got)
+	noerr(t, err)
+	if !cmp.Equal(got, doc) {
+		t.Errorf("Unmarshaled documents do not match. got %v; want %v", got, doc)
+	}
+}
