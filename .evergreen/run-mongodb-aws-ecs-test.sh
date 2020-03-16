9d07c0cd239a8e4ddf86928e5843a28659523135
iwysiu <isabella.siu@10gen.com>
2020-03-16 10:30:54 -0400

    clyde comments

--- .evergreen/run-mongodb-aws-ecs-test.sh
+++ .evergreen/run-mongodb-aws-ecs-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Don't trace since the URI contains a password that shouldn't show up in the logs
+set -o xtrace
 set -o errexit  # Exit the script with error if any of the commands fail
 
 ############################################
@@ -19,7 +19,5 @@ if echo "$MONGODB_URI" | grep -q "@"; then
   echo "MONGODB_URI unexpectedly contains user credentials in ECS test!";
   exit 1
 fi
-# Now we can safely enable xtrace
-set -o xtrace
 
 ./src/main
