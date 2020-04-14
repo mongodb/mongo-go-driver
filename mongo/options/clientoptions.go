f9623444ee9783a133412532f29bf198f3154f9e
Divjot Arora <divjot.arora@10gen.com>
2020-04-14 13:32:42 -0400

    GODRIVER-1557 Clarify SetCompressors documentation

--- mongo/options/clientoptions.go
+++ mongo/options/clientoptions.go
@@ -377,11 +377,10 @@ func (c *ClientOptions) SetAuth(auth Credential) *ClientOptions {
 // 3. "zstd" - requires server version >= 4.2, and driver version >= 1.2.0 with cgo support enabled or driver version >= 1.3.0
 //    without cgo
 //
-// To use compression, it must be enabled on the server as well. If this option is specified, the driver will perform a
-// negotiation with the server to determine a common list of of compressors and will use the first one in that list when
-// performing operations. See
+// If this option is specified, the driver will perform a negotiation with the server to determine a common list of of
+// compressors and will use the first one in that list when performing operations. See
 // https://docs.mongodb.com/manual/reference/program/mongod/#cmdoption-mongod-networkmessagecompressors for more
-// information about how to enable this feature on the server.
+// information about compression settings on the server.
 //
 // This can also be set through the "compressors" URI option (e.g. "compressors=zstd,zlib,snappy"). The default is
 // an empty slice, meaning no compression will be enabled.
