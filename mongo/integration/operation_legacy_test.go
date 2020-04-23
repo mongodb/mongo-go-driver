492d58f0ad24d3e7899bfa18e8423ddb183499f1
Divjot Arora <divjot.arora@10gen.com>
2020-04-23 17:14:06 -0400

    test fixes

--- mongo/integration/operation_legacy_test.go
+++ mongo/integration/operation_legacy_test.go
@@ -63,11 +63,12 @@ func TestOperationLegacy(t *testing.T) {
 		}
 		for _, tc := range cases {
 			mt.RunOpts(tc.name, mtest.NewOptions().ClientOptions(testClientOpts), func(mt *mtest.T) {
-				// clear any messages written during test setup
+				// Clear any messages written during test setup and replace the consumed messages in ReadResp with
+				// fakeOpReply.
 				for len(testConn.Written) > 0 {
 					<-testConn.Written
+					testConn.ReadResp <- fakeOpReply
 				}
-				testConn.ReadResp <- fakeOpReply
 				expectedQuery := tc.cmdFn(mt)
 
 				assert.NotEqual(mt, 0, len(testConn.Written), "no message written to connection")
