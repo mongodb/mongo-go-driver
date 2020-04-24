d8f11fe80be8aefc2eb0fe659d7c278fd8ee3704
Tim Fogarty <tim.fogarty@mongodb.com>
2020-04-24 18:36:25 +0200

    More info on struct tags

--- bson/doc.go
+++ bson/doc.go
@@ -89,7 +89,10 @@
 //     5. When unmarshalling, a field of type interface{} will follow the D/M type mappings listed above. BSON documents
 //     unmarshalled into an interface{} field will be unmarshalled as a D.
 //
-// The following struct tags can be used to configure behavior:
+// The encoding of each struct field can be customized by the "bson" struct tag. 
+// The tag gives the name of the field, possibly followed by a comma-separated list of options. 
+// The name may be empty in order to specify options without overriding the default field name. The following options can be used 
+// to configure behavior:
 //
 //     1. omitempty: If the omitempty struct tag is specified on a field, the field will not be marshalled if it is set to
 //     the zero value. By default, a struct field is only considered empty if the field's type implements the Zeroer
