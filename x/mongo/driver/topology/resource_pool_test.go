a08d6d24c98a45b88870010fc2080b1e5336007d
iwysiu <isabella.siu@10gen.com>
2020-05-05 14:20:58 -0400

    check that timer is reset after maintain is run

--- x/mongo/driver/topology/resource_pool_test.go
+++ x/mongo/driver/topology/resource_pool_test.go
@@ -182,6 +182,7 @@ func TestResourcePool(t *testing.T) {
 			assert.Equal(t, int32(5), expiredCalled, "expected expire to be called 5 times, got %v", expiredCalled)
 			closeCalled := ec.getCloseCalled()
 			assert.Equal(t, int32(3), closeCalled, "expected close to be called 3 times, got %v", closeCalled)
+			assert.True(t, rp.maintainTimer.Stop(), "expected timer to be reset")
 		})
 	})
 }
