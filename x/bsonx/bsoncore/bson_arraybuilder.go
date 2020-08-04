c3f0ca4812d7dd41c7d42e56445a6066f56cc2c8
kriti ravindran <kriti.ravindran@mongodb.com>
2020-08-04 13:10:37 -0400

    split bsonbuilder file into arrays and documents, refactored and added tests

---
+++ x/bsonx/bsoncore/bson_arraybuilder.go
@@ -0,0 +1,224 @@
+// Copyright (C) MongoDB, Inc. 2017-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+package bsoncore
+
+import (
+	"reflect"
+	"strconv"
+
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ArrayBuilder builds a bson array
+type ArrayBuilder struct {
+	arr     []byte
+	indexes []int32
+	keys    []int
+}
+
+// NewArrayBuilder creates a new ArrayBuilder
+func NewArrayBuilder() *ArrayBuilder {
+	return (&ArrayBuilder{}).startArray()
+}
+
+// startArray reserves the array's length and sets the index to where the length begins
+func (a *ArrayBuilder) startArray() *ArrayBuilder {
+	var index int32
+	index, a.arr = AppendArrayStart(a.arr)
+	a.indexes = append(a.indexes, index)
+	a.keys = append(a.keys, 0)
+	return a
+}
+
+// append element updates the
+func (a *ArrayBuilder) appendElement(fn interface{}, params []interface{}) *ArrayBuilder {
+	last := len(a.keys) - 1
+	function := reflect.ValueOf(fn)
+	parameters := make([]reflect.Value, 0, len(params))
+	for _, param := range params {
+		parameters = append(parameters, reflect.ValueOf(param))
+	}
+	a.arr = function.Call(parameters)[0].Interface().([]byte)
+	a.keys[last]++
+	return a
+}
+
+// Build updates the length of the array and index to the beginning of the documents length
+// bytes, then returns the array (bson bytes)
+func (a *ArrayBuilder) Build() (Array, error) {
+	lastIndex := len(a.indexes) - 1
+	lastKey := len(a.keys) - 1
+	var err error
+	a.arr, err = AppendArrayEnd(a.arr, a.indexes[lastIndex])
+	if err != nil {
+		return nil, err
+	}
+	a.indexes = a.indexes[:lastIndex]
+	a.keys = a.keys[:lastKey]
+	return a.arr, nil
+}
+
+// AppendInt32 will append i32 to ArrayBuilder.arr
+func (a *ArrayBuilder) AppendInt32(i32 int32) *ArrayBuilder {
+	a = a.appendElement(AppendInt32Element, []interface{}{a.arr, strconv.Itoa(a.keys[len(a.keys)-1]), i32})
+	return a
+}
+
+// AppendDocument will append doc to ArrayBuilder.arr
+func (a *ArrayBuilder) AppendDocument(doc []byte) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendDocumentElement(a.arr, strconv.Itoa(a.keys[last]), doc)
+	a.keys[last]++
+	return a
+}
+
+// AppendArray will append arr to ArrayBuilder.arr
+func (a *ArrayBuilder) AppendArray(arr []byte) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendHeader(a.arr, bsontype.Array, strconv.Itoa(a.keys[last]))
+	a.arr = AppendArray(a.arr, arr)
+	a.keys[last]++
+	return a
+}
+
+// AppendDouble will append f to ArrayBuilder.doc
+func (a *ArrayBuilder) AppendDouble(f float64) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendDoubleElement(a.arr, strconv.Itoa(a.keys[last]), f)
+	a.keys[last]++
+	return a
+}
+
+// AppendString will append str to ArrayBuilder.doc
+func (a *ArrayBuilder) AppendString(str string) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendStringElement(a.arr, strconv.Itoa(a.keys[last]), str)
+	a.keys[last]++
+	return a
+}
+
+// AppendObjectID will append oid to ArrayBuilder.doc
+func (a *ArrayBuilder) AppendObjectID(oid primitive.ObjectID) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendObjectIDElement(a.arr, strconv.Itoa(a.keys[last]), oid)
+	a.keys[last]++
+	return a
+}
+
+// AppendBinary will append a BSON binary element using subtype, and
+// b to a.arr
+func (a *ArrayBuilder) AppendBinary(subtype byte, b []byte) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendBinaryElement(a.arr, strconv.Itoa(a.keys[last]), subtype, b)
+	a.keys[last]++
+	return a
+}
+
+// AppendBoolean will append a boolean element using b to a.arr
+func (a *ArrayBuilder) AppendBoolean(b bool) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendBooleanElement(a.arr, strconv.Itoa(a.keys[last]), b)
+	a.keys[last]++
+	return a
+}
+
+// AppendDateTime will append datetime element dt to a.arr
+func (a *ArrayBuilder) AppendDateTime(dt int64) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendDateTimeElement(a.arr, strconv.Itoa(a.keys[last]), dt)
+	a.keys[last]++
+	return a
+}
+
+// AppendNull will append a null element to a.arr
+func (a *ArrayBuilder) AppendNull() *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendNullElement(a.arr, strconv.Itoa(a.keys[last]))
+	a.keys[last]++
+	return a
+}
+
+// AppendRegex will append pattern and options to a.arr
+func (a *ArrayBuilder) AppendRegex(pattern, options string) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendRegexElement(a.arr, strconv.Itoa(a.keys[last]), pattern, options)
+	a.keys[last]++
+	return a
+}
+
+// AppendJavaScript will append js to a.arr
+func (a *ArrayBuilder) AppendJavaScript(js string) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendJavaScriptElement(a.arr, strconv.Itoa(a.keys[last]), js)
+	a.keys[last]++
+	return a
+}
+
+// AppendCodeWithScope will append code and scope to a.arr
+func (a *ArrayBuilder) AppendCodeWithScope(code Document, scope []byte) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendCodeWithScopeElement(a.arr, strconv.Itoa(a.keys[last]), string(code), scope)
+	a.keys[last]++
+	return a
+}
+
+// AppendTimestamp will append t and i to a.arr
+func (a *ArrayBuilder) AppendTimestamp(t, i uint32) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendTimestampElement(a.arr, strconv.Itoa(a.keys[last]), t, i)
+	a.keys[last]++
+	return a
+}
+
+// AppendInt64 will append i64 to a.arr
+func (a *ArrayBuilder) AppendInt64(i64 int64) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendInt64Element(a.arr, strconv.Itoa(a.keys[last]), i64)
+	a.keys[last]++
+	return a
+}
+
+// AppendDecimal128 will append d128 to a.arr
+func (a *ArrayBuilder) AppendDecimal128(d128 primitive.Decimal128) *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendDecimal128Element(a.arr, strconv.Itoa(a.keys[last]), d128)
+	a.keys[last]++
+	return a
+}
+
+// AppendMaxKey will append a max key element to a.arr
+func (a *ArrayBuilder) AppendMaxKey() *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendMaxKeyElement(a.arr, strconv.Itoa(a.keys[last]))
+	a.keys[last]++
+	return a
+}
+
+// AppendMinKey will append a min key element to a.arr
+func (a *ArrayBuilder) AppendMinKey() *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendMinKeyElement(a.arr, strconv.Itoa(a.keys[last]))
+	a.keys[last]++
+	return a
+}
+
+// StartArray starts building an inline Array. After this document is completed,
+// the user must call a.FinishArray
+func (a *ArrayBuilder) StartArray() *ArrayBuilder {
+	last := len(a.keys) - 1
+	a.arr = AppendHeader(a.arr, bsontype.Array, strconv.Itoa(a.keys[last]))
+	a.keys[last]++
+	a.startArray()
+	return a
+}
+
+// FinishArray builds the most recent array created
+func (a *ArrayBuilder) FinishArray() *ArrayBuilder {
+	a.arr, _ = a.Build()
+	return a
+}
---
+++ x/bsonx/bsoncore/bson_documentbuilder_test.go
@@ -0,0 +1,207 @@
+// Copyright (C) MongoDB, Inc. 2017-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+package bsoncore
+
+import (
+	"bytes"
+	"encoding/binary"
+	"math"
+	"reflect"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestDocumentBuilder(t *testing.T) {
+	bits := math.Float64bits(3.14159)
+	pi := make([]byte, 8)
+	binary.LittleEndian.PutUint64(pi, bits)
+
+	testCases := []struct {
+		name     string
+		fn       interface{}
+		params   []interface{}
+		expected []byte
+	}{
+		{
+			"AppendInt32",
+			NewDocumentBuilder().AppendInt32,
+			[]interface{}{"foobar", int32(256)},
+			BuildDocumentFromElements(nil, AppendInt32Element(nil, "foobar", 256)),
+		},
+		{
+			"AppendDouble",
+			NewDocumentBuilder().AppendDouble,
+			[]interface{}{"foobar", float64(3.14159)},
+			BuildDocumentFromElements(nil, AppendDoubleElement(nil, "foobar", float64(3.14159))),
+		},
+		{
+			"AppendString",
+			NewDocumentBuilder().AppendString,
+			[]interface{}{"foobar", "x"},
+			BuildDocumentFromElements(nil, AppendStringElement(nil, "foobar", "x")),
+		},
+		{
+			"AppendDocument",
+			NewDocumentBuilder().AppendDocument,
+			[]interface{}{"foobar", []byte{0x05, 0x00, 0x00, 0x00, 0x00}},
+			BuildDocumentFromElements(nil, AppendDocumentElement(nil, "foobar", []byte{0x05, 0x00, 0x00, 0x00, 0x00})),
+		},
+		{
+			"AppendArray",
+			NewDocumentBuilder().AppendArray,
+			[]interface{}{"foobar", []byte{0x05, 0x00, 0x00, 0x00, 0x00}},
+			BuildDocumentFromElements(nil, AppendArrayElement(nil, "foobar", []byte{0x05, 0x00, 0x00, 0x00, 0x00})),
+		},
+		{
+			"AppendBinary",
+			NewDocumentBuilder().AppendBinary,
+			[]interface{}{"foobar", byte(0x02), []byte{0x01, 0x02, 0x03}},
+			BuildDocumentFromElements(nil, AppendBinaryElement(nil, "foobar", byte(0x02), []byte{0x01, 0x02, 0x03})),
+		},
+		{
+			"AppendObjectID",
+			NewDocumentBuilder().AppendObjectID,
+			[]interface{}{
+				"foobar",
+				primitive.ObjectID{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C},
+			},
+			BuildDocumentFromElements(nil, AppendObjectIDElement(nil, "foobar",
+				primitive.ObjectID{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C})),
+		},
+		{
+			"AppendBoolean",
+			NewDocumentBuilder().AppendBoolean,
+			[]interface{}{"foobar", true},
+			BuildDocumentFromElements(nil, AppendBooleanElement(nil, "foobar", true)),
+		},
+		{
+			"AppendDateTime",
+			NewDocumentBuilder().AppendDateTime,
+			[]interface{}{"foobar", int64(256)},
+			BuildDocumentFromElements(nil, AppendDateTimeElement(nil, "foobar", int64(256))),
+		},
+		{
+			"AppendNull",
+			NewDocumentBuilder().AppendNull,
+			[]interface{}{"foobar"},
+			BuildDocumentFromElements(nil, AppendNullElement(nil, "foobar")),
+		},
+		{
+			"AppendRegex",
+			NewDocumentBuilder().AppendRegex,
+			[]interface{}{"foobar", "bar", "baz"},
+			BuildDocumentFromElements(nil, AppendRegexElement(nil, "foobar", "bar", "baz")),
+		},
+		{
+			"AppendJavaScript",
+			NewDocumentBuilder().AppendJavaScript,
+			[]interface{}{"foobar", "barbaz"},
+			BuildDocumentFromElements(nil, AppendJavaScriptElement(nil, "foobar", "barbaz")),
+		},
+		{
+			"AppendCodeWithScope",
+			NewDocumentBuilder().AppendCodeWithScope,
+			[]interface{}{"foobar", Document("barbaz"), []byte{0x05, 0x00, 0x00, 0x00, 0x00}},
+			BuildDocumentFromElements(nil, AppendCodeWithScopeElement(nil, "foobar", "barbaz", []byte{0x05, 0x00, 0x00, 0x00, 0x00})),
+		},
+		{
+			"AppendTimestamp",
+			NewDocumentBuilder().AppendTimestamp,
+			[]interface{}{"foobar", uint32(65536), uint32(256)},
+			BuildDocumentFromElements(nil, AppendTimestampElement(nil, "foobar", uint32(65536), uint32(256))),
+		},
+		{
+			"AppendInt64",
+			NewDocumentBuilder().AppendInt64,
+			[]interface{}{"foobar", int64(4294967296)},
+			BuildDocumentFromElements(nil, AppendInt64Element(nil, "foobar", int64(4294967296))),
+		},
+		{
+			"AppendDecimal128",
+			NewDocumentBuilder().AppendDecimal128,
+			[]interface{}{"foobar", primitive.NewDecimal128(4294967296, 65536)},
+			BuildDocumentFromElements(nil, AppendDecimal128Element(nil, "foobar", primitive.NewDecimal128(4294967296, 65536))),
+		},
+		{
+			"AppendMaxKey",
+			NewDocumentBuilder().AppendMaxKey,
+			[]interface{}{"foobar"},
+			BuildDocumentFromElements(nil, AppendMaxKeyElement(nil, "foobar")),
+		},
+		{
+			"AppendMinKey",
+			NewDocumentBuilder().AppendMinKey,
+			[]interface{}{"foobar"},
+			BuildDocumentFromElements(nil, AppendMinKeyElement(nil, "foobar")),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			fn := reflect.ValueOf(tc.fn)
+			if fn.Kind() != reflect.Func {
+				t.Fatalf("fn must be of kind Func but is a %v", fn.Kind())
+			}
+			if fn.Type().NumIn() != len(tc.params) {
+				t.Fatalf("tc.params must match the number of params in tc.fn. params %d; fn %d", fn.Type().NumIn(), len(tc.params))
+			}
+			if fn.Type().NumOut() != 1 || fn.Type().Out(0) != reflect.TypeOf(&DocumentBuilder{}) {
+				t.Fatalf("fn must have one return parameter and it must be a DocumentBuilder.")
+			}
+			params := make([]reflect.Value, 0, len(tc.params))
+			for _, param := range tc.params {
+				params = append(params, reflect.ValueOf(param))
+			}
+			results := fn.Call(params)
+			got := results[0].Interface().(*DocumentBuilder)
+			doc, _ := got.Build()
+			want := tc.expected
+			if !bytes.Equal(doc, want) {
+				t.Errorf("Did not receive expected bytes. got %v; want %v", got, want)
+			}
+		})
+	}
+	t.Run("TestBuildOneElement", func(t *testing.T) {
+		expected := []byte{0x0c, 0x00, 0x00, 0x00, 0x10, 'x', 0x00, 0x00, 0x01, 0x00, 0x00, 0x00}
+		result, _ := NewDocumentBuilder().AppendInt32("x", int32(256)).Build()
+		if !bytes.Equal(result, expected) {
+			t.Errorf("Documents do not match. got %v; want %v", result, expected)
+		}
+	})
+	t.Run("TestBuildTwoElements", func(t *testing.T) {
+		expected := []byte{
+			0x1b, 0x00, 0x00, 0x00, 0x10, 'x', 0x00, 0x03, 0x00, 0x00, 0x00, 0x04,
+			'y', 0x00, 0x0c, 0x00, 0x00, 0x00, 0x10, '0', 0x00, 0x01, 0x00, 0x00,
+			0x00, 0x00, 0x00,
+		}
+		elem, _ := NewArrayBuilder().AppendInt32(int32(1)).Build()
+		result, _ := NewDocumentBuilder().AppendInt32("x", int32(3)).AppendArray("y", elem).Build()
+		if !bytes.Equal(result, expected) {
+			t.Errorf("Documents do not match. got %v; want %v", result, expected)
+		}
+	})
+	t.Run("TestBuildInlineDocument", func(t *testing.T) {
+		docElement := BuildDocumentFromElements(nil, AppendInt32Element(nil, "x", int32(256)))
+		var expected Document
+		expected = BuildDocumentFromElements(nil, AppendDocumentElement(nil, "y", docElement))
+		result, _ := NewDocumentBuilder().StartDocument("y").AppendInt32("x", int32(256)).FinishDocument().Build()
+		if !bytes.Equal(result, expected) {
+			t.Errorf("Documents do not match. got %v; want %v", result, expected)
+		}
+	})
+	t.Run("TestBuildNestedInlineDocument", func(t *testing.T) {
+		docElement := BuildDocumentFromElements(nil, AppendDoubleElement(nil, "x", 3.14))
+		docInline := BuildDocumentFromElements(nil, AppendDocumentElement(nil, "y", docElement))
+		var expected Document
+		expected = BuildDocumentFromElements(nil, AppendDocumentElement(nil, "z", docInline))
+		result, _ := NewDocumentBuilder().StartDocument("z").StartDocument("y").AppendDouble("x", 3.14).FinishDocument().FinishDocument().Build()
+		if !bytes.Equal(result, expected) {
+			t.Errorf("Documents do not match. got %v; want %v", result, expected)
+		}
+	})
+}
