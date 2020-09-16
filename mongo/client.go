51e3de61f04616a2d97d7c0d9c8758d96b4e52c7
Isabella Siu <isabella.siu@mongodb.com>
2020-09-16 11:51:13 -0400

    GODRIVER-1734 Fix txnNumber addition for retryable writes (#497)

--- mongo/client.go
+++ mongo/client.go
@@ -275,7 +275,8 @@ func (c *Client) StartSession(opts ...*options.SessionOptions) (Session, error) 
 		return nil, replaceErrors(err)
 	}
 
-	sess.RetryWrite = c.retryWrites
+	// Writes are not retryable on standalones, so let operation determine whether to retry
+	sess.RetryWrite = false
 	sess.RetryRead = c.retryReads
 
 	return &sessionImpl{
--- mongo/integration/retryable_writes_prose_test.go
+++ mongo/integration/retryable_writes_prose_test.go
@@ -7,10 +7,12 @@
 package integration
 
 import (
+	"bytes"
 	"testing"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/internal/testutil/assert"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver"
@@ -19,12 +21,12 @@ import (
 func TestRetryableWritesProse(t *testing.T) {
 	clientOpts := options.Client().SetRetryWrites(true).SetWriteConcern(mtest.MajorityWc).
 		SetReadConcern(mtest.MajorityRc)
-	mtOpts := mtest.NewOptions().ClientOptions(clientOpts).MinServerVersion("3.6").Topologies(mtest.ReplicaSet, mtest.Sharded).
-		CreateClient(false)
+	mtOpts := mtest.NewOptions().ClientOptions(clientOpts).MinServerVersion("3.6").CreateClient(false)
 	mt := mtest.New(t, mtOpts)
 	defer mt.Close()
 
-	mt.RunOpts("txn number included", noClientOpts, func(mt *mtest.T) {
+	includeOpts := mtest.NewOptions().Topologies(mtest.ReplicaSet, mtest.Sharded).CreateClient(false)
+	mt.RunOpts("txn number included", includeOpts, func(mt *mtest.T) {
 		updateDoc := bson.D{{"$inc", bson.D{{"x", 1}}}}
 		insertOneDoc := bson.D{{"x", 1}}
 		insertManyOrderedArgs := bson.D{
@@ -75,7 +77,8 @@ func TestRetryableWritesProse(t *testing.T) {
 			})
 		}
 	})
-	mt.Run("wrap mmapv1 error", func(mt *mtest.T) {
+	errorOpts := mtest.NewOptions().Topologies(mtest.ReplicaSet, mtest.Sharded)
+	mt.RunOpts("wrap mmapv1 error", errorOpts, func(mt *mtest.T) {
 		res, err := mt.DB.RunCommand(mtest.Background, bson.D{{"serverStatus", 1}}).DecodeBytes()
 		assert.Nil(mt, err, "serverStatus error: %v", err)
 		storageEngine, ok := res.Lookup("storageEngine", "name").StringValueOK()
@@ -86,5 +89,51 @@ func TestRetryableWritesProse(t *testing.T) {
 		_, err = mt.Coll.InsertOne(mtest.Background, bson.D{{"x", 1}})
 		assert.Equal(mt, driver.ErrUnsupportedStorageEngine, err,
 			"expected error %v, got %v", driver.ErrUnsupportedStorageEngine, err)
+	})
+
+	standaloneOpts := mtest.NewOptions().Topologies(mtest.Single).CreateClient(false)
+	mt.RunOpts("transaction number not sent on writes", standaloneOpts, func(mt *mtest.T) {
+		mt.Run("explicit session", func(mt *mtest.T) {
+			// Standalones do not support retryable writes and will error if a transaction number is sent
+
+			sess, err := mt.Client.StartSession()
+			assert.Nil(mt, err, "StartSession error: %v", err)
+			defer sess.EndSession(mtest.Background)
+
+			mt.ClearEvents()
+
+			err = mongo.WithSession(mtest.Background, sess, func(ctx mongo.SessionContext) error {
+				doc := bson.D{{"foo", 1}}
+				_, err := mt.Coll.InsertOne(ctx, doc)
+				return err
+			})
+			assert.Nil(mt, err, "InsertOne error: %v", err)
+
+			_, wantID := sess.ID().Lookup("id").Binary()
+			command := mt.GetStartedEvent().Command
+			lsid, err := command.LookupErr("lsid")
+			assert.Nil(mt, err, "Error getting lsid: %v", err)
+			_, gotID := lsid.Document().Lookup("id").Binary()
+			assert.True(mt, bytes.Equal(wantID, gotID), "expected session ID %v, got %v", wantID, gotID)
+			txnNumber, err := command.LookupErr("txnNumber")
+			assert.NotNil(mt, err, "expected no txnNumber, got %v", txnNumber)
+		})
+		mt.Run("implicit session", func(mt *mtest.T) {
+			// Standalones do not support retryable writes and will error if a transaction number is sent
+
+			mt.ClearEvents()
+
+			doc := bson.D{{"foo", 1}}
+			_, err := mt.Coll.InsertOne(mtest.Background, doc)
+			assert.Nil(mt, err, "InsertOne error: %v", err)
+
+			command := mt.GetStartedEvent().Command
+			lsid, err := command.LookupErr("lsid")
+			assert.Nil(mt, err, "Error getting lsid: %v", err)
+			_, gotID := lsid.Document().Lookup("id").Binary()
+			assert.NotNil(mt, gotID, "expected session ID, got nil")
+			txnNumber, err := command.LookupErr("txnNumber")
+			assert.NotNil(mt, err, "expected no txnNumber, got %v", txnNumber)
+		})
 	})
 }
