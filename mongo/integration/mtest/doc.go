5505ce800314aea3028bc3c1569f02ccdf978e29
iwysiu <isabella.siu@mongodb.com>
2020-12-01 10:56:06 -0500

    GODRIVER-1767 mark mtest unstable

---
+++ mongo/integration/mtest/doc.go
@@ -0,0 +1,9 @@
+// Copyright (C) MongoDB, Inc. 2017-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Package mtest is unstable and there is no backward compatibility guarantee.
+// It is experimental and subject to change.
+package mtest
