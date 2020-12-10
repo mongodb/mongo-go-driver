ff52649ab2cccdf72890e44d45ee1c3eaba59d81
Benjamin Rewis <benji.rewis@mongodb.com>
2020-12-10 09:50:47 -0500

    Move tests to data/versioned-api

--- mongo/integration/unified/unified_spec_test.go
+++ mongo/integration/unified/unified_spec_test.go
@@ -23,7 +23,7 @@ import (
 var (
 	directories = []string{
 		"unified-test-format/valid-pass",
-		"unified-test-format/versioned-api",
+		"versioned-api",
 	}
 
 	skippedTestDescriptions = map[string]struct{}{
