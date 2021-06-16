8905a5064c25d99fb788e98fc6ef2d235a4be44e
iwysiu <isabella.siu@mongodb.com>
2021-06-16 14:02:20 -0400

    GODRIVER-2019 test agains 5.0 servers

--- mongo/integration/collection_test.go
+++ mongo/integration/collection_test.go
@@ -1737,11 +1737,11 @@ func TestCollection(t *testing.T) {
 			deletes := len(mt.GetAllStartedEvents())
 			assert.True(mt, deletes > 1, "expected multiple batches, got %v", deletes)
 		})
-		mt.Run("update with batches", func(mt *mtest.T) {
+		mt.RunOpts("update with batches", mtest.NewOptions().ClientType(mtest.Mock), func(mt *mtest.T) {
+			maxBatchCount := int(mtest.MockDescription.MaxBatchCount)
+			numModels := maxBatchCount + 50
 			var models []mongo.WriteModel
-			numModels := 100050
 
-			// it's significantly faster to upsert one model and modify the rest than to upsert all of them
 			for i := 0; i < numModels-1; i++ {
 				update := bson.D{
 					{"$set", bson.D{
@@ -1762,6 +1762,29 @@ func TestCollection(t *testing.T) {
 				SetUpsert(true),
 			)
 
+			// Seed mock responses. Responses look like: 
+			// {ok: 1, n: <matched count>, nModified: <modified count>, upserted: [{index:<index>, _id:<ID>},...]}.
+			var responses []bson.D
+			for i := numModels; i > 0; i -= maxBatchCount {
+				count := maxBatchCount
+				// the first document is upserted
+				upsert := bson.D{{"index", 0}, {"_id", "foo"}}
+				if i < maxBatchCount {
+					count = i
+					// the last document is upserted
+					upsert = bson.D{{"index", count-1}, {"_id", "bar"}}
+				}
+				res := mtest.CreateSuccessResponse(
+					bson.E{"n", count},
+					bson.E{"nModified", count-1},
+					bson.E{"upserted",
+						bson.A{upsert},
+					},
+				)
+				responses = append(responses, res)
+			}
+			mt.AddMockResponses(responses...)
+
 			mt.ClearEvents()
 			res, err := mt.Coll.BulkWrite(mtest.Background, models)
 			assert.Nil(mt, err, "BulkWrite error: %v", err)
@@ -1774,31 +1797,17 @@ func TestCollection(t *testing.T) {
 			assert.True(mt, updates > 1, "expected multiple batches, got %v", updates)
 
 			assert.Equal(mt, int64(numModels-2), res.ModifiedCount, "expected %v modified documents, got %v", numModels-2, res.ModifiedCount)
-			assert.Equal(mt, int64(numModels-2), res.MatchedCount, "expected %v matched documents, got %v", numModels-2, res.ModifiedCount)
+			assert.Equal(mt, int64(numModels-2), res.MatchedCount, "expected %v matched documents, got %v", numModels-2, res.MatchedCount)
 			assert.Equal(mt, int64(2), res.UpsertedCount, "expected %v upserted documents, got %v", 2, res.UpsertedCount)
 			assert.Equal(mt, 2, len(res.UpsertedIDs), "expected %v upserted ids, got %v", 2, len(res.UpsertedIDs))
 
 			// find the upserted documents and check their contents
 			id1, ok := res.UpsertedIDs[0]
 			assert.True(mt, ok, "expected id at key 0")
+			assert.Equal(mt, "foo", id1, "expected id to be foo, got %v", id1)
 			id2, ok := res.UpsertedIDs[int64(numModels-1)]
 			assert.True(mt, ok, "expected id at key %v", numModels-1)
-
-			doc, err := mt.Coll.FindOne(mtest.Background, bson.D{{"_id", id1}}).DecodeBytes()
-			a, ok := doc.Lookup("a").Int32OK()
-			assert.True(mt, ok, "expected a to be an int32")
-			assert.Equal(mt, int32(numModels-1), a, "expected a value %v, got %v", numModels-1, a)
-			b, ok := doc.Lookup("b").Int32OK()
-			assert.True(mt, ok, "expected b to be an int32")
-			assert.Equal(mt, int32((numModels-2)*2), b, "expected b value %v, got %v", (numModels-2)*2, b)
-			c, ok := doc.Lookup("c").Int32OK()
-			assert.True(mt, ok, "expected c to be an int32")
-			assert.Equal(mt, int32((numModels-2)*3), c, "expected b value %v, got %v", (numModels-2)*3, c)
-
-			doc, err = mt.Coll.FindOne(mtest.Background, bson.D{{"_id", id2}}).DecodeBytes()
-			x, ok := doc.Lookup("x").Int32OK()
-			assert.True(mt, ok, "expected x to be an int32")
-			assert.Equal(mt, int32(1), x, "expected a value 1, got %v", x)
+			assert.Equal(mt, "bar", id2, "expected id to be bar, got %v", id2)
 		})
 		mt.RunOpts("map hint", noClientOpts, func(mt *mtest.T) {
 			filter := bson.D{{"_id", "foo"}}
