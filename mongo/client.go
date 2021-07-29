2a65b047604588ad62befb15a8711be965260018
Benjamin Rewis <benji.rewis@mongodb.com>
2021-07-29 18:20:05 -0400

    Remove necessarily

--- mongo/client.go
+++ mongo/client.go
@@ -271,7 +271,7 @@ func (c *Client) Ping(ctx context.Context, rp *readpref.ReadPref) error {
 
 // StartSession starts a new session configured with the given options.
 //
-// StartSession does not actually communicate with the server and will not necessarily error if the client is
+// StartSession does not actually communicate with the server and will not error if the client is
 // disconnected.
 //
 // If the DefaultReadConcern, DefaultWriteConcern, or DefaultReadPreference options are not set, the client's read
