2035d751ac8cba01e8c79deabfa244b5742ca124
Benjamin Rewis <benji.rewis@mongodb.com>
2021-08-02 13:21:53 -0400

    Initial fix

--- mongo/integration/client_side_encryption_prose_test.go
+++ mongo/integration/client_side_encryption_prose_test.go
@@ -1037,48 +1037,44 @@ func TestClientSideEncryptionProse(t *testing.T) {
 		}
 	})
 
-	// These tests only run when a KMS mock server is running on localhost:8000.
+	// These tests only run when KMS mock servers are running on localhost:8000 and 8001.
 	mt.RunOpts("kms tls tests", noClientOpts, func(mt *mtest.T) {
-		kmsTlsTestcase := os.Getenv("KMS_TLS_TESTCASE")
-		if kmsTlsTestcase == "" {
-			mt.Skipf("Skipping test as KMS_TLS_TESTCASE is not set")
+		testKmsTls := os.Getenv("TEST_KMS_TLS")
+		if testKmsTls == "" || testKmsTls == "false" {
+			mt.Skipf("Skipping test as TEST_KMS_TLS is unset or false")
 		}
 
 		testcases := []struct {
 			name       string
-			envValue   string
+			kmsPort    int
 			errMessage string
 		}{
 			{
 				"invalid certificate",
-				"INVALID_CERT",
+				8000,
 				"expired",
 			},
 			{
 				"invalid hostname",
-				"INVALID_HOSTNAME",
+				8001,
 				"SANs",
 			},
 		}
 
 		for _, tc := range testcases {
 			mt.Run(tc.name, func(mt *mtest.T) {
-				// Only run test if correct KMS mock server is running.
-				if kmsTlsTestcase != tc.envValue {
-					mt.Skipf("Skipping test as KMS_TLS_TESTCASE is set to %q, expected %v", kmsTlsTestcase, tc.envValue)
-				}
-
 				ceo := options.ClientEncryption().
 					SetKmsProviders(fullKmsProvidersMap).
 					SetKeyVaultNamespace(kvNamespace)
 				cpt := setup(mt, nil, nil, ceo)
 				defer cpt.teardown(mt)
 
+				endpoint := fmt.Sprintf("127.0.0.1:%v", tc.kmsPort)
 				_, err := cpt.clientEnc.CreateDataKey(context.Background(), "aws", options.DataKey().SetMasterKey(
 					bson.D{
 						{"region", "us-east-1"},
 						{"key", "arn:aws:kms:us-east-1:579766882180:key/89fcc2c4-08b0-4bd9-9f25-e30687b580d0"},
-						{"endpoint", "mongodb://127.0.0.1:8000"},
+						{"endpoint", endpoint},
 					},
 				))
 				assert.NotNil(mt, err, "expected CreateDataKey error, got nil")
