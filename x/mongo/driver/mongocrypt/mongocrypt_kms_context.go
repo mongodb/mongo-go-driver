9157fecd792ec7baddf01d4829de815acdc07ad7
Gabriela Asuncion <gabriela.asuncion@mongodb.com>
2021-11-16 10:17:40 -0500

    add KMS provider accessor

--- x/mongo/driver/mongocrypt/mongocrypt_kms_context.go
+++ x/mongo/driver/mongocrypt/mongocrypt_kms_context.go
@@ -33,12 +33,9 @@ func (kc *KmsContext) HostName() (string, error) {
 }
 
 // TODO: create new method to access KMS context's current KMS provider
-func (kc *KmsContext) KMSProvider() (string, error) {
-	var kmsProvider *C.char // out param for mongocrypt function to fill in kmsProvider
-	if ok := C.mongocrypt_kms_ctx_get_kms_provider(kc.wrapped, &kmsProvider); !ok {
-		return "", kc.createErrorFromStatus()
-	}
-	return C.GoString(kmsProvider), nil
+func (kc *KmsContext) KMSProvider() string {
+	kmsProvider := C.mongocrypt_kms_ctx_get_kms_provider(kc.wrapped, nil)
+	return C.GoString(kmsProvider)
 }
 
 // Message returns the message to send to the KMS.
