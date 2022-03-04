bcf0500256bca72623978438c6dfb9d656686aca
Benjamin Rewis <benji.rewis@mongodb.com>
2022-03-04 14:38:44 -0500

    operation_test typo

--- x/mongo/driver/operation_test.go
+++ x/mongo/driver/operation_test.go
@@ -524,7 +524,7 @@ func TestOperation(t *testing.T) {
 						Kind: tc.server,
 					},
 				}
-				wm, _, err := op.createQueryWireMessage(wm, desc)
+				wm, _, err := op.createQueryWireMessage(context.Background(), wm, desc)
 				noerr(t, err)
 
 				// We know where the $query would be within the OP_QUERY, so we'll just index into there.
