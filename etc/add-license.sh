cb1233e1e5dae8833864e6f544ae1be5ae37558c
Benjamin Rewis <benji.rewis@mongodb.com>
2022-04-05 12:23:20 -0400

    Use variable for year

--- etc/add-license.sh
+++ etc/add-license.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-copyright=$'// Copyright (C) MongoDB, Inc. 2022-present.
+year=$(date +"%Y")
+copyright=$"// Copyright (C) MongoDB, Inc. $year-present.
 //
-// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// Licensed under the Apache License, Version 2.0 (the \"License\"); you may
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-'
+"
 
 add_copyright() {
     # Check if first 24 bytes match first 24 bytes of copyright notice.
