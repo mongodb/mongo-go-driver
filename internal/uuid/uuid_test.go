a0988dde208bf74d291212187768e2354f3258de
Qingyang Hu <qingyang.hu@mongodb.com>
2022-05-20 18:06:14 -0400

    GODRIVER-2361 Touch up test cases

--- internal/uuid/uuid_test.go
+++ internal/uuid/uuid_test.go
@@ -17,7 +17,7 @@ import (
 // Test that initializing many package-global UUID sources concurrently never leads to any duplicate
 // UUIDs being generated.
 func TestGlobalSource(t *testing.T) {
-	t.Run("exp rand 1 UUID x 1,000,000 goroutines", func(t *testing.T) {
+	t.Run("exp rand 1 UUID x 1,000,000 goroutines using a global source", func(t *testing.T) {
 		// Read a UUID from each of 1,000,000 goroutines and assert that there is never a duplicate value.
 		const iterations = 1e6
 		uuids := new(sync.Map)
@@ -35,7 +35,28 @@ func TestGlobalSource(t *testing.T) {
 		}
 		wg.Wait()
 	})
-	t.Run("exp rand 1,000 UUIDs x 1,000 goroutines", func(t *testing.T) {
+	t.Run("exp rand 1 UUID x 1,000,000 goroutines each initializing a new source", func(t *testing.T) {
+		// Read a UUID from each of 1,000,000 goroutines and assert that there is never a duplicate value.
+		// The goal is to emulate many separate Go driver processes starting at the same time and
+		// initializing the uuid package at the same time.
+		const iterations = 1e6
+		uuids := new(sync.Map)
+		var wg sync.WaitGroup
+		wg.Add(iterations)
+		for i := 0; i < iterations; i++ {
+			go func(i int) {
+				defer wg.Done()
+				s := newSource()
+				uuid, err := s.new()
+				require.NoError(t, err, "new() error")
+				_, ok := uuids.Load(uuid)
+				require.Falsef(t, ok, "New returned a duplicate UUID on iteration %d: %v", i, uuid)
+				uuids.Store(uuid, true)
+			}(i)
+		}
+		wg.Wait()
+	})
+	t.Run("exp rand 1,000 UUIDs x 1,000 goroutines each initializing a new source", func(t *testing.T) {
 		// Read 1,000 UUIDs from each goroutine and assert that there is never a duplicate value, either
 		// from the same goroutine or from separate goroutines.
 		const iterations = 1000
@@ -45,8 +66,9 @@ func TestGlobalSource(t *testing.T) {
 		for i := 0; i < iterations; i++ {
 			go func(i int) {
 				defer wg.Done()
+				s := newSource()
 				for j := 0; j < iterations; j++ {
-					uuid, err := New()
+					uuid, err := s.new()
 					require.NoError(t, err, "new() error")
 					_, ok := uuids.Load(uuid)
 					require.Falsef(t, ok, "goroutine %d returned a duplicate UUID on iteration %d: %v", i, j, uuid)
