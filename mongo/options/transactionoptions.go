f5ba6e55b251997c0fd6586465e7f5bb184da03f
Benjamin Rewis <benji.rewis@mongodb.com>
2022-07-18 16:43:33 -0400

    Change timeout deprecations to NOTEs

--- mongo/options/transactionoptions.go
+++ mongo/options/transactionoptions.go
@@ -35,9 +35,9 @@ type TransactionOptions struct {
 	// server. The default value is nil, which means that the default maximum commit time of the session used to
 	// start the transaction will be used.
 	//
-	// Deprecated: This option is deprecated and will eventually be removed in version 2.0 of the driver. The more
-	// general Timeout option should be used in its place to control the amount of time that the transaction can run
-	// before returning an error. MaxCommitTime is still usable through the deprecated setter.
+	// NOTE(benjirewis): The use of MaxCommitTime is discouraged. The more general Timeout option be used in its
+	// place to control the amount of time that a single operation can run before returning an error. MaxCommitTime
+	// is ignored if Timeout is set on the client.
 	MaxCommitTime *time.Duration
 }
 
@@ -66,9 +66,9 @@ func (t *TransactionOptions) SetWriteConcern(wc *writeconcern.WriteConcern) *Tra
 
 // SetMaxCommitTime sets the value for the MaxCommitTime field.
 //
-// Deprecated: This option is deprecated and will eventually be removed in version 2.0 of the driver.
-// The more general Timeout option should be used in its place to control the amount of time that the
-// transaction can run before returning an error.
+// NOTE(benjirewis): The use of MaxCommitTime is discouraged. The more general Timeout option should
+// be used in its place to control the amount of time that a single operation can run before
+// returning an error. MaxCommitTime is ignored if Timeout is set on the client.
 func (t *TransactionOptions) SetMaxCommitTime(mct *time.Duration) *TransactionOptions {
 	t.MaxCommitTime = mct
 	return t
