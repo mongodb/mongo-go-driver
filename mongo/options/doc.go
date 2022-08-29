40b326455bf8977fce6bc15b8b079900b857f8ff
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2022-08-29 09:55:37 -0700

    Add real description for the 'options' package.

--- mongo/options/doc.go
+++ mongo/options/doc.go
@@ -4,5 +4,5 @@
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-// Package options asdfasdfa.
+// Package options defines the optional configurations for the MongoDB Go Driver.
 package options
