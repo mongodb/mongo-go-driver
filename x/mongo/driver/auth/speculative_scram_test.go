8c3cec2eeb12ae92cd7d84edd8e37c7e82f36980
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-06 12:24:09 -0600

    GODRIVER-2099 remove hard-coded wire

--- x/mongo/driver/auth/speculative_scram_test.go
+++ x/mongo/driver/auth/speculative_scram_test.go
@@ -15,6 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/internal"
 	"go.mongodb.org/mongo-driver/internal/testutil/assert"
 	"go.mongodb.org/mongo-driver/mongo/address"
+	"go.mongodb.org/mongo-driver/version"
 	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/drivertest"
 )
@@ -27,7 +28,7 @@ var (
 		bsoncore.AppendInt32Element(nil, "maxBsonObjectSize", 16777216),
 		bsoncore.AppendInt32Element(nil, "maxMessageSizeBytes", 48000000),
 		bsoncore.AppendInt32Element(nil, "minWireVersion", 0),
-		bsoncore.AppendInt32Element(nil, "maxWireVersion", 6),
+		bsoncore.AppendInt32Element(nil, "maxWireVersion", version.MinSupportedWire),
 	}
 	// The first payload sent by the driver for SCRAM-SHA-1/256 authentication.
 	firstScramSha1ClientPayload   = []byte("n,,n=user,r=fyko+d2lbbFgONRv9qkxdawL")
