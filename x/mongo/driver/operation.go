5fcb147028929da63ecddb2ff029b68f390ce1b0
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-10-10 10:15:49 -0600

    GODRIVER-2539 Remove MinWireVersion < 6 Logic (#1084)

--- x/mongo/driver/operation.go
+++ x/mongo/driver/operation.go
@@ -505,24 +505,6 @@ func (op Operation) Execute(ctx context.Context) error {
 		}
 
 		desc := description.SelectedServer{Server: conn.Description(), Kind: op.Deployment.Kind()}
-		if desc.WireVersion == nil || desc.WireVersion.Max < 4 {
-			switch op.Legacy {
-			case LegacyFind:
-				return op.legacyFind(ctx, (*wm)[:0], srvr, conn, desc, maxTimeMS)
-			case LegacyGetMore:
-				return op.legacyGetMore(ctx, (*wm)[:0], srvr, conn, desc)
-			case LegacyKillCursors:
-				return op.legacyKillCursors(ctx, (*wm)[:0], srvr, conn, desc)
-			}
-		}
-		if desc.WireVersion == nil || desc.WireVersion.Max < 3 {
-			switch op.Legacy {
-			case LegacyListCollections:
-				return op.legacyListCollections(ctx, (*wm)[:0], srvr, conn, desc)
-			case LegacyListIndexes:
-				return op.legacyListIndexes(ctx, (*wm)[:0], srvr, conn, desc, maxTimeMS)
-			}
-		}
 
 		if batching {
 			targetBatchSize := desc.MaxDocumentSize
@@ -830,7 +812,6 @@ func (op Operation) retryable(desc description.Server) bool {
 			return true
 		}
 		if retryWritesSupported(desc) &&
-			desc.WireVersion != nil && desc.WireVersion.Max >= 6 &&
 			op.Client != nil && !(op.Client.TransactionInProgress() || op.Client.TransactionStarting()) &&
 			writeconcern.AckWrite(op.WriteConcern) {
 			return true
@@ -839,8 +820,7 @@ func (op Operation) retryable(desc description.Server) bool {
 		if op.Client != nil && (op.Client.Committing || op.Client.Aborting) {
 			return true
 		}
-		if desc.WireVersion != nil && desc.WireVersion.Max >= 6 &&
-			(op.Client == nil || !(op.Client.TransactionInProgress() || op.Client.TransactionStarting())) {
+		if op.Client == nil || !(op.Client.TransactionInProgress() || op.Client.TransactionStarting()) {
 			return true
 		}
 	}
