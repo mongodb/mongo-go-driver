861364b2ec885826130592c239524bdd09af5026
Qingyang Hu <103950869+qingyang-hu@users.noreply.github.com>
2022-10-28 18:03:12 -0400

    GODRIVER-2620 Fix hostname parsing for SRV polling. (#1112)
    * GODRIVER-2620 Fix hostname parsing for SRV polling.
    
    Co-authored-by: Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
    Co-authored-by: Kevin Albertson <kevin.albertson@10gen.com>

--- x/mongo/driver/topology/topology.go
+++ x/mongo/driver/topology/topology.go
@@ -14,6 +14,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net"
+	"net/url"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -230,7 +232,21 @@ func (t *Topology) Connect() error {
 
 	t.serversLock.Unlock()
 	if t.pollingRequired {
-		go t.pollSRVRecords()
+		uri, err := url.Parse(t.cfg.uri)
+		if err != nil {
+			return err
+		}
+		// sanity check before passing the hostname to resolver
+		if parsedHosts := strings.Split(uri.Host, ","); len(parsedHosts) != 1 {
+			return fmt.Errorf("URI with SRV must include one and only one hostname")
+		}
+		_, _, err = net.SplitHostPort(uri.Host)
+		if err == nil {
+			// we were able to successfully extract a port from the host,
+			// but should not be able to when using SRV
+			return fmt.Errorf("URI with srv must not include a port number")
+		}
+		go t.pollSRVRecords(uri.Host)
 		t.pollingwg.Add(1)
 	}
 
@@ -552,7 +568,7 @@ func (t *Topology) selectServerFromDescription(desc description.Topology,
 	return suitable, nil
 }
 
-func (t *Topology) pollSRVRecords() {
+func (t *Topology) pollSRVRecords(hosts string) {
 	defer t.pollingwg.Done()
 
 	serverConfig := newServerConfig(t.cfg.serverOpts...)
@@ -568,13 +584,6 @@ func (t *Topology) pollSRVRecords() {
 			<-t.pollingDone
 		}
 	}()
-
-	// remove the scheme
-	uri := t.cfg.uri[14:]
-	hosts := uri
-	if idx := strings.IndexAny(uri, "/?@"); idx != -1 {
-		hosts = uri[:idx]
-	}
 
 	for {
 		select {
