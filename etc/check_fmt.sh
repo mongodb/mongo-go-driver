0e5ae89f1baf3d6edca093467ffd1f870b441add
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2022-11-28 16:11:31 -0500

    GODRIVER-2661 Fix make targets that still refer to PKGS. (#1141)

--- etc/check_fmt.sh
+++ etc/check_fmt.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# check_fmt gopackages...
-# Runs gofmt on given packages and checks that *_example_test.go files have wrapped lines.
+# check_fmt
+# Runs go fmt on all packages in the repo and checks that *_example_test.go files have wrapped lines.
 
-gofmt_out="$(gofmt -l -s "$@")"
+gofmt_out="$(go fmt ./...)"
 
 if [[ $gofmt_out ]]; then
-  echo "gofmt check failed for:";
+  echo "go fmt check failed for:";
   sed -e 's/^/ - /' <<< "$gofmt_out";
   exit 1;
 fi
@@ -16,7 +16,7 @@ fi
 # E.g ignored lines:
 #     // "mongodb://ldap-user:ldap-pwd@localhost:27017/?authMechanism=PLAIN"
 #     // (https://www.mongodb.com/docs/manual/core/authentication-mechanisms-enterprise/#security-auth-ldap).
-lll_out="$(find "$@" -type f -name "*_examples_test.go" | lll -w 4 -l 80 -e '^\s*\/\/.+:\/\/' --files)"
+lll_out="$(find . -type f -name "*_examples_test.go" | lll -w 4 -l 80 -e '^\s*\/\/.+:\/\/' --files)"
 
 if [[ $lll_out ]]; then
   echo "lll check failed for:";
