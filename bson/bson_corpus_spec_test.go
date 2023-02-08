a57559b2839cc944068e3ee01cff3b682c4a0a21
manunio <max.manu.nair@gmail.com>
2023-02-09 03:13:15 +0530

    Changes per feedback

--- bson/bson_corpus_spec_test.go
+++ bson/bson_corpus_spec_test.go
@@ -26,11 +26,62 @@ import (
 	"go.mongodb.org/mongo-driver/internal/require"
 )
 
+type testCase struct {
+	Description  string                `json:"description"`
+	BsonType     string                `json:"bson_type"`
+	TestKey      *string               `json:"test_key"`
+	Valid        []validityTestCase    `json:"valid"`
+	DecodeErrors []decodeErrorTestCase `json:"decodeErrors"`
+	ParseErrors  []parseErrorTestCase  `json:"parseErrors"`
+	Deprecated   *bool                 `json:"deprecated"`
+}
+
+type validityTestCase struct {
+	Description       string  `json:"description"`
+	CanonicalBson     string  `json:"canonical_bson"`
+	CanonicalExtJSON  string  `json:"canonical_extjson"`
+	RelaxedExtJSON    *string `json:"relaxed_extjson"`
+	DegenerateBSON    *string `json:"degenerate_bson"`
+	DegenerateExtJSON *string `json:"degenerate_extjson"`
+	ConvertedBSON     *string `json:"converted_bson"`
+	ConvertedExtJSON  *string `json:"converted_extjson"`
+	Lossy             *bool   `json:"lossy"`
+}
+
+type decodeErrorTestCase struct {
+	Description string `json:"description"`
+	Bson        string `json:"bson"`
+}
+
+type parseErrorTestCase struct {
+	Description string `json:"description"`
+	String      string `json:"string"`
+}
+
 const dataDir = "../testdata/bson-corpus/"
+
+func findJSONFilesInDir(dir string) ([]string, error) {
+	files := make([]string, 0)
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() || path.Ext(entry.Name()) != ".json" {
+			continue
+		}
+
+		files = append(files, entry.Name())
+	}
+
+	return files, nil
+}
 
 // seedExtJSON will add the byte representation of the "extJSON" string to the fuzzer's coprus.
 func seedExtJSON(f *testing.F, extJSON string, extJSONType string, desc string) {
-	jbytes, err := JsonToBytes(extJSON, extJSONType, desc)
+	jbytes, err := jsonToBytes(extJSON, extJSONType, desc)
 	if err != nil {
 		f.Fatalf("failed to convert JSON to bytes: %v", err)
 	}
@@ -40,7 +91,7 @@ func seedExtJSON(f *testing.F, extJSON string, extJSONType string, desc string) 
 
 // seedTestCase will add the byte representation for each "extJSON" string of each valid test case to the fuzzer's
 // corpus.
-func seedTestCase(f *testing.F, tcase *TestCase) {
+func seedTestCase(f *testing.F, tcase *testCase) {
 	for _, vtc := range tcase.Valid {
 		seedExtJSON(f, vtc.CanonicalExtJSON, "canonical", vtc.Description)
 
@@ -64,7 +115,7 @@ func seedTestCase(f *testing.F, tcase *TestCase) {
 // seedBSONCorpus will unmarshal the data from "testdata/bson-corpus" into a slice of "testCase" structs and then
 // marshal the "*_extjson" field of each "validityTestCase" into a slice of bytes to seed the fuzz corpus.
 func seedBSONCorpus(f *testing.F) {
-	fileNames, err := FindJSONFilesInDir(dataDir)
+	fileNames, err := findJSONFilesInDir(dataDir)
 	if err != nil {
 		f.Fatalf("failed to find JSON files in directory %q: %v", dataDir, err)
 	}
@@ -77,7 +128,7 @@ func seedBSONCorpus(f *testing.F) {
 			f.Fatalf("failed to open file %q: %v", filePath, err)
 		}
 
-		var tcase TestCase
+		var tcase testCase
 		if err := json.NewDecoder(file).Decode(&tcase); err != nil {
 			f.Fatal(err)
 		}
@@ -204,6 +255,30 @@ func nativeToBSON(t *testing.T, cB []byte, doc D, testDesc, bType, docSrcDesc st
 	}
 }
 
+// jsonToNative decodes the extended JSON string (ej) into a native Document
+func jsonToNative(ej, ejType, testDesc string) (D, error) {
+	var doc D
+	if err := UnmarshalExtJSON([]byte(ej), ejType != "relaxed", &doc); err != nil {
+		return nil, fmt.Errorf("%s: decoding %s extended JSON: %w", testDesc, ejType, err)
+	}
+	return doc, nil
+}
+
+// jsonToBytes decodes the extended JSON string (ej) into canonical BSON and then encodes it into a byte slice.
+func jsonToBytes(ej, ejType, testDesc string) ([]byte, error) {
+	native, err := jsonToNative(ej, ejType, testDesc)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := Marshal(native)
+	if err != nil {
+		return nil, fmt.Errorf("%s: encoding %s BSON: %w", testDesc, ejType, err)
+	}
+
+	return b, nil
+}
+
 // nativeToJSON encodes the native Document (doc) into an extended JSON string
 func nativeToJSON(t *testing.T, ej string, doc D, testDesc, ejType, ejShortName, docSrcDesc string) {
 	actualEJ, err := MarshalExtJSON(doc, ejType != "relaxed", true)
@@ -226,7 +301,7 @@ func runTest(t *testing.T, file string) {
 	testName := "bson_corpus--" + file
 
 	t.Run(testName, func(t *testing.T) {
-		var test TestCase
+		var test testCase
 		require.NoError(t, json.Unmarshal(content, &test))
 
 		t.Run("valid", func(t *testing.T) {
@@ -376,7 +451,7 @@ func runTest(t *testing.T, file string) {
 }
 
 func Test_BsonCorpus(t *testing.T) {
-	jsonFiles, err := FindJSONFilesInDir(dataDir)
+	jsonFiles, err := findJSONFilesInDir(dataDir)
 	if err != nil {
 		t.Fatalf("error finding JSON files in %s: %v", dataDir, err)
 	}
--- cmd/build-oss-fuzz-corpus/main.go
+++ cmd/build-oss-fuzz-corpus/main.go
@@ -9,43 +9,116 @@ package main
 import (
 	"archive/zip"
 	"crypto/sha1"
-	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"path"
-	"strings"
+	"path/filepath"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 const dataDir = "testdata/bson-corpus/"
 
+type testCase struct {
+	Description  string                `json:"description"`
+	BsonType     string                `json:"bson_type"`
+	TestKey      *string               `json:"test_key"`
+	Valid        []validityTestCase    `json:"valid"`
+	DecodeErrors []decodeErrorTestCase `json:"decodeErrors"`
+	ParseErrors  []parseErrorTestCase  `json:"parseErrors"`
+	Deprecated   *bool                 `json:"deprecated"`
+}
+
+type validityTestCase struct {
+	Description       string  `json:"description"`
+	CanonicalBson     string  `json:"canonical_bson"`
+	CanonicalExtJSON  string  `json:"canonical_extjson"`
+	RelaxedExtJSON    *string `json:"relaxed_extjson"`
+	DegenerateBSON    *string `json:"degenerate_bson"`
+	DegenerateExtJSON *string `json:"degenerate_extjson"`
+	ConvertedBSON     *string `json:"converted_bson"`
+	ConvertedExtJSON  *string `json:"converted_extjson"`
+	Lossy             *bool   `json:"lossy"`
+}
+
+type decodeErrorTestCase struct {
+	Description string `json:"description"`
+	Bson        string `json:"bson"`
+}
+
+type parseErrorTestCase struct {
+	Description string `json:"description"`
+	String      string `json:"string"`
+}
+
+func findJSONFilesInDir(dir string) ([]string, error) {
+	files := make([]string, 0)
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() || path.Ext(entry.Name()) != ".json" {
+			continue
+		}
+
+		files = append(files, entry.Name())
+	}
+
+	return files, nil
+}
+
+// jsonToNative decodes the extended JSON string (ej) into a native Document
+func jsonToNative(ej, ejType, testDesc string) (bson.D, error) {
+	var doc bson.D
+	if err := bson.UnmarshalExtJSON([]byte(ej), ejType != "relaxed", &doc); err != nil {
+		return nil, fmt.Errorf("%s: decoding %s extended JSON: %w", testDesc, ejType, err)
+	}
+	return doc, nil
+}
+
+// jsonToBytes decodes the extended JSON string (ej) into canonical BSON and then encodes it into a byte slice.
+func jsonToBytes(ej, ejType, testDesc string) ([]byte, error) {
+	native, err := jsonToNative(ej, ejType, testDesc)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := bson.Marshal(native)
+	if err != nil {
+		return nil, fmt.Errorf("%s: encoding %s BSON: %w", testDesc, ejType, err)
+	}
+
+	return b, nil
+}
+
 // seedExtJSON will add the byte representation of the "extJSON" string to the fuzzer's coprus.
 func seedExtJSON(zw *zip.Writer, extJSON string, extJSONType string, desc string) {
-	jbytes, err := bson.JsonToBytes(extJSON, extJSONType, desc)
+	jbytes, err := jsonToBytes(extJSON, extJSONType, desc)
 	if err != nil {
 		log.Fatalf("failed to convert JSON to bytes: %v", err)
 	}
 
-	hash := sha1.New()
-	hash.Write(jbytes)
-	file_in_zip := hex.EncodeToString(hash.Sum(nil))
+	zipFile := fmt.Sprintf("%x", sha1.Sum(jbytes))
 
-	f, err := zw.Create(file_in_zip)
+	f, err := zw.Create(zipFile)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error creating zip file: %v", err)
 	}
 
 	_, err = f.Write(jbytes)
 	if err != nil {
-		log.Fatalf("Failed to write file: %s into zip file", file_in_zip)
+		log.Fatalf("failed to write file: %s into zip file: %v", zipFile, err)
 	}
 }
 
 // seedTestCase will add the byte representation for each "extJSON" string of each valid test case to the fuzzer's
 // corpus.
-func seedTestCase(zw *zip.Writer, tcase *bson.TestCase) {
+func seedTestCase(zw *zip.Writer, tcase *testCase) {
 	for _, vtc := range tcase.Valid {
 		seedExtJSON(zw, vtc.CanonicalExtJSON, "canonical", vtc.Description)
 
@@ -69,7 +142,7 @@ func seedTestCase(zw *zip.Writer, tcase *bson.TestCase) {
 // seedBSONCorpus will unmarshal the data from "testdata/bson-corpus" into a slice of "testCase" structs and then
 // marshal the "*_extjson" field of each "validityTestCase" into a slice of bytes to seed the fuzz corpus.
 func seedBSONCorpus(zw *zip.Writer) {
-	fileNames, err := bson.FindJSONFilesInDir(dataDir)
+	fileNames, err := findJSONFilesInDir(dataDir)
 	if err != nil {
 		log.Fatalf("failed to find JSON files in directory %q: %v", dataDir, err)
 	}
@@ -82,40 +155,39 @@ func seedBSONCorpus(zw *zip.Writer) {
 			log.Fatalf("failed to open file %q: %v", filePath, err)
 		}
 
-		var tcase bson.TestCase
+		var tcase testCase
 		if err := json.NewDecoder(file).Decode(&tcase); err != nil {
-			log.Fatal(err)
+			log.Fatalf("failed to decode file %q: %v", filePath, err)
 		}
 
 		seedTestCase(zw, &tcase)
 	}
 }
 
-// This cmd generates and adds slice of bytes to files with sha1 hash name, and zip them as a <fuzzer_name>_seed_corpus.zip,
-// which is used later by oss-fuzz as seed corpus, This is done because as of now oss-fuzz does not support go native t.Add()
-// method.
+// main packs the local corpus as <fuzzer_name>_seed_corpus.zip, which is used by OSS-Fuzz to seed remote fuzzing
+// of the MongoDB Go Driver. See here for more details: https://google.github.io/oss-fuzz/architecture/
 func main() {
-	seed_corpus := os.Args[1]
-	if !strings.HasSuffix(seed_corpus, ".zip") {
-		log.Fatalln("Expected command line:", os.Args[0], "<seed_corpus>.zip")
+	seedCorpus := os.Args[1]
+	if filepath.Ext(seedCorpus) != ".zip" {
+		log.Fatalf("expected zip file <corpus>.zip, got %s", seedCorpus)
 	}
 
-	zip_file, err := os.Create(seed_corpus)
+	zipFile, err := os.Create(seedCorpus)
 	if err != nil {
-		log.Fatalf("Failed creating file: %s", err)
+		log.Fatalf("failed creating zip file: %v", err)
 	}
 
 	defer func() {
-		err := zip_file.Close()
+		err := zipFile.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("failed to close zip file: %v", err)
 		}
 	}()
 
-	zip_writer := zip.NewWriter(zip_file)
-	seedBSONCorpus(zip_writer)
+	zipWriter := zip.NewWriter(zipFile)
+	seedBSONCorpus(zipWriter)
 
-	if err := zip_writer.Close(); err != nil {
-		log.Fatal(err)
+	if err := zipWriter.Close(); err != nil {
+		log.Fatalf("failed to close zip writer: %v", err)
 	}
 }
