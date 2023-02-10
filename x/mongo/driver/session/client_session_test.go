0e1d438223bbf5e67d86b3bf74fd8b605c7465ed
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2023-02-09 16:25:47 -0800

    Fix formatting

--- x/mongo/driver/session/client_session_test.go
+++ x/mongo/driver/session/client_session_test.go
@@ -286,11 +286,11 @@ func TestClientSession(t *testing.T) {
 }
 
 func TestImplicitClientSession(t *testing.T) {
-        t.Parallel()
-        
+	t.Parallel()
+
 	t.Run("causal consistency is false", func(t *testing.T) {
-	    t.Parallel()
-	    
+		t.Parallel()
+
 		id, err := uuid.New()
 		require.NoError(t, err)
 
