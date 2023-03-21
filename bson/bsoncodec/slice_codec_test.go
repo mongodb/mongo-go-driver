431006a254284cbb3b463f929a72fa35185412ec
pvragov <pvragov@ptsecurity.com>
2023-03-21 14:49:17 +0700

    Add slice codec tests

--- bson/bsoncodec/slice_codec_test.go
+++ bson/bsoncodec/slice_codec_test.go
@@ -5,19 +5,22 @@ import (
 	"strings"
 	"testing"
 
-	"go.mongodb.org/mongo-driver/bson/bsonrw"
+	"go.mongodb.org/mongo-driver/bson/bsonrw/bsonrwtest"
 	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/internal/assert"
+	"go.mongodb.org/mongo-driver/internal/require"
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
 )
 
 func BenchmarkSliceCodec_EncodeValue(b *testing.B) {
 	codec := NewSliceCodec()
 	eContext := EncodeContext{}
-	vw := &valueWriterMock{}
 	dataToEncode := []byte(strings.Repeat("t", 4096))
 	val := reflect.ValueOf(&dataToEncode).Elem()
 
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
+		vw := &bsonrwtest.ValueReaderWriter{}
 		err := codec.EncodeValue(eContext, vw, val)
 		if err != nil {
 			b.Fatal(err)
@@ -29,12 +32,17 @@ func BenchmarkSliceCodec_EncodeValue(b *testing.B) {
 func BenchmarkSliceCodec_DecodeValue(b *testing.B) {
 	codec := NewSliceCodec()
 	dContext := DecodeContext{}
-	vr := &valueReaderMock{}
+	dataToDecode := bsoncore.AppendBinary(nil, bsontype.BinaryGeneric, []byte(strings.Repeat("t", 4096)))
+
 	var buf []byte
 	val := reflect.ValueOf(&buf).Elem()
 
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
+		vr := &bsonrwtest.ValueReaderWriter{BSONType: bsontype.Binary, Return: bsoncore.Value{
+			Type: bsontype.Binary,
+			Data: dataToDecode,
+		}}
 		err := codec.DecodeValue(dContext, vr, val)
 		if err != nil {
 			b.Fatal(err)
@@ -43,24 +51,34 @@ func BenchmarkSliceCodec_DecodeValue(b *testing.B) {
 	b.ReportAllocs()
 }
 
-type valueWriterMock struct {
-	bsonrw.ValueWriter
-}
+func TestSliceCodec_DecodeValue(t *testing.T) {
+	t.Parallel()
+	codec := NewSliceCodec()
 
-func (m *valueWriterMock) WriteBinary(_ []byte) error {
-	return nil
-}
+	t.Run("decode binary", func(t *testing.T) {
+		expected := []byte("test bytes")
+		vr := &bsonrwtest.ValueReaderWriter{BSONType: bsontype.Binary, Return: bsoncore.Value{
+			Type: bsontype.Binary,
+			Data: bsoncore.AppendBinary(nil, bsontype.BinaryGeneric, expected),
+		}}
 
-type valueReaderMock struct {
-	bsonrw.ValueReader
-}
+		actual := []byte("dummy")
+		val := reflect.ValueOf(&actual).Elem()
+		err := codec.DecodeValue(DecodeContext{}, vr, val)
+		require.NoError(t, err)
 
-func (m *valueReaderMock) Type() bsontype.Type {
-	return bsontype.Binary
-}
+		assert.Equal(t, expected, actual)
+	})
 
-var dataToDecode = []byte(strings.Repeat("t", 4096))
+	t.Run("decode string", func(t *testing.T) {
+		expected := "test string"
+		vr := &bsonrwtest.ValueReaderWriter{BSONType: bsontype.String, Return: expected}
 
-func (m *valueReaderMock) ReadBinary() (b []byte, btype byte, err error) {
-	return dataToDecode, bsontype.BinaryGeneric, nil
+		actual := []byte("dummy")
+		val := reflect.ValueOf(&actual).Elem()
+		err := codec.DecodeValue(DecodeContext{}, vr, val)
+		require.NoError(t, err)
+
+		assert.Equal(t, expected, string(actual))
+	})
 }
