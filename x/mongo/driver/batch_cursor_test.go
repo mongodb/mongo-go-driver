8d7f05ff302ffbdd0d82836d51bbd6d53dfe908d
Preston Vasquez <prestonvasquez@icloud.com>
2023-03-23 15:28:46 -0600

    GODRIVER-1605 Add SetBatchSize to mongo.Cursor (#1201)
    Co-authored-by: Matt Dale <9760375+matthewdale@users.noreply.github.com>

--- x/mongo/driver/batch_cursor_test.go
+++ x/mongo/driver/batch_cursor_test.go
@@ -13,7 +13,11 @@ import (
 )
 
 func TestBatchCursor(t *testing.T) {
+	t.Parallel()
+
 	t.Run("setBatchSize", func(t *testing.T) {
+		t.Parallel()
+
 		var size int32
 		bc := &BatchCursor{
 			batchSize: size,
@@ -23,5 +27,66 @@ func TestBatchCursor(t *testing.T) {
 		size = int32(4)
 		bc.SetBatchSize(size)
 		assert.Equal(t, size, bc.batchSize, "expected batchSize %v, got %v", size, bc.batchSize)
+	})
+
+	t.Run("calcGetMoreBatchSize", func(t *testing.T) {
+		t.Parallel()
+
+		for _, tcase := range []struct {
+			name                               string
+			size, limit, numReturned, expected int32
+			ok                                 bool
+		}{
+			{
+				name:     "empty",
+				expected: 0,
+				ok:       true,
+			},
+			{
+				name:     "batchSize NEQ 0",
+				size:     4,
+				expected: 4,
+				ok:       true,
+			},
+			{
+				name:     "limit NEQ 0",
+				limit:    4,
+				expected: 0,
+				ok:       true,
+			},
+			{
+				name:        "limit NEQ and batchSize + numReturned EQ limit",
+				size:        4,
+				limit:       8,
+				numReturned: 4,
+				expected:    4,
+				ok:          true,
+			},
+			{
+				name:        "limit makes batchSize negative",
+				numReturned: 4,
+				limit:       2,
+				expected:    -2,
+				ok:          false,
+			},
+		} {
+			tcase := tcase
+			t.Run(tcase.name, func(t *testing.T) {
+				t.Parallel()
+
+				bc := &BatchCursor{
+					limit:       tcase.limit,
+					batchSize:   tcase.size,
+					numReturned: tcase.numReturned,
+				}
+
+				bc.SetBatchSize(tcase.size)
+
+				size, ok := calcGetMoreBatchSize(*bc)
+
+				assert.Equal(t, tcase.expected, size, "expected batchSize %v, got %v", tcase.expected, size)
+				assert.Equal(t, tcase.ok, ok, "expected ok %v, got %v", tcase.ok, ok)
+			})
+		}
 	})
 }
