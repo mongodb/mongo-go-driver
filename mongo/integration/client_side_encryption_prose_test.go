3a75a42f6c514cddeea94705af9274cbd7c9682f
Kevin Albertson <kevin.albertson@mongodb.com>
2023-03-29 13:46:58 +0000

    skip QE prose tests on serverless

--- mongo/integration/client_side_encryption_prose_test.go
+++ mongo/integration/client_side_encryption_prose_test.go
@@ -1563,6 +1563,10 @@ func TestClientSideEncryptionProse(t *testing.T) {
 	// Only test MongoDB Server 7.0+. MongoDB Server 7.0 introduced a backwards breaking change to the Queryable Encryption (QE) protocol: QEv2.
 	// libmongocrypt is configured to use the QEv2 protocol.
 	mt.RunOpts("12. explicit encryption", runOpts, func(mt *mtest.T) {
+		if mtest.Serverless() {
+			// Skip tests if running against serverless, as capped collections are banned.
+			mt.Skip("Queryable Encryption tests are skipped on serverless until QEv2 protocol is enabled on serverless by default: DRIVERS-2589")
+		}
 		// Test Setup ... begin
 		encryptedFields := readJSONFile(mt, "encrypted-fields.json")
 		key1Document := readJSONFile(mt, "key1-document.json")
@@ -2066,6 +2070,10 @@ func TestClientSideEncryptionProse(t *testing.T) {
 	// Only test MongoDB Server 7.0+. MongoDB Server 7.0 introduced a backwards breaking change to the Queryable Encryption (QE) protocol: QEv2.
 	// libmongocrypt is configured to use the QEv2 protocol.
 	mt.RunOpts("21. automatic data encryption keys", autoKeyRunOpts, func(mt *mtest.T) {
+		if mtest.Serverless() {
+			// Skip tests if running against serverless, as capped collections are banned.
+			mt.Skip("Queryable Encryption tests are skipped on serverless until QEv2 protocol is enabled on serverless by default: DRIVERS-2589")
+		}
 		setup := func() (*mongo.Client, *mongo.ClientEncryption, error) {
 			opts := options.Client().ApplyURI(mtest.ClusterURI())
 			client, err := mongo.Connect(context.Background(), opts)
@@ -2228,6 +2236,10 @@ func TestClientSideEncryptionProse(t *testing.T) {
 	// Only test MongoDB Server 7.0+. MongoDB Server 7.0 introduced a backwards breaking change to the Queryable Encryption (QE) protocol: QEv2.
 	// libmongocrypt is configured to use the QEv2 protocol.
 	mt.RunOpts("22. range explicit encryption", rangeRunOpts, func(mt *mtest.T) {
+		if mtest.Serverless() {
+			// Skip tests if running against serverless, as capped collections are banned.
+			mt.Skip("Queryable Encryption tests are skipped on serverless until QEv2 protocol is enabled on serverless by default: DRIVERS-2589")
+		}
 		type testcase struct {
 			typeStr       string
 			field         string
