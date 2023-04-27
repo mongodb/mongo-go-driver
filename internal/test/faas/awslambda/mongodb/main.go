298dbd776f1d790557cfb9d7b2e72bebfafb36aa
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2023-04-27 14:12:14 -0600

    GODRIVER-2575 remove example aws lambda test

--- internal/test/faas/awslambda/mongodb/main.go
+++ internal/test/faas/awslambda/mongodb/main.go
@@ -4,8 +4,6 @@
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-//go:build faas
-
 package main
 
 import (
