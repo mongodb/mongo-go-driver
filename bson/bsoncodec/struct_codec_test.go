54a3206c25f4b625c43aedbed95d83123d8bb7f7
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2023-05-05 11:58:58 -0700

    GODRIVER-2716 Introduce a new BSON Encoder and Decoder configuration API to replace the bsoncodec configurations. (#1229)
    Co-authored-by: Preston Vasquez <prestonvasquez@icloud.com>
    Co-authored-by: Kevin Albertson <kevin.albertson@10gen.com>

--- bson/bsoncodec/struct_codec_test.go
+++ bson/bsoncodec/struct_codec_test.go
@@ -13,35 +13,142 @@ import (
 	"go.mongodb.org/mongo-driver/internal/assert"
 )
 
-func TestZeoerInterfaceUsedByDecoder(t *testing.T) {
-	enc := &StructCodec{}
+var _ Zeroer = zeroer{}
 
-	// cases that are zero, because they are known types or pointers
-	var st *nonZeroer
-	assert.True(t, enc.isZero(st))
-	assert.True(t, enc.isZero(0))
-	assert.True(t, enc.isZero(false))
+type zeroer struct {
+	val int
+}
 
-	// cases that shouldn't be zero
-	st = &nonZeroer{value: false}
-	assert.False(t, enc.isZero(struct{ val bool }{val: true}))
-	assert.False(t, enc.isZero(struct{ val bool }{val: false}))
-	assert.False(t, enc.isZero(st))
-	st.value = true
-	assert.False(t, enc.isZero(st))
+func (z zeroer) IsZero() bool {
+	return z.val != 0
+}
 
-	// a test to see if the interface impacts the outcome
-	z := zeroTest{}
-	assert.False(t, enc.isZero(z))
+func TestIsZero(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		description    string
+		value          interface{}
+		omitZeroStruct bool
+		want           bool
+	}{
+		{
+			description: "false",
+			value:       false,
+			want:        true,
+		},
+		{
+			description: "0",
+			value:       0,
+			want:        true,
+		},
+		{
+			description: "nil pointer to int",
+			value:       (*int)(nil),
+			want:        true,
+		},
+		{
+			description: "time.Time",
+			value:       time.Unix(1682123781, 0),
+			want:        false,
+		},
+		{
+			description: "empty time.Time",
+			value:       time.Time{},
+			want:        true,
+		},
+		{
+			description: "nil pointer to time.Time",
+			value:       (*time.Time)(nil),
+			want:        true,
+		},
+		{
+			description: "zero struct",
+			value:       struct{ Val bool }{},
+			want:        false,
+		},
+		{
+			description: "non-zero struct",
+			value:       struct{ Val bool }{Val: true},
+			want:        false,
+		},
+		{
+			description: "nil pointer to struct",
+			value:       (*struct{ Val bool })(nil),
+			want:        true,
+		},
+		{
+			description: "pointer to struct",
+			value:       &struct{ Val bool }{},
+			want:        false,
+		},
+		{
+			description: "zero struct that implements Zeroer",
+			value:       zeroer{},
+			want:        false,
+		},
+		{
+			description: "non-zero struct that implements Zeroer",
+			value:       &zeroer{val: 1},
+			want:        true,
+		},
+		{
+			description: "pointer to zero struct that implements Zeroer",
+			value:       &zeroer{},
+			want:        false,
+		},
+		{
+			description: "pointer to non-zero struct that implements Zeroer",
+			value:       zeroer{val: 1},
+			want:        true,
+		},
+		{
+			description:    "zero struct with omitZeroStruct",
+			value:          struct{ Val bool }{},
+			omitZeroStruct: true,
+			want:           true,
+		},
+		{
+			description:    "non-zero struct with omitZeroStruct",
+			value:          struct{ Val bool }{Val: true},
+			omitZeroStruct: true,
+			want:           false,
+		},
+		{
+			description:    "zero struct with only private fields omitZeroStruct",
+			value:          struct{ val bool }{},
+			omitZeroStruct: true,
+			want:           true,
+		},
+		// TODO(GODRIVER-2820): Change the expected value to "false" once the logic is updated to
+		// TODO also inspect private struct fields.
+		{
+			description:    "non-zero struct with only private fields with omitZeroStruct",
+			value:          struct{ val bool }{val: true},
+			omitZeroStruct: true,
+			want:           true,
+		},
+		{
+			description:    "pointer to zero struct with omitZeroStruct",
+			value:          &struct{ Val bool }{},
+			omitZeroStruct: true,
+			want:           false,
+		},
+		{
+			description:    "pointer to non-zero struct with omitZeroStruct",
+			value:          &struct{ Val bool }{Val: true},
+			omitZeroStruct: true,
+			want:           false,
+		},
+	}
 
-	z.reportZero = true
-	assert.True(t, enc.isZero(z))
+	for _, tc := range testCases {
+		tc := tc // Capture range variable.
 
-	// *time.Time with nil should be zero
-	var tp *time.Time
-	assert.True(t, enc.isZero(tp))
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
 
-	// actually all zeroer if nil should also be zero
-	var zp *zeroTest
-	assert.True(t, enc.isZero(zp))
+			got := isZero(tc.value, tc.omitZeroStruct)
+			assert.Equal(t, tc.want, got, "expected and actual isZero return are different")
+		})
+	}
 }
