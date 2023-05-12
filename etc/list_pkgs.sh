55fd8c9c30b37d48cf147901d98d942bc76ff23c
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2023-05-12 09:14:13 -0700

    GODRIVER-2837 Backport safe Makefile to the release branch. (#1256)

--- etc/list_pkgs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# list_pkgs <directory>
-# Lists all Go packages in the given directory.
-
-directory="$1"
-if [ -z "$directory" ]; then
-    directory="."
-fi
-go list $directory/... | sed -e "s/^go.mongodb.org\/mongo-driver/./"
