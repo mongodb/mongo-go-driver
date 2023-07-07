a1ff5d5bca08b1ae24dd51e26d39280a3b2ece91
Steven Silvester <steven.silvester@ieee.org>
2023-07-07 09:55:50 -0500

    GODRIVER-2883 Handle boolean 'ok' responses

--- x/mongo/driver/auth/plain_test.go
+++ x/mongo/driver/auth/plain_test.go
@@ -145,3 +145,49 @@ func TestPlainAuthenticator_Succeeds(t *testing.T) {
 	)
 	compareResponses(t, <-c.Written, expectedCmd, "$external")
 }
+
+
+func TestPlainAuthenticator_SucceedsBoolean(t *testing.T) {
+	t.Parallel()
+
+	authenticator := PlainAuthenticator{
+		Username: "user",
+		Password: "pencil",
+	}
+
+	resps := make(chan []byte, 1)
+	writeReplies(resps, bsoncore.BuildDocumentFromElements(nil,
+		bsoncore.AppendBooleanElement(nil, "ok", true),
+		bsoncore.AppendInt32Element(nil, "conversationId", 1),
+		bsoncore.AppendBinaryElement(nil, "payload", 0x00, []byte{}),
+		bsoncore.AppendBooleanElement(nil, "done", true),
+	))
+
+	desc := description.Server{
+		WireVersion: &description.VersionRange{
+			Max: 6,
+		},
+	}
+	c := &drivertest.ChannelConn{
+		Written:  make(chan []byte, 1),
+		ReadResp: resps,
+		Desc:     desc,
+	}
+
+	err := authenticator.Auth(context.Background(), &Config{Description: desc, Connection: c})
+	if err != nil {
+		t.Fatalf("expected no error but got \"%s\"", err)
+	}
+
+	if len(c.Written) != 1 {
+		t.Fatalf("expected 1 messages to be sent but had %d", len(c.Written))
+	}
+
+	payload, _ := base64.StdEncoding.DecodeString("AHVzZXIAcGVuY2ls")
+	expectedCmd := bsoncore.BuildDocumentFromElements(nil,
+		bsoncore.AppendInt32Element(nil, "saslStart", 1),
+		bsoncore.AppendStringElement(nil, "mechanism", "PLAIN"),
+		bsoncore.AppendBinaryElement(nil, "payload", 0x00, payload),
+	)
+	compareResponses(t, <-c.Written, expectedCmd, "$external")
+}
