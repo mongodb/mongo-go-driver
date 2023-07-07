e291f7cd7eb5dea8356feba76330080a4f49ff6b
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2023-07-07 15:30:32 -0600

    GODRIVER-2572 Add UST

--- mongo/integration/unified/unified_spec_test.go
+++ mongo/integration/unified/unified_spec_test.go
@@ -29,6 +29,7 @@ var (
 		"client-side-encryption/unified",
 		"client-side-operations-timeout",
 		"gridfs",
+		"server-selection/logging",
 	}
 	failDirectories = []string{
 		"unified-test-format/valid-fail",
