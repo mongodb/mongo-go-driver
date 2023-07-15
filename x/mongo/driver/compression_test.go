9bbdb317f6514b39c4f57347b48bfb8c8b917678
Charlie Vieth <charlie.vieth@mongodb.com>
2023-07-14 21:36:19 -0400

    x/mongo/driver: enable parallel zlib compression and improve zstd decompression
    This commit fixes a bug where zlib compression was serialized across all
    goroutines. This occurred because only one shared zlib decompresser was
    instantiated for each compression level which had to be locked when used
    and thus preventing concurrent compression.
    
    The decompression performance of zstd is also improved by using a pool
    of zstd decoders (instantiating a zstd encoded or decoder is fairly
    expensive). It also slightly cleans up the logic used to store and
    acquire zstd encoders.
    
    The CompressPayload benchmark is changed to use a stable payload, which
    is now stored in testdata/compression.go. Previously it used the
    compression.go file itself, which made benchmarks results confusing and
    liable to change whenever the compression/decompression logic changed.
    
    ```
    goos: darwin
    goarch: arm64
    pkg: go.mongodb.org/mongo-driver/x/mongo/driver
                                        │ base.20.txt  │             new.20.txt              │
                                        │    sec/op    │   sec/op     vs base                │
    CompressPayload/CompressorZLib-10     5387.4µ ± 0%   651.1µ ± 1%  -87.91% (p=0.000 n=20)
    CompressPayload/CompressorZstd-10      64.56µ ± 1%   64.10µ ± 0%   -0.72% (p=0.000 n=20)
    DecompressPayload/CompressorZLib-10    125.7µ ± 1%   123.7µ ± 0%   -1.60% (p=0.000 n=20)
    DecompressPayload/CompressorZstd-10    70.13µ ± 1%   45.80µ ± 1%  -34.70% (p=0.000 n=20)
    geomean                                235.3µ        124.0µ       -47.31%
    
                                        │ base.20.txt  │               new.20.txt               │
                                        │     B/s      │      B/s       vs base                 │
    CompressPayload/CompressorZLib-10     365.2Mi ± 0%   3021.4Mi ± 1%  +727.41% (p=0.000 n=20)
    CompressPayload/CompressorZstd-10     29.76Gi ± 1%    29.97Gi ± 0%    +0.73% (p=0.000 n=20)
    DecompressPayload/CompressorZLib-10   15.28Gi ± 1%    15.53Gi ± 0%    +1.63% (p=0.000 n=20)
    DecompressPayload/CompressorZstd-10   27.39Gi ± 1%    41.95Gi ± 1%   +53.13% (p=0.000 n=20)
    geomean                               8.164Gi         15.49Gi        +89.77%
    
                                        │ base.20.txt  │               new.20.txt               │
                                        │     B/op     │     B/op      vs base                  │
    CompressPayload/CompressorZLib-10     14.02Ki ± 0%   14.00Ki ± 0%   -0.10% (p=0.000 n=20)
    CompressPayload/CompressorZstd-10     3.398Ki ± 0%   3.398Ki ± 0%        ~ (p=1.000 n=20) ¹
    DecompressPayload/CompressorZLib-10   2.008Mi ± 0%   2.008Mi ± 0%        ~ (p=1.000 n=20) ¹
    DecompressPayload/CompressorZstd-10   4.109Mi ± 0%   1.969Mi ± 0%  -52.08% (p=0.000 n=20)
    geomean                               142.5Ki        118.5Ki       -16.82%
    ¹ all samples are equal
    
                                        │ base.20.txt  │              new.20.txt              │
                                        │  allocs/op   │ allocs/op   vs base                  │
    CompressPayload/CompressorZLib-10       1.000 ± 0%   1.000 ± 0%        ~ (p=1.000 n=20) ¹
    CompressPayload/CompressorZstd-10       4.000 ± 0%   4.000 ± 0%        ~ (p=1.000 n=20) ¹
    DecompressPayload/CompressorZLib-10     26.00 ± 0%   26.00 ± 0%        ~ (p=1.000 n=20) ¹
    DecompressPayload/CompressorZstd-10   104.000 ± 0%   1.000 ± 0%  -99.04% (p=0.000 n=20)
    geomean                                 10.20        3.193       -68.69%
    ¹ all samples are equal
    ```

--- x/mongo/driver/compression_test.go
+++ x/mongo/driver/compression_test.go
@@ -46,6 +46,46 @@ func TestCompression(t *testing.T) {
 	}
 }
 
+func TestCompressionLevels(t *testing.T) {
+	errEq := func(e1, e2 error) bool {
+		if e1 == nil || e2 == nil {
+			return (e1 == nil) == (e2 == nil)
+		}
+		return e1.Error() == e2.Error()
+	}
+
+	in := []byte("abc")
+	wr := new(bytes.Buffer)
+
+	t.Run("ZLib", func(t *testing.T) {
+		opts := CompressionOpts{
+			Compressor: wiremessage.CompressorZLib,
+		}
+		for lvl := zlib.HuffmanOnly - 2; lvl < zlib.BestCompression+2; lvl++ {
+			opts.ZlibLevel = lvl
+			_, err1 := CompressPayload(in, opts)
+			_, err2 := zlib.NewWriterLevel(wr, lvl)
+			if !errEq(err1, err2) {
+				t.Fatalf("%d: error: %v, want: %v", lvl, err1, err2)
+			}
+		}
+	})
+
+	t.Run("Zstd", func(t *testing.T) {
+		opts := CompressionOpts{
+			Compressor: wiremessage.CompressorZstd,
+		}
+		for lvl := zstd.SpeedFastest - 2; lvl < zstd.SpeedBestCompression+2; lvl++ {
+			opts.ZstdLevel = int(lvl)
+			_, err1 := CompressPayload(in, opts)
+			_, err2 := zstd.NewWriter(wr, zstd.WithEncoderLevel(zstd.EncoderLevelFromZstd(opts.ZstdLevel)))
+			if !errEq(err1, err2) {
+				t.Fatalf("%d: error: %v, want: %v", lvl, err1, err2)
+			}
+		}
+	})
+}
+
 func TestDecompressFailures(t *testing.T) {
 	t.Parallel()
 
