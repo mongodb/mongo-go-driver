522e9dcbb47516192d33d48b46eab80d829d0ffd
Steven Silvester <steven.silvester@ieee.org>
2023-08-01 19:55:03 -0500

    fix sha handling

--- etc/api_report.sh
+++ etc/api_report.sh
@@ -10,8 +10,14 @@ if [ -z $cmd ]; then
     go install golang.org/x/exp/cmd/gorelease@latest
     cmd="$(go env GOPATH)/gorelease"
 fi
-branch=$(git rev-parse --abbrev-ref HEAD)
-sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
+
+if [ -n "$GITHUB_BASE_REF" ]; then
+    sha=$GITHUB_BASE_REF
+else
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
+fi
+
 output=$($cmd -base=$sha || true)
 
 echo "hello"
