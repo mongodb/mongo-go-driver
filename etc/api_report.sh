f4278b3b305d53c63fb1af3d55618b8d64a74242
Steven Silvester <steven.silvester@ieee.org>
2023-08-01 20:47:12 -0500

    make git show work

--- etc/api_report.sh
+++ etc/api_report.sh
@@ -14,12 +14,17 @@ if [ -n "$GITHUB_BASE_REF" ]; then
     git fetch origin $GITHUB_BASE_REF
     sha=$(git rev-parse origin/$branch)
     git checkout -b my-new-branch
-    git clean -dffx
+    touch test.txt
+    # https://github.community/t/github-actions-bot-email-address/17204/6
+    git_user_name="41898282+github-actions[bot]"
+    git config user.email "${git_user_name}@users.noreply.github.com"
+    git config user.name "{$git_user_name}"
+    git add test.txt
+    git commit -a -m "foo"
 else
     branch=$(git rev-parse --abbrev-ref HEAD)
     sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
 fi
 
-git status --porcelain
 git --no-pager show
 gorelease -base=$sha > $1
