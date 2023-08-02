d16f44697cbef1c716481217819ccf6c24f5afee
Steven Silvester <steven.silvester@ieee.org>
2023-08-01 19:16:58 -0500

    clean up script

--- etc/api_report.sh
+++ etc/api_report.sh
@@ -6,6 +6,6 @@ set -eux
 go install golang.org/x/exp/cmd/gorelease@latest
 branch=$(git rev-parse --abbrev-ref HEAD)
 sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
-output=$(gorelease -base=$sha)
+output=$(gorelease -base=$sha || true)
 echo "hi"
 echo $output
