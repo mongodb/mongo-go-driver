fd2a9e8665abdedf952d7df024cc2cdd8e027ce2
Steven Silvester <steven.silvester@ieee.org>
2023-08-01 19:51:06 -0500

    more debug

--- etc/api_report.sh
+++ etc/api_report.sh
@@ -14,4 +14,5 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
 output=$($cmd -base=$sha || true)
 
+echo "hello"
 echo $output
