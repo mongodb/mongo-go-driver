a488ab9c20d19df52a936903c9a3ac824d870ccf
Steven Silvester <steven.silvester@ieee.org>
2023-08-01 20:53:44 -0500

    try explicit checkout

--- etc/api_report.sh
+++ etc/api_report.sh
@@ -9,14 +9,7 @@ if [ -z $cmd ]; then
     go install golang.org/x/exp/cmd/gorelease@latest
 fi
 
-if [ -n "$GITHUB_BASE_REF" ]; then
-    branch=$GITHUB_BASE_REF
-    git fetch origin
-    sha=$(git rev-parse origin/$branch)
-    git checkout -b my-new-branch
-else
-    branch=$(git rev-parse --abbrev-ref HEAD)
-    sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
-fi
+branch=$(git rev-parse --abbrev-ref HEAD)
+sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
 
 gorelease -base=$sha > $1
