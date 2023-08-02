0bd2e94883e301042c11bfd9789988877f963103
Steven Silvester <steven.silvester@ieee.org>
2023-08-01 20:41:19 -0500

    try again

--- etc/api_report.sh
+++ etc/api_report.sh
@@ -14,7 +14,7 @@ if [ -n "$GITHUB_BASE_REF" ]; then
     git fetch origin $GITHUB_BASE_REF
     sha=$(git rev-parse origin/$branch)
     git checkout -b my-new-branch
-    git clean -dfx
+    git clean -dffx
 else
     branch=$(git rev-parse --abbrev-ref HEAD)
     sha=$(git --no-pager reflog show $branch | tail -n 1 | awk '{print $1;}')
