fa0d99d613f8ebfce8733e85536686e06a1ad863
Steven Silvester <steven.silvester@ieee.org>
2023-08-26 15:17:42 -0500

    cleanup

--- etc/get_aws_secrets.sh
+++ etc/get_aws_secrets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # get-aws-secrets
 # Gets AWS secrets from the vault
-set -eux
+set -eu
 
 if [ -z "$DRIVERS_TOOLS" ]; then
     echo "Please define DRIVERS_TOOLS variable"
@@ -12,8 +12,12 @@ pushd ${DRIVERS_TOOLS}/.evergreen/auth_aws
 . ./activate-authawsvenv.sh
 popd
 
-# TODO: add note in setup_secrets.py about setup and using
-# AWS_PROFILE
 # TODO: Add a section to https://wiki.corp.mongodb.com/display/DRIVERS/Using+AWS+Secrets+Manager+to+Store+Testing+Secrets
 # about using a bash script that can be run locally
+# Add this note:
+# Note: for local testing using AWS SSO credentials,
+# you may need to set the AWS_PROFILE environment variable
+# to point to your local profile name.
+echo "Getting secrets: $@"
 python ${DRIVERS_TOOLS}/.evergreen/auth_aws/setup_secrets.py $@
+echo "Got secrets"
