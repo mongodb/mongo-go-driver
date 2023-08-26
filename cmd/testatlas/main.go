5f7fdd5c5bbbb236d5bcb90c8676e8fc81c631c8
Steven Silvester <steven.silvester@ieee.org>
2023-08-26 05:47:24 -0500

    GODRIVER-2958 Use AWS Secrets for Atlas tests

--- cmd/testatlas/main.go
+++ cmd/testatlas/main.go
@@ -23,7 +23,11 @@ func main() {
 	uris := flag.Args()
 	ctx := context.Background()
 
+	fmt.Printf("Running atlas tests for %d uris\n", len(uris))
+
 	for idx, uri := range uris {
+		fmt.Printf("Running test %d\n", idx)
+
 		// Set a low server selection timeout so we fail fast if there are errors.
 		clientOpts := options.Client().
 			ApplyURI(uri).
---
+++ etc/get_aws_secrets.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# get-aws-secrets
+# Gets AWS secrets from the vault
+set -eux
+
+if [ -z "$DRIVERS_TOOLS" ]; then
+    echo "Please define DRIVERS_TOOLS variable"
+    exit 1
+fi
+
+# TODO: this should be built into activate-authawsvenv.sh
+export PIP_QUIET=1
+pushd ${DRIVERS_TOOLS}/.evergreen/auth_aws
+. ./activate-authawsvenv.sh
+# TODO: this should be built into activate-authawsvenv.sh
+pip install pyyaml
+popd
+
+# TODO: add note in setup_secrets.py about setup and using
+# AWS_PROFILE
+# TODO: Add a section to https://wiki.corp.mongodb.com/display/DRIVERS/Using+AWS+Secrets+Manager+to+Store+Testing+Secrets
+# about using a bash script that can be run locally
+python ${DRIVERS_TOOLS}/.evergreen/auth_aws/setup_secrets.py $@
