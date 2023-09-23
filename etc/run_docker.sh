92ddc6ddc537eb216fa5a85baef95f2bb85893ac
Steven Silvester <steven.silvester@ieee.org>
2023-09-23 11:45:54 -0500

    clean up and add platform support

--- etc/run_docker.sh
+++ etc/run_docker.sh
@@ -7,12 +7,15 @@ if [ -z "$DRIVERS_TOOLS" ]; then
     echo "Please set DRIVERS_TOOLS env variable."
     exit 1
 fi
-pushd $DRIVERS_TOOLS/.evergreen/docker
-docker build -t drivers-evergreen-tools .
+PLATFORM=${DOCKER_PLATFORM:-}
+
+pushd $DRIVERS_TOOLS/.evergreen/docker/ubuntu20.04
+docker build $PLATFORM -t drivers-evergreen-tools .
 popd
-docker build -t go-test .
+docker build $PLATFORM -t go-test .
 
 # Handle environment variables and optional positional arg for the makefile target.
+
 MAKEFILE_TARGET=${1:-evg-test-versioned-api}
 MONGODB_VERSION=${MONGODB_VERSION:-latest}
 TOPOLOGY=${TOPOLOGY:-replica_set}
@@ -28,8 +31,10 @@ ENV="$ENV -e GO_BUILD_TAGS=$GO_BUILD_TAGS"
 
 VOL="-v `pwd`:/src"
 VOL="$VOL -v $DRIVERS_TOOLS:/root/drivers-evergreen-tools"
+USE_TTY=""
+test -t 1 && USE_TTY="-t"
 
-docker run --rm $VOL $ENV -t go-test
+docker run $PLATFORM --rm $VOL $ENV -i $USE_TTY go-test
 if [ -f "test.suite" ]; then
     tail test.suite
 fi
