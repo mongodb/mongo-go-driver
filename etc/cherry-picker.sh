5f70167b2df818714742f7307841f4deac9b9a69
Steven Silvester <steven.silvester@ieee.org>
2023-09-27 10:37:29 -0500

    GODRIVER-2978 Remove branch synching workflow and improve local script (#1403)

--- etc/cherry-picker.sh
+++ etc/cherry-picker.sh
@@ -14,7 +14,7 @@ if [ -z "$user" ]; then
 fi
 
 mkdir -p $dirname
-if [ -z $AUTH_TOKEN ]; then
+if [ -z "$AUTH_TOKEN" ]; then
     git clone git@github.com:mongodb/mongo-go-driver.git $dirname
 else
     echo "$AUTH_TOKEN" > mytoken.txt
@@ -23,7 +23,7 @@ else
 fi
 
 cd $dirname
-if [ -z $AUTH_TOKEN ]; then
+if [ -z "$AUTH_TOKEN" ]; then
     git remote add $user git@github.com:$user/mongo-go-driver.git
 else
     git remote add $user https://$user:${AUTH_TOKEN}@github.com/$user/mongo-go-driver.git
@@ -35,7 +35,20 @@ head="$user:$branch"
 git fetch origin $base
 git fetch origin $target
 git checkout -b $branch origin/$target
-git cherry-pick -x $sha
+git cherry-pick -x $sha || true
+
+files=$(git ls-files -m)
+if [ -n "${files}" ]; then
+    EDITOR=${EDITOR:-$(git config core.editor)}
+    EDITOR=${EDITOR:-vim}
+    for fname in $files; do
+        echo "Fixing $fname..."
+        $EDITOR $fname
+        git add $fname
+    done
+    echo "Finishing cherry pick."
+    git cherry-pick --continue
+fi
 
 old_title=$(git --no-pager log  -1 --pretty=%B | head -n 1)
 ticket=$(echo $old_title | sed -r 's/([A-Z]+-[0-9]+).*/\1/')
@@ -53,14 +66,14 @@ echo "Base: $target"
 echo "Head: $head"
 echo
 
-if [ -n $GITHUB_ACTOR ]; then
+if [ -n "$GITHUB_ACTOR" ]; then
     choice=Y
 else
     read -p 'Push changes? (Y/n) ' choice
 fi
 
 if [[ "$choice" == "Y" || "$choice" == "y" || -z "$choice" ]]; then
-    if [ -n $user ]; then
+    if [ -n "$user" ]; then
         git push $user
     fi
     gh pr create --title "$title" --base $target --head $head --body "$body"
