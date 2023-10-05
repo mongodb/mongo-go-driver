7bff8e23973fd3b1db4147ce64ca18a2a0b67a21
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2023-10-05 14:15:26 -0600

    GODRIVER-2979 Remove DecodeBytes for v2

--- mongo/single_result.go
+++ mongo/single_result.go
@@ -98,15 +98,6 @@ func (sr *SingleResult) Raw() (bson.Raw, error) {
 	return sr.rdr, nil
 }
 
-// DecodeBytes will return the document represented by this SingleResult as a bson.Raw. If there was an error from the
-// operation that created this SingleResult, both the result and that error will be returned. If the operation returned
-// no documents, this will return (nil, ErrNoDocuments).
-//
-// Deprecated: Use [SingleResult.Raw] instead.
-func (sr *SingleResult) DecodeBytes() (bson.Raw, error) {
-	return sr.Raw()
-}
-
 // setRdrContents will set the contents of rdr by iterating the underlying cursor if necessary.
 func (sr *SingleResult) setRdrContents() error {
 	switch {
