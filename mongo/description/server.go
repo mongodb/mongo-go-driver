171895f94fa0dd2f3c2229ddebf45121a4595aad
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2023-10-10 10:43:38 -0600

    GODRIVER-2943 Remove Ok from server

--- mongo/description/server.go
+++ mongo/description/server.go
@@ -64,7 +64,6 @@ type Server struct {
 	TopologyVersion          *TopologyVersion
 	Kind                     ServerKind
 	WireVersion              *VersionRange
-	Ok                       int32
 }
 
 // NewServer creates a new server description from the given hello command response.
@@ -230,8 +229,6 @@ func NewServer(addr address.Address, response bson.Raw) Server {
 				desc.LastError = errors.New("not ok")
 				return desc
 			}
-
-			desc.Ok = okay
 		case "passives":
 			var err error
 			desc.Passives, err = stringSliceFromRawElement(element)
