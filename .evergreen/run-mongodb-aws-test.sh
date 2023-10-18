f01b7ebe3ed0546abc328f6160438d4cbea2fd9f
Steven Silvester <steven.silvester@ieee.org>
2023-10-18 10:08:24 -0500

    GODRIVER-2607 Remove legacy shell from test scripts (#1429)
    (cherry picked from commit 1066e2a3b56f6ef4ee0a99140ed8814c9d455de9)

--- .evergreen/run-mongodb-aws-test.sh
+++ .evergreen/run-mongodb-aws-test.sh
@@ -12,20 +12,9 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #                 mechanism.
 
 echo "Running MONGODB-AWS authentication tests"
-# ensure no secrets are printed in log files
-set +x
 
-# load the script
-shopt -s expand_aliases # needed for `urlencode` alias
-[ -s "${PROJECT_DIRECTORY}/prepare_mongodb_aws.sh" ] && source "${PROJECT_DIRECTORY}/prepare_mongodb_aws.sh"
-
-MONGODB_URI=${MONGODB_URI:-"mongodb://localhost"}
-MONGODB_URI="${MONGODB_URI}/aws?authMechanism=MONGODB-AWS"
-if [[ -n ${SESSION_TOKEN} ]]; then
-    MONGODB_URI="${MONGODB_URI}&authMechanismProperties=AWS_SESSION_TOKEN:${SESSION_TOKEN}"
-fi
-
-export MONGODB_URI="$MONGODB_URI"
+# Handle credentials and environment setup.
+. $DRIVERS_TOOLS/.evergreen/auth_aws/aws_setup.sh $1
 
 # show test output
 set -x
