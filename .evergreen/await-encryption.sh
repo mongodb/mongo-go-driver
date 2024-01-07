a6deedb9fcc876f6065ef6ff31f34889acefad63
Steven Silvester <steven.silvester@ieee.org>
2024-01-07 07:41:20 -0600

    use await encryption

---
+++ .evergreen/await-encryption.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Script to wait for the kmip servers to start
+set -eux
+
+if [ -z "$DRIVERS_TOOLS" ]; then
+    echo "Please define DRIVERS_TOOLS variable"
+    exit 1
+fi
+
+source ./secrets-export.sh
+bash $DRIVERS_TOOLS/.evergreen/csfle/await_servers.sh
