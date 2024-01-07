25ffc375bd6c4000930219e244a37277892b80d5
Steven Silvester <steven.silvester@ieee.org>
2024-01-07 17:05:19 -0600

    fix file handling

--- etc/setup-encryption.sh
+++ etc/setup-encryption.sh
@@ -20,10 +20,12 @@ bash $DRIVERS_TOOLS/.evergreen/csfle/setup_secrets.sh
 
 # Map some of the secrets to expected environment variables.
 source ./secrets-export.sh
-printf "\nexport AZURE_TENANT_ID=\"$FLE_AZURE_TENANTID\"" >> secrets-export.sh
-printf "export AZURE_CLIENT_ID=\"$FLE_AZURE_CLIENTID\"" >> secrets-export.sh
-printf "export AZURE_CLIENT_SECRET=\"$FLE_AZURE_CLIENTSECRET\"" >> secrets-export.sh
-printf "export GCP_EMAIL=\"$FLE_GCP_EMAIL\"" >> secrets-export.sh
-printf "export GCP_PRIVATE_KEY=\"$FLE_GCP_PRIVATEKEY\"\n" >> secrets-export.sh
+echo "" >> secrets-export.sh
+echo "export AZURE_TENANT_ID=\"$FLE_AZURE_TENANTID\"" >> secrets-export.sh
+echo "export AZURE_CLIENT_ID=\"$FLE_AZURE_CLIENTID\"" >> secrets-export.sh
+echo "export AZURE_CLIENT_SECRET=\"$FLE_AZURE_CLIENTSECRET\"" >> secrets-export.sh
+echo "export GCP_EMAIL=\"$FLE_GCP_EMAIL\"" >> secrets-export.sh
+echo "export GCP_PRIVATE_KEY=\"$FLE_GCP_PRIVATEKEY\"" >> secrets-export.sh
+echo "" >> secrets-export.sh
 
 bash $DRIVERS_TOOLS/.evergreen/csfle/start_servers.sh
