724f3b55091bde6e8ff75be7354a2de8e88834b0
Steven Silvester <steven.silvester@ieee.org>
2024-01-07 18:05:24 -0600

    use vault names

--- etc/setup-encryption.sh
+++ etc/setup-encryption.sh
@@ -17,17 +17,4 @@ export CSFLE_TLS_CERT_FILE="${PARENT_DIR}/testdata/kmip-certs/server-ec.pem"
 export CSFLE_TLS_CLIENT_CERT_FILE="${PARENT_DIR}/testdata/kmip-certs/client-ec.pem"
 
 bash $DRIVERS_TOOLS/.evergreen/csfle/setup_secrets.sh
-
-# Map some of the secrets to expected environment variables.
-source ./secrets-export.sh
-cat <<EOT >> ./secrets-export.sh
-
-export AZURE_TENANT_ID="$FLE_AZURE_TENANTID"
-export AZURE_CLIENT_ID="$FLE_AZURE_CLIENTID"
-export AZURE_CLIENT_SECRET="$FLE_AZURE_CLIENTSECRET"
-export GCP_EMAIL="$FLE_GCP_EMAIL"
-export GCP_PRIVATE_KEY="$FLE_GCP_PRIVATEKEY"
-
-EOT
-
 bash $DRIVERS_TOOLS/.evergreen/csfle/start_servers.sh
