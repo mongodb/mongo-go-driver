a791b772547f8fede9a5bb8ab72e3f4f7bef4946
Preston Vasquez <prestonvasquez@icloud.com>
2024-01-23 14:46:27 -0700

    GODRIVER-2466 Implement assertEventCount for unified SDAM tests (#1522)

--- internal/integration/unified/client_entity.go
+++ internal/integration/unified/client_entity.go
@@ -320,11 +320,31 @@ func (c *clientEntity) getEventCount(eventType monitoringEventType) int32 {
 	return c.eventsCount[eventType]
 }
 
-func (c *clientEntity) getServerDescriptionChangedEventCount(evt serverDescriptionChangedEventInfo) int32 {
+func (c *clientEntity) getServerDescriptionChangedEventCount(expected serverDescriptionChangedEventInfo) int32 {
 	c.serverDescriptionChangedEventsCountLock.Lock()
 	defer c.serverDescriptionChangedEventsCountLock.Unlock()
 
-	return c.serverDescriptionChangedEventsCount[evt]
+	// If the previousDescription or newDescription is "nil" in the expected case,
+	// then those fields can be anything.
+	for actual, count := range c.serverDescriptionChangedEventsCount {
+		actPrevD := actual.PreviousDescription
+		expPrevD := expected.PreviousDescription
+
+		if expPrevD != nil && expPrevD.Type != actPrevD.Type {
+			continue
+		}
+
+		actNewD := actual.NewDescription
+		expNewD := expected.NewDescription
+
+		if expNewD != nil && expNewD.Type != actNewD.Type {
+			continue
+		}
+
+		return count
+	}
+
+	return 0
 }
 
 func getSecondsSinceEpoch() float64 {
