d9607d42458a16c6eb8348f81424354fb613a3bc
Steven Silvester <steven.silvester@ieee.org>
2024-02-10 05:26:56 -0600

    fix enterprise auth

--- etc/run-enterprise-plain-test.sh
+++ etc/run-enterprise-plain-test.sh
@@ -2,7 +2,7 @@
 # run-enterprise-gssapi-test
 # Runs the enterprise auth tests with plain credentials.
 set -eu
-bash ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh drivers/enterprise_auth
+. ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh drivers/enterprise_auth
 export MONGODB_URI="mongodb://${SASL_USER}:${SASL_PASS}@${SASL_HOST}:${SASL_PORT}/ldap?authMechanism=PLAIN"
 export MONGO_GO_DRIVER_COMPRESSOR="${MONGO_GO_DRIVER_COMPRESSOR:-}"
 make -s evg-test-enterprise-auth
