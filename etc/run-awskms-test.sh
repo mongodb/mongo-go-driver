4871fdfb32dbd72468db6a2e6c3b211f6f3dbb00
Steven Silvester <steven.silvester@ieee.org>
2024-02-10 05:36:55 -0600

    fix awskms

--- etc/run-awskms-test.sh
+++ etc/run-awskms-test.sh
@@ -14,8 +14,7 @@ export MONGODB_URI="$ATLAS_FREE"
 if [ -z "${EXPECT_ERROR:-}" ]; then
     . ${DRIVERS_TOOLS}/.evergreen/csfle/setup-secrets.sh
     export AWS_SECRET_ACCESS_KEY=$FLE_AWS_SECRET_ACCESS_KEY
-    export AWS_ACCESS_KEY_ID=$$FLE_AWS_ACCESS_KEY_ID
+    export AWS_ACCESS_KEY_ID=$FLE_AWS_ACCESS_KEY_ID
 fi
-echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
 
 LD_LIBRARY_PATH=./install/libmongocrypt/lib64 PROVIDER='aws' ./testkms
