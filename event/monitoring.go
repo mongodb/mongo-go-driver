9c6603d14407d0411cc7a82021a817ce7109bbf3
Qingyang Hu <103950869+qingyang-hu@users.noreply.github.com>
2024-02-13 09:36:59 -0500

    GODRIVER-2335 Preemptively cancel in progress operations when SDAM heartbeats timeout. (#1549)

--- event/monitoring.go
+++ event/monitoring.go
@@ -120,8 +120,9 @@ type PoolEvent struct {
 	Reason       string              `json:"reason"`
 	// ServiceID is only set if the Type is PoolCleared and the server is deployed behind a load balancer. This field
 	// can be used to distinguish between individual servers in a load balanced deployment.
-	ServiceID *primitive.ObjectID `json:"serviceId"`
-	Error     error               `json:"error"`
+	ServiceID    *primitive.ObjectID `json:"serviceId"`
+	Interruption bool                `json:"interruptInUseConnections"`
+	Error        error               `json:"error"`
 }
 
 // PoolMonitor is a function that allows the user to gain access to events occurring in the pool
