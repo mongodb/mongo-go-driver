51be6e83cbda49425d96a68a4c0ac1c9514acf00
Steven Silvester <steve.silvester@mongodb.com>
2024-03-05 06:41:16 -0600

    GODRIVER-3147 Fix Atlas Test Runner Return Value Handling

--- etc/run-atlas-test.sh
+++ etc/run-atlas-test.sh
@@ -8,4 +8,4 @@ set +x
 . ${DRIVERS_TOOLS}/.evergreen/atlas/setup-secrets.sh
 
 echo "Running cmd/testatlas/main.go"
-go run ./cmd/testatlas/main.go "$ATLAS_REPL" "$ATLAS_SHRD" "$ATLAS_FREE" "$ATLAS_TLS11" "$ATLAS_TLS12" "$ATLAS_SERVERLESS" "$ATLAS_SRV_REPL" "$ATLAS_SRV_SHRD" "$ATLAS_SRV_FREE" "$ATLAS_SRV_TLS11" "$ATLAS_SRV_TLS12" "$ATLAS_SRV_SERVERLESS" | tee test.suite
+go run ./cmd/testatlas/main.go "$ATLAS_REPL" "$ATLAS_SHRD" "$ATLAS_FREE" "$ATLAS_TLS11" "$ATLAS_TLS12" "$ATLAS_SERVERLESS" "$ATLAS_SRV_REPL" "$ATLAS_SRV_SHRD" "$ATLAS_SRV_FREE" "$ATLAS_SRV_TLS11" "$ATLAS_SRV_TLS12" "$ATLAS_SRV_SERVERLESS" >> test.suite
