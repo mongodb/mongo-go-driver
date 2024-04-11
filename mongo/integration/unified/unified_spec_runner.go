832e5020a4021a8c79eb5a45a700e7019ce792b6
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2024-04-10 19:51:32 -0700

    Fix wording and add comment on unified spec test skips.

--- mongo/integration/unified/unified_spec_runner.go
+++ mongo/integration/unified/unified_spec_runner.go
@@ -44,13 +44,17 @@ var (
 		"listSearchIndexes ignores read and write concern":     "Sync GODRIVER-3074, but skip testing bug GODRIVER-3043",
 		"updateSearchIndex ignores the read and write concern": "Sync GODRIVER-3074, but skip testing bug GODRIVER-3043",
 
-		"timeoutMS can be overridden for a find":                                               "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
-		"timeoutMS can be configured for an operation - find on collection":                    "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
-		"timeoutMS can be configured for an operation - aggregate on collection":               "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
-		"timeoutMS can be configured for an operation - aggregate on database":                 "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
-		"operation is retried multiple times for non-zero timeoutMS - find on collection":      "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
-		"operation is retried multiple times for non-zero timeoutMS - aggregate on collection": "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
-		"operation is retried multiple times for non-zero timeoutMS - aggregate on database":   "CSOT is disabled on find and aggregate. See DRIVERS-2722.",
+		// DRIVERS-2722: Setting "maxTimeMS" on a command that creates a cursor
+		// also limits the lifetime of the cursor. That may be surprising to
+		// users, so omit "maxTimeMS" from operations that return user-managed
+		// cursors.
+		"timeoutMS can be overridden for a find":                                               "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+		"timeoutMS can be configured for an operation - find on collection":                    "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+		"timeoutMS can be configured for an operation - aggregate on collection":               "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+		"timeoutMS can be configured for an operation - aggregate on database":                 "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+		"operation is retried multiple times for non-zero timeoutMS - find on collection":      "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+		"operation is retried multiple times for non-zero timeoutMS - aggregate on collection": "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+		"operation is retried multiple times for non-zero timeoutMS - aggregate on database":   "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
 	}
 
 	logMessageValidatorTimeout = 10 * time.Millisecond
