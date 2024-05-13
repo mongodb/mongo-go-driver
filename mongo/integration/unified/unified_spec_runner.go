15542f0f652b99be8fc5e6e7094ba75fd3f4dce0
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2024-05-13 11:45:12 -0700

    GODRIVER-3157 Skip Serverless Proxy test that requires failpoint on hello.

--- mongo/integration/unified/unified_spec_runner.go
+++ mongo/integration/unified/unified_spec_runner.go
@@ -10,6 +10,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
 	"strings"
 	"testing"
@@ -23,7 +24,7 @@ import (
 )
 
 var (
-	skippedTestDescriptions = map[string]string{
+	skippedTests = map[string]string{
 		// GODRIVER-1773: This test runs a "find" with limit=4 and batchSize=3. It expects batchSize values of three for
 		// the "find" and one for the "getMore", but we send three for both.
 		"A successful find event with a getmore and the server kills the cursor (<= 4.4)": "See GODRIVER-1773",
@@ -66,6 +67,10 @@ var (
 		"operation is retried multiple times for non-zero timeoutMS - find on collection":      "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
 		"operation is retried multiple times for non-zero timeoutMS - aggregate on collection": "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
 		"operation is retried multiple times for non-zero timeoutMS - aggregate on database":   "maxTimeMS is disabled on find and aggregate. See DRIVERS-2722.",
+	}
+
+	skippedServerlessProxyTests = map[string]string{
+		"errors during the initial connection hello are ignored": "Serverless Proxy does not support failpoints on hello (see GODRIVER-3157)",
 	}
 
 	logMessageValidatorTimeout = 10 * time.Millisecond
@@ -248,8 +253,13 @@ func (tc *TestCase) Run(ls LoggerSkipper) error {
 	if tc.SkipReason != nil {
 		ls.Skipf("skipping for reason: %q", *tc.SkipReason)
 	}
-	if skipReason, ok := skippedTestDescriptions[tc.Description]; ok {
-		ls.Skipf("skipping due to known failure: %v", skipReason)
+	if skipReason, ok := skippedTests[tc.Description]; ok {
+		ls.Skipf("skipping due to known failure: %q", skipReason)
+	}
+	// If we're running against a Serverless Proxy instance, also check the
+	// tests that should be skipped only for Serverless Proxy.
+	if skipReason, ok := skippedServerlessProxyTests[tc.Description]; ok && os.Getenv("IS_SERVERLESS_PROXY") == "true" {
+		ls.Skipf("skipping due to known failure with Serverless Proxy: %q", skipReason)
 	}
 
 	// Validate that we support the schema declared by the test file before attempting to use its contents.
