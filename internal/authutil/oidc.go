a47681f2394360c3c357c5224bc8013c421a3209
Patrick Meredith <pmeredit@gmail.com>
2024-06-11 18:05:35 -0400

    GODRIVER-2911: Initial attempted to untie the Gordian not, this will not go live, I'm sure

---
+++ internal/authutil/oidc.go
@@ -0,0 +1,80 @@
+package authutil
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
+)
+
+const oidcMech = "MONGODB-OIDC"
+const tokenResourceProp = "TOKEN_RESOURCE"
+const environmentProp = "ENVIRONMENT"
+const principalProp = "PRINCIPAL"
+const allowedHostsProp = "ALLOWED_HOSTS"
+const azureEnvironmentValue = "azure"
+const gcpEnvironmentValue = "gcp"
+
+// OIDCAuthenticator is synchronized and handles caching of the access token, refreshToken,
+// and IDPInfo. It also provides a mechanism to refresh the access token, but this functionality
+// is only for the OIDC Human flow.
+type OIDCAuthenticator struct {
+	mu sync.Mutex // Guards all of the info in the OIDCAuthenticator struct.
+
+	AuthMechanismProperties map[string]string
+
+	accessToken  string
+	refreshToken *string
+	idpInfo      *IDPInfo
+}
+
+type IDPInfo struct {
+	Issuer        string   `bson:"issuer"`
+	ClientID      string   `bson:"clientId"`
+	RequestScopes []string `bson:"requestScopes"`
+}
+
+type OIDCCallback func(context.Context, *OIDCAargs) (*OIDCCredential, error)
+
+type OIDCAargs struct {
+	Version      int
+	IDPInfo      *IDPInfo
+	RefreshToken *string
+}
+
+type OIDCCredential struct {
+	AccessToken  string
+	ExpiresAt    time.Time
+	RefreshToken *string
+}
+
+type oidcOneStep struct {
+	accessToken string
+}
+
+func (oos *oidcOneStep) Start() (string, []byte, error) {
+	return oidcMech, jwtStepRequest(oos.accessToken), nil
+}
+
+func (oos *oidcOneStep) Next(context.Context, []byte) ([]byte, error) {
+	return nil, newAuthError("unexpected step in OIDC machine authentication", nil)
+}
+
+func (*oidcOneStep) Completed() bool {
+	return true
+}
+
+func jwtStepRequest(accessToken string) []byte {
+	return bsoncore.NewDocumentBuilder().
+		AppendString("jwt", accessToken).
+		Build()
+}
+
+func principalStepRequest(principal string) []byte {
+	doc := bsoncore.NewDocumentBuilder()
+	if principal != "" {
+		doc.AppendString("n", principal)
+	}
+	return doc.Build()
+}
