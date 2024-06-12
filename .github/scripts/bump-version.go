9a10e2a1a00f7d45b2f2946dbde66b55b0436db1
Steven Silvester <steve.silvester@mongodb.com>
2024-06-12 08:30:05 -0500

    update version writer

--- .github/scripts/bump-version.go
+++ .github/scripts/bump-version.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"path"
@@ -9,35 +8,28 @@ import (
 
 const (
 	versionFile = "version/version.go"
+	prefix      = `// Copyright (C) MongoDB, Inc. 2017-present.
+	//
+	// Licensed under the Apache License, Version 2.0 (the "License"); you may
+	// not use this file except in compliance with the License. You may obtain
+	// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+	
+	// Package version defines the Go Driver version.
+	package version // import "go.mongodb.org/mongo-driver/version"
+	
+	// Driver is the current version of the driver.
+	`
 )
 
 func main() {
 	repoDir, err := os.Getwd()
 	versionFilePath := path.Join(repoDir, versionFile)
-	readFile, err := os.Open(versionFilePath)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	var fileLines []string
-
-	for fileScanner.Scan() {
-		fileLines = append(fileLines, fileScanner.Text())
-	}
-
-	readFile.Close()
-
-	fileLines[len(fileLines)-1] = "var Driver = \"" + os.Args[1] + "\""
 
 	writeFile, err := os.OpenFile(versionFilePath, os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, line := range fileLines {
-		fmt.Fprintln(writeFile, line)
-	}
-
+	text := prefix + "var Driver = \"" + os.Args[1] + "\"\n"
+	fmt.Fprint(writeFile, text)
 	writeFile.Close()
 }
