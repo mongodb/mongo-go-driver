f6def8d6e47e304cdcbbecc7cb36bf3cb06ac56d
Patrick Meredith <pmeredit@gmail.com>
2024-06-12 21:22:13 -0400

    GODRIVER-2911: Set authenticator from Command to Operation

--- x/mongo/driver/operation/command.go
+++ x/mongo/driver/operation/command.go
@@ -108,6 +108,7 @@ func (c *Command) Execute(ctx context.Context) error {
 		ServerAPI:      c.serverAPI,
 		Timeout:        c.timeout,
 		Logger:         c.logger,
+		Authenticator:  c.authenticator,
 	}.Execute(ctx)
 }
 
