16a7d33e5a5c09f71b525c53ad80fa0368cc3c3e
Patrick Meredith <pmeredit@protonmail.com>
2024-08-20 12:38:23 -0400

    GODRIVER-2758: I don't know why this needs to be lowercase, but it does

--- mongo/client_examples_test.go
+++ mongo/client_examples_test.go
@@ -470,7 +470,7 @@ func ExampleConnect_bSONOptions() {
 	}
 }
 
-func ExampleConnect_OIDC() {
+func ExampleConnect_oIDC() {
 	// The `MONGODB-OIDC authentication mechanism` is available in MongoDB 7.0+
 	// on Linux platforms.
 	//
