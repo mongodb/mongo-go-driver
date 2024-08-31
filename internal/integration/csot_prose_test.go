411d63507e0cd6fb4be43f1fc404cd96b5164c1c
Preston Vasquez <prestonvasquez@icloud.com>
2024-08-30 18:45:31 -0600

    GODRIVER-3322 Update time from S to MS

--- internal/integration/csot_prose_test.go
+++ internal/integration/csot_prose_test.go
@@ -202,7 +202,7 @@ func TestCSOTProse(t *testing.T) {
 		})
 
 		// Create a new MongoClient with timeoutMS=10.
-		cliOptions := options.Client().SetTimeout(10 * time.Second).ApplyURI(mtest.ClusterURI())
+		cliOptions := options.Client().SetTimeout(10 * time.Millisecond).ApplyURI(mtest.ClusterURI())
 		integtest.AddTestServerAPIVersion(cliOptions)
 
 		client, err := mongo.Connect(cliOptions)
