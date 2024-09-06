cc7d77edca455ec208387117b7e37e0dc18da6aa
Steven Silvester <steve.silvester@mongodb.com>
2024-09-06 07:35:55 -0500

    try topology_options and client

--- mongo/client.go
+++ mongo/client.go
@@ -212,21 +212,42 @@ func NewClient(opts ...*options.ClientOptions) (*Client, error) {
 	}
 
 	if clientOpt.Auth != nil {
-		client.authenticator, err = auth.CreateAuthenticator(
-			clientOpt.Auth.AuthMechanism,
-			topology.ConvertCreds(clientOpt.Auth),
-			clientOpt.HTTPClient,
-		)
+		var oidcMachineCallback auth.OIDCCallback
+		if clientOpt.Auth.OIDCMachineCallback != nil {
+			oidcMachineCallback = func(ctx context.Context, args *driver.OIDCArgs) (*driver.OIDCCredential, error) {
+				cred, err := clientOpt.Auth.OIDCMachineCallback(ctx, convertOIDCArgs(args))
+				return (*driver.OIDCCredential)(cred), err
+			}
+		}
+
+		var oidcHumanCallback auth.OIDCCallback
+		if clientOpt.Auth.OIDCHumanCallback != nil {
+			oidcHumanCallback = func(ctx context.Context, args *driver.OIDCArgs) (*driver.OIDCCredential, error) {
+				cred, err := clientOpt.Auth.OIDCHumanCallback(ctx, convertOIDCArgs(args))
+				return (*driver.OIDCCredential)(cred), err
+			}
+		}
+
+		// Create an authenticator for the client
+		client.authenticator, err = auth.CreateAuthenticator(clientOpt.Auth.AuthMechanism, &auth.Cred{
+			Source:              clientOpt.Auth.AuthSource,
+			Username:            clientOpt.Auth.Username,
+			Password:            clientOpt.Auth.Password,
+			PasswordSet:         clientOpt.Auth.PasswordSet,
+			Props:               clientOpt.Auth.AuthMechanismProperties,
+			OIDCMachineCallback: oidcMachineCallback,
+			OIDCHumanCallback:   oidcHumanCallback,
+		}, clientOpt.HTTPClient)
 		if err != nil {
-			return nil, fmt.Errorf("error creating authenticator: %w", err)
+			return nil, err
 		}
 	}
 
 	cfg, err := topology.NewConfigWithAuthenticator(clientOpt, client.clock, client.authenticator)
+
 	if err != nil {
 		return nil, err
 	}
-
 	client.serverAPI = topology.ServerAPIFromServerOptions(cfg.ServerOpts)
 
 	if client.deployment == nil {
@@ -243,6 +264,19 @@ func NewClient(opts ...*options.ClientOptions) (*Client, error) {
 	}
 
 	return client, nil
+}
+
+// convertOIDCArgs converts the internal *driver.OIDCArgs into the equivalent
+// public type *options.OIDCArgs.
+func convertOIDCArgs(args *driver.OIDCArgs) *options.OIDCArgs {
+	if args == nil {
+		return nil
+	}
+	return &options.OIDCArgs{
+		Version:      args.Version,
+		IDPInfo:      (*options.IDPInfo)(args.IDPInfo),
+		RefreshToken: args.RefreshToken,
+	}
 }
 
 // Connect initializes the Client by starting background monitoring goroutines.
