81f15522717f1afbdb4d2bb1e229efbd8d514120
Steven Silvester <steve.silvester@mongodb.com>
2024-09-07 05:22:38 -0500

    debug again

--- x/mongo/driver/topology/topology_options.go
+++ x/mongo/driver/topology/topology_options.go
@@ -90,6 +90,7 @@ func ConvertCreds(cred *options.Credential) *driver.Cred {
 	if cred == nil {
 		return nil
 	}
+	fmt.Printf("convert creds: %s", cred.AuthSource)
 
 	var oidcMachineCallback auth.OIDCCallback
 	if cred.OIDCMachineCallback != nil {
