612ca2ebf83c881017e9fb7fed06a8d2cd9fd290
Steven Silvester <steve.silvester@mongodb.com>
2024-09-20 06:49:37 -0500

    debug

--- etc/run-awskms-test.sh
+++ etc/run-awskms-test.sh
@@ -7,6 +7,7 @@ set -eu
 bash ${DRIVERS_TOOLS}/.evergreen/csfle/await-servers.sh
 
 GO_BUILD_TAGS="cse" task setup-test
+cat .test.env
 task build-kms-test
 
 . ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh drivers/atlas_connect
