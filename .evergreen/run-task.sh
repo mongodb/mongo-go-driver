367c346ca9df6abd5f790535a3208064c1601f6a
Steven Silvester <steve.silvester@mongodb.com>
2024-09-22 20:38:07 -0500

    clean up run task

--- .evergreen/run-task.sh
+++ .evergreen/run-task.sh
@@ -3,5 +3,11 @@
 # Source the env.sh file and run the given task
 set -eu
 
-source ./env.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIRECTORY=$(dirname $SCRIPT_DIR)
+pushd ${PROJECT_DIRECTORY} > /dev/null
+
+source env.sh
 task "$@"
+
+popd > /dev/null
