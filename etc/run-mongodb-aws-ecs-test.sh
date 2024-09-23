4fcd7dbe1cf203a8368c06b2e311f536693cc671
Steven Silvester <steve.silvester@mongodb.com>
2024-09-23 08:19:39 -0500

    refactor

---
+++ etc/run-mongodb-aws-ecs-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+if [ "${SKIP_ECS_AUTH_TEST}" = "true" ]; then
+    echo "This platform does not support the ECS auth test, skipping..."
+    exit 0
+fi
+
+go build ${BUILD_TAGS} ./internal/cmd/testaws/main.go
+    
+AUTH_AWS_DIR=${DRIVERS_TOOLS}/.evergreen/auth_aws
+ECS_SRC_DIR=$AUTH_AWS_DIR/src
+
+# pack up project directory to ssh it to the container
+mkdir -p $ECS_SRC_DIR/src
+tar -czf $ECS_SRC_DIR/src/src.tgz -C ${PROJECT_DIRECTORY} .
+PROJECT_DIRECTORY="${ECS_SRC_DIR}" $AUTH_AWS_DIR/aws_setup.sh ecs
