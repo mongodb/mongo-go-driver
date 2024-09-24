f77e77a7464a2160ef8c6d1d34c3afa35344e4e3
Steven Silvester <steve.silvester@mongodb.com>
2024-09-24 15:24:28 -0500

    debug

--- etc/golangci-lint.sh
+++ etc/golangci-lint.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 # Keep this in sync with go version used in static-analysis Evergreen build variant.
 set -ex
-# ls $GOPATH/bin
-# ls $GOROOT
 go install golang.org/dl/go1.22.7@latest
-# ls $GOPATH/bin
-env
-ls $GOPATH/bin/linux_386
-# ls $GOROOT
+# Prepend the cross-platform path if necessary.
+if [ -n "$GOOS" ]; then
+    export PATH="$GOPATH/bin/$GOOS_$GOGOARCH:$PATH"
+fi
 go1.22.7 download
 export PATH="$(go1.22.7 env GOROOT)/bin:$PATH"
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.1
