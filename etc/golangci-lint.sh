fdcb2729d97fc0482a28a5a64d10fe538160c1ba
Steven Silvester <steve.silvester@mongodb.com>
2024-09-24 11:56:35 -0500

    debug

--- etc/golangci-lint.sh
+++ etc/golangci-lint.sh
@@ -2,7 +2,7 @@
 # Keep this in sync with go version used in static-analysis Evergreen build variant.
 set -ex
 go install golang.org/dl/go1.22.7@latest
-go1.22.7 download 2> /dev/null
+go1.22.7 download
 export PATH="$(go1.22.7 env GOROOT)/bin:$PATH"
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.1
 golangci-lint run --config .golangci.yml ./...
