90d48aaf622d37a9076d68cc07340bdd045c589b
Steven Silvester <steve.silvester@mongodb.com>
2024-09-24 17:24:26 -0500

    refactor

--- etc/golangci-lint.sh
+++ etc/golangci-lint.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # Keep this in sync with go version used in static-analysis Evergreen build variant.
 set -ex
+GOOS_ORIG=${GOOS:-}
+export GOOS=
+GOARCH_ORIG=${GOARCH:-}
+export GOARCH=
+
 go install golang.org/dl/go1.22.7@latest
-# Prepend the cross-platform path if necessary.
-if [ -n "$GOOS" ]; then
-    export PATH="$GOPATH/bin/${GOOS}_${GOARCH}:$PATH"
-fi
-export GOROOT=
 go1.22.7 download
 export PATH="$(go1.22.7 env GOROOT)/bin:$PATH"
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.1
+
+export GOOS=$GOOS_ORIG
+export GOARCH=$GOARCH_ORIG
 golangci-lint run --config .golangci.yml ./...
