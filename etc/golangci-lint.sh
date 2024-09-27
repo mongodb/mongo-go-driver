d758adc1671255fa27519e0116d2f7ac281fe8d6
Steven Silvester <steven.silvester@ieee.org>
2024-09-27 06:17:28 -0500

    GODRIVER-3357 Make linting more repeatable (#1826)

---
+++ etc/golangci-lint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+# Unset the cross-compiler overrides while downloading binaries.
+GOOS_ORIG=${GOOS:-}
+export GOOS=
+GOARCH_ORIG=${GOARCH:-}
+export GOARCH=
+
+# Keep this in sync with go version used in static-analysis Evergreen build variant.
+go install golang.org/dl/go1.22.7@latest
+go1.22.7 download
+export PATH="$(go1.22.7 env GOROOT)/bin:$PATH"
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.1
+
+export GOOS=$GOOS_ORIG
+export GOARCH=$GOARCH_ORIG
+golangci-lint run --config .golangci.yml ./...
