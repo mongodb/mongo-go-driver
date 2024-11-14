64e6b9ffb2635aec4fad07f35a1e64481428ca36
Preston Vasquez <prestonvasquez@icloud.com>
2024-11-14 15:29:09 -0700

    GODRIVER-1412 Fix license

--- internal/integration/handshake_test.go
+++ internal/integration/handshake_test.go
@@ -1,5 +1,5 @@
-//
 // Copyright (C) MongoDB, Inc. 2023-present.
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -36,9 +36,11 @@ func TestHandshakeProse(t *testing.T) {
 		ClientType(mtest.Proxy)
 
 	clientMetadata := func(env bson.D, info *options.DriverInfo) bson.D {
-		driverName := "mongo-go-driver"
-		driverVersion := version.Driver
-		platform := runtime.Version()
+		var (
+			driverName    = "mongo-go-driver"
+			driverVersion = version.Driver
+			platform      = runtime.Version()
+		)
 
 		if info != nil {
 			driverName = driverName + "|" + info.Name
