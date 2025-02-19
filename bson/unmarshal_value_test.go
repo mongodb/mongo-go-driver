a84f0f865b36d196672c7abdeb43392ef90896da
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2025-02-19 13:29:39 -0800

    GODRIVER-3470 Correct BSON unmarshaling logic for null values (#1924) [master] (#1945)
    Co-authored-by: Preston Vasquez <prestonvasquez@icloud.com>
    (cherry picked from commit 25df82ff2956607022f725f544f10261dd08796d)

--- bson/unmarshal_value_test.go
+++ bson/unmarshal_value_test.go
@@ -13,6 +13,7 @@ import (
 	"testing"
 
 	"go.mongodb.org/mongo-driver/v2/internal/assert"
+	"go.mongodb.org/mongo-driver/v2/internal/require"
 	"go.mongodb.org/mongo-driver/v2/x/bsonx/bsoncore"
 )
 
@@ -37,6 +38,26 @@ func TestUnmarshalValue(t *testing.T) {
 			})
 		}
 	})
+}
+
+func TestInitializedPointerDataWithBSONNull(t *testing.T) {
+	// Set up the test case with initialized pointers.
+	tc := unmarshalBehaviorTestCase{
+		BSONValuePtrTracker: &unmarshalBSONValueCallTracker{},
+		BSONPtrTracker:      &unmarshalBSONCallTracker{},
+	}
+	// Create BSON data where the '*_ptr_tracker' fields are explicitly set to
+	// null.
+	bytes := docToBytes(D{
+		{Key: "bv_ptr_tracker", Value: nil},
+		{Key: "b_ptr_tracker", Value: nil},
+	})
+	// Unmarshal the BSON data into the test case struct. This should set the
+	// pointer fields to nil due to the BSON null value.
+	err := Unmarshal(bytes, &tc)
+	require.NoError(t, err)
+	assert.Nil(t, tc.BSONValuePtrTracker)
+	assert.Nil(t, tc.BSONPtrTracker)
 }
 
 // tests covering GODRIVER-2779
