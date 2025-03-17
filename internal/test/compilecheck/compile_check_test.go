3913232bd3a670e1da18241339f6219860b3c7ec
Preston Vasquez <prestonvasquez@icloud.com>
2025-03-17 16:37:22 -0600

    Add test/compilecheck

---
+++ internal/test/compilecheck/compile_check_test.go
@@ -0,0 +1,27 @@
+package compilecheck
+
+import (
+	"testing"
+)
+
+func TestCompileCheck(t *testing.T) {
+	//ctx := context.Background()
+
+	//req := testcontainer.ContainerRequest{
+	//	Image:      "alpine:latest",
+	//	Cmd:        []string{"echo", "hello world"},
+	//	WaitingFor: wait.ForLog("hello world"),
+	//}
+
+	//container, err := testcontianer.GenericContainer(ctx, testcontainer.GenericContainerRequest{
+	//	ContainerRequest: req,
+	//	Started:          true,
+	//})
+
+	//require.NoError(t, err)
+
+	//defer func() {
+	//	err := container.Terminate(ctx)
+	//	require.NoError(t, err)
+	//}()
+}
---
+++ internal/test/compilecheck/go.mod
@@ -0,0 +1,5 @@
+module go.mongodb.go/mongo-driver/internal/test/compilecheck
+
+go 1.22
+
+//replace go.mongodb.org/mongo-driver => ../../../
