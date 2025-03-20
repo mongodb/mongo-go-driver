280383026940e600494f451d62e026591047d70e
Preston Vasquez <prestonvasquez@icloud.com>
2025-03-20 14:47:17 -0600

    GODRIVER-3493 Clean up script

--- etc/compile_check.sh
+++ etc/compile_check.sh
@@ -2,42 +2,41 @@
 set -e # exit when any command fails
 set -x # show all commands being run
 
-# Default to Go 1.18 if GO_VERSION is not set.
-#
-# Use the "=" operator (instead of the more common ":-" operator) so that it
-# allows setting GO_VERSION="" to use the Go installation in the PATH, and it
-# sets the GO_VERSION variable if the default is used.
 GC=go
 COMPILE_CHECK_DIR="internal/cmd/compilecheck"
+ARCHITECTURES=("386" "arm" "arm64" "ppc64le" "s390x")
+BUILD_CMD="${GC} build -buildvcs=false"
+
+# Compilation modules are not part of the workspace as testcontainers requires
+# a version of klauspost/compress not supported by the Go Driver / other modules
+# in the workspace.
+export GOWORK=off
 
 # compile_check will attempt to build the internal/test/compilecheck project
 # using the provided Go version. This is to simulate an end-to-end use case.
 function compile_check {
   # Change the directory to the compilecheck test directory.
-  pushd ${COMPILE_CHECK_DIR}
+  pushd "${COMPILE_CHECK_DIR}" >/dev/null
 
   ${GC} version
   ${GC} mod tidy
 
-  # Check simple build.
-  GOWORK=off ${GC} build -buildvcs=false ./...
+  # Standard build
+  $BUILD_CMD ./...
 
-  # Check build with dynamic linking.
-  GOWORK=off ${GC} build -buildvcs=false -buildmode=plugin
+  # Dynamic linking
+  $BUILD_CMD -buildmode=plugin
 
   # Check build with tags.
-  GOWORK=off ${GC} build -buildvcs=false $BUILD_TAGS ./...
+  [[ -n "$BUILD_TAGS" ]] && $BUILD_CMD $BUILD_TAGS ./...
 
   # Check build with various architectures.
-  GOWORK=off GOOS=linux GOARCH=386 ${GC} build -buildvcs=false ./...
-  GOWORK=off GOOS=linux GOARCH=arm ${GC} build -buildvcs=false ./...
-  GOWORK=off GOOS=linux GOARCH=arm64 ${GC} build -buildvcs=false ./...
-  GOWORK=off GOOS=linux GOARCH=amd64 ${GC} build -buildvcs=false ./...
-  GOWORK=off GOOS=linux GOARCH=ppc64le ${GC} build -buildvcs=false ./...
-  GOWORK=off GOOS=linux GOARCH=s390x ${GC} build -buildvcs=false ./...
+  for ARCH in "${ARCHITECTURES[@]}"; do
+    GOOS=linux GOARCH=$ARCH $BUILD_CMD ./...
+  done
 
   # Change the directory back to the working directory.
-  popd
+  popd >/dev/null
 }
 
 compile_check
