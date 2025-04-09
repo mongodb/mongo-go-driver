0107d58b2f1fc6fbee080694a78260c193d60d4e
Preston Vasquez <prestonvasquez@icloud.com>
2025-04-09 09:18:25 -0600

    Skip disambiguated path errors (#2013)

--- internal/spectest/skip.go
+++ internal/spectest/skip.go
@@ -779,6 +779,7 @@ var skipTests = map[string][]string{
 	"Change streams expanded events for MongoDB 8.2+ (GODRIVER-3524)": {
 		"TestUnifiedSpec/change-streams/tests/unified/change-streams-disambiguatedPaths.json/disambiguatedPaths_is_not_present_when_showExpandedEvents_is_false/unset",
 		"TestUnifiedSpec/change-streams/tests/unified/change-streams.json/Test_insert,_update,_replace,_and_delete_event_types",
+		"TestUnifiedSpec/change-streams/tests/unified/change-streams.json/Test_array_truncation",
 	},
 
 	// TODO(DRIVERS-3153): Re-enable once resolved.
