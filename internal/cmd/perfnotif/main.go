24d243d303970c1d0ffae873560151ec59d11986
Selena Zhou <selena.zhou@mongodb.com>
2025-07-03 10:50:38 -0400

    test filtering documents

--- internal/cmd/perfnotif/main.go
+++ internal/cmd/perfnotif/main.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
@@ -30,16 +31,54 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
 	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Panicf("Error pinging MongoDB Analytics: %v", err)
 	}
-
 	fmt.Println("Successfully connected to MongoDB Analytics node.")
+
+	coll := client.Database("expanded_metrics").Collection("change_points")
+	var cursor *mongo.Cursor
+	cursor, err = getDocsWithContext(coll)
+	if err != nil {
+		log.Panicf("Error retrieving documents from collection.")
+	}
+	fmt.Printf("Successfully retrieved %d documents.", cursor.RemainingBatchLength())
 
 	err = client.Disconnect(context.Background())
 	if err != nil {
 		log.Panicf("Failed to disconnect client: %v", err)
 	}
+
+}
+
+func getDocsWithContext(coll *mongo.Collection) (*mongo.Cursor, error) {
+	filter := bson.D{
+		{"time_series_info.project", "mongo-go-driver"},
+		{"time_series_info.variant", "perf"},
+		{"time_series_info.task", "perf"},
+		// {"commit", commitName},
+		{"triage_contexts", bson.M{"$in": []string{"GoDriver perf (h-score)"}}},
+	}
+
+	projection := bson.D{
+		{"time_series_info.project", 1},
+		{"time_series_info.task", 1},
+		{"time_series_info.test", 1},
+		{"triage_contexts", 1},
+		{"h_score", 1},
+	}
+
+	findOptions := options.Find().SetProjection(projection)
+
+	findCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	cursor, err := coll.Find(findCtx, filter, findOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(findCtx)
+
+	return cursor, err
 }
