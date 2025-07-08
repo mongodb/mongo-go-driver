4a9511e7b70848964072ca73a15e162c487c6432
Selena Zhou <selena.zhou@mongodb.com>
2025-07-08 09:48:29 -0400

    decode docs and display in evg logs, dummy commit for now

--- internal/cmd/perfnotif/main.go
+++ internal/cmd/perfnotif/main.go
@@ -18,6 +18,17 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
+type ChangePoint struct {
+	TimeSeriesInfo struct {
+		Project     string `bson:"project"`
+		Task        string `bson:"task"`
+		Test        string `bson:"test"`
+		Measurement string `bson:"measurement"`
+	} `bson:"time_series_info"`
+	TriageContexts []string `bson:"triage_contexts"`
+	HScore         float64  `bson:"h_score"`
+}
+
 func main() {
 	uri := os.Getenv("perf_uri_private_endpoint")
 	if uri == "" {
@@ -37,13 +48,33 @@ func main() {
 	}
 	fmt.Println("Successfully connected to MongoDB Analytics node.")
 
-	coll := client.Database("expanded_metrics").Collection("change_points")
-	var cursor *mongo.Cursor
-	cursor, err = getDocsWithContext(coll)
-	if err != nil {
-		log.Panicf("Error retrieving documents from collection.")
+	commit := os.Getenv("COMMIT") // TODO: get PR from evergreen instead of just the commit, and use PR to get latest commit
+	if commit == "" {
+		log.Panic("could not retrieve commit number")
 	}
-	fmt.Printf("Successfully retrieved %d documents.", cursor.RemainingBatchLength())
+
+	coll := client.Database("expanded_metrics").Collection("change_points")
+	var changePoints []ChangePoint
+	changePoints, err = getDocsWithContext(coll, "50cf0c20d228975074c0010bfb688917e25934a4") // TODO: restore test commit
+	if err != nil {
+		log.Panicf("Error retrieving and decoding documents from collection: %v.", err)
+	}
+
+	if len(changePoints) == 0 {
+		log.Panicf("Nothing was decoded")
+	}
+
+	fmt.Print("Documents:")
+	for _, cp := range changePoints {
+		fmt.Printf("  Project: %s, Task: %s, Test: %s, Measurement: %s, Triage Contexts: %v, H-Score: %f\n",
+			cp.TimeSeriesInfo.Project,
+			cp.TimeSeriesInfo.Task,
+			cp.TimeSeriesInfo.Test,
+			cp.TimeSeriesInfo.Measurement,
+			cp.TriageContexts,
+			cp.HScore,
+		)
+	}
 
 	err = client.Disconnect(context.Background())
 	if err != nil {
@@ -52,12 +83,12 @@ func main() {
 
 }
 
-func getDocsWithContext(coll *mongo.Collection) (*mongo.Cursor, error) {
+func getDocsWithContext(coll *mongo.Collection, commit string) ([]ChangePoint, error) {
 	filter := bson.D{
 		{"time_series_info.project", "mongo-go-driver"},
 		{"time_series_info.variant", "perf"},
 		{"time_series_info.task", "perf"},
-		// {"commit", commitName},
+		{"commit", commit},
 		{"triage_contexts", bson.M{"$in": []string{"GoDriver perf (h-score)"}}},
 	}
 
@@ -65,6 +96,7 @@ func getDocsWithContext(coll *mongo.Collection) (*mongo.Cursor, error) {
 		{"time_series_info.project", 1},
 		{"time_series_info.task", 1},
 		{"time_series_info.test", 1},
+		{"time_series_info.measurement", 1},
 		{"triage_contexts", 1},
 		{"h_score", 1},
 	}
@@ -80,5 +112,20 @@ func getDocsWithContext(coll *mongo.Collection) (*mongo.Cursor, error) {
 	}
 	defer cursor.Close(findCtx)
 
-	return cursor, err
+	fmt.Printf("Successfully retrieved %d documents from commit %s.", cursor.RemainingBatchLength(), commit)
+
+	var changePoints []ChangePoint
+	for cursor.Next(findCtx) {
+		var cp ChangePoint
+		if err := cursor.Decode(&cp); err != nil {
+			return nil, err
+		}
+		changePoints = append(changePoints, cp)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return changePoints, nil
 }
