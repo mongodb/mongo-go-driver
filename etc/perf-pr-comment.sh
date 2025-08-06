4ee9c1276163644d355fcf410aa3c9862a80ee62
Selena Zhou <selena.zhou@mongodb.com>
2025-08-06 16:44:27 -0400

    GODRIVER-3102: Perf comp PR comment pipeline (#2149)

--- etc/perf-pr-comment.sh
+++ etc/perf-pr-comment.sh
@@ -5,5 +5,22 @@
 set -eux
 
 pushd ./internal/cmd/perfcomp >/dev/null || exist
-GOWORK=off go run main.go --project="mongo-go-driver" ${VERSION_ID}
+GOWORK=off go build -o ../../../bin/perfcomp .
 popd >/dev/null
+
+# Generate perf report.
+GOWORK=off ./bin/perfcomp compare --project="mongo-go-driver" ${VERSION_ID} > ./internal/cmd/perfcomp/perf-report.txt
+
+if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
+    # Parse and generate perf comparison comment.
+    GOWORK=off ./bin/perfcomp mdreport
+    # Make the PR comment.
+    target=$DRIVERS_TOOLS/.evergreen/github_app/create_or_modify_comment.sh
+    bash $target -m "## 🧪 Performance Results" -c "$(pwd)/perf-report.md" -h $HEAD_SHA -o "mongodb" -n "mongo-go-driver"
+    rm ./perf-report.md
+else
+    # Skip comment if it isn't a PR run.
+    echo "Skipping Perf PR comment"
+fi
+
+rm ./internal/cmd/perfcomp/perf-report.txt
