3b620783d5385597a0fdba9a0e3de4a1dc076dee
Selena Zhou <selena.zhou@mongodb.com>
2025-08-08 10:11:13 -0400

    Reroute shell script to run DET perfcomp

--- etc/perf-pr-comment.sh
+++ etc/perf-pr-comment.sh
@@ -4,23 +4,22 @@
 
 set -eux
 
-pushd ./internal/cmd/perfcomp >/dev/null || exist
-GOWORK=off go build -o ../../../bin/perfcomp .
-popd >/dev/null
+pushd $DRIVERS_TOOLS/.evergreen >/dev/null
+sh run-perf-comp.sh
 
-# Generate perf report.
-GOWORK=off ./bin/perfcomp compare --project="mongo-go-driver" ${VERSION_ID} > ./internal/cmd/perfcomp/perf-report.txt
+./perfcomp/bin/perfcomp compare --project="mongo-go-driver" ${VERSION_ID}
 
-if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
+    if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
     # Parse and generate perf comparison comment.
-    GOWORK=off ./bin/perfcomp mdreport
+    GOWORK=off ./perfcomp/bin/perfcomp mdreport
     # Make the PR comment.
-    target=$DRIVERS_TOOLS/.evergreen/github_app/create_or_modify_comment.sh
+    target=github_app/create_or_modify_comment.sh
     bash $target -m "## 🧪 Performance Results" -c "$(pwd)/perf-report.md" -h $HEAD_SHA -o "mongodb" -n "mongo-go-driver"
+    rm ./perf-report.txt
     rm ./perf-report.md
 else
     # Skip comment if it isn't a PR run.
     echo "Skipping Perf PR comment"
 fi
 
-rm ./internal/cmd/perfcomp/perf-report.txt
+popd >/dev/null
