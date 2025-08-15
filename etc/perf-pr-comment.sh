7b015ad9b6f32d1035d64bfc06146ecc8a6ab15d
Selena Zhou <selena.zhou@mongodb.com>
2025-08-15 15:37:53 -0400

    Update path

--- etc/perf-pr-comment.sh
+++ etc/perf-pr-comment.sh
@@ -10,7 +10,7 @@ PROJECT="mongo-go-driver" CONTEXT="GoDriver perf task" TASK="perf" VARIANT="perf
 if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
     # Make the PR comment.
     target=github_app/create_or_modify_comment.sh
-    bash $target -m "## 🧪 Performance Results" -c "$(pwd)/perf-report.md" -h $HEAD_SHA -o "mongodb" -n "mongo-go-driver"
+    bash $target -m "## 🧪 Performance Results" -c "$(pwd)/perfcomp/perf-report.md" -h $HEAD_SHA -o "mongodb" -n "mongo-go-driver"
     rm ./perfcomp/perf-report.md
 else
     # Skip comment if it isn't a PR run.
