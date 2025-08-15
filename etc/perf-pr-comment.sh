16eabd315c5bbce03942554b6a9a44b025c8ecb2
Selena Zhou <selena.zhou@mongodb.com>
2025-08-15 11:48:14 -0400

    Use DET shell script to run perfcomp

--- etc/perf-pr-comment.sh
+++ etc/perf-pr-comment.sh
@@ -5,13 +5,9 @@
 set -eux
 
 pushd $DRIVERS_TOOLS/.evergreen >/dev/null
-sh run-perf-comp.sh
+PROJECT="mongo-go-driver" CONTEXT="GoDriver perf task" TASK="perf" VARIANT="perf" sh run-perf-comp.sh
 
-./perfcomp/bin/perfcomp compare --project="mongo-go-driver" ${VERSION_ID}
-
-    if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
-    # Parse and generate perf comparison comment.
-    GOWORK=off ./perfcomp/bin/perfcomp mdreport
+if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
     # Make the PR comment.
     target=github_app/create_or_modify_comment.sh
     bash $target -m "## 🧪 Performance Results" -c "$(pwd)/perf-report.md" -h $HEAD_SHA -o "mongodb" -n "mongo-go-driver"
