de44d3e9f5bb13cdbdb909bbe7b3a5dd7aa66052
Qingyang Hu <103950869+qingyang-hu@users.noreply.github.com>
2026-03-19 11:31:29 -0400

    GODRIVER-3835 Update govulncheck script to use latest version. (#2342)

--- etc/govulncheck.sh
+++ etc/govulncheck.sh
@@ -7,12 +7,16 @@ set -ex
 # Note: this needs to be updated if the listed Go version has vulnerabilities
 # discovered because they will show up in the scan results along with Go Driver
 # and dependency vulnerabilities.
-GO_VERSION=1.25.7
+VERSION_REGEX='1\\.25\\.[0-9]+'
+JQ_FILTER="first(.[] \
+| select(.stable == true and (.version | test(\"^go${VERSION_REGEX}$\"))) \
+| .version)"
+GO_VERSION=$(curl -sSfL 'https://golang.org/dl/?mode=json' | jq -r "$JQ_FILTER")
 
-go install golang.org/dl/go$GO_VERSION@latest
-go${GO_VERSION} download
+go install golang.org/dl/$GO_VERSION@latest
+${GO_VERSION} download
 go install golang.org/x/vuln/cmd/govulncheck@latest
 
 # govulncheck uses the Go binary it finds from the PATH, so modify PATH to point
 # to the Go version we just downloaded.
-PATH="$(go${GO_VERSION} env GOROOT)/bin:$PATH" govulncheck -show verbose ./...
+PATH="$(${GO_VERSION} env GOROOT)/bin:$PATH" govulncheck -show verbose ./...
