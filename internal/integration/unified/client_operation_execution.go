0d5a0842e4bd42656d5175e03dcb9ee99e4a8299
Matt Dale <9760375+matthewdale@users.noreply.github.com>
2026-06-01 17:42:34 -0700

    GODRIVER-3874 Send afterClusterTime on writes in causally-consistent sessions

--- internal/integration/unified/client_operation_execution.go
+++ internal/integration/unified/client_operation_execution.go
@@ -168,6 +168,33 @@ func executeListDatabases(ctx context.Context, operation *operation, nameOnly bo
 	return newDocumentResult(raw, nil), nil
 }
 
+func executeDropDatabase(ctx context.Context, operation *operation) (*operationResult, error) {
+	client, err := entities(ctx).client(operation.Object)
+	if err != nil {
+		return nil, err
+	}
+
+	var dbName string
+	elems, _ := operation.Arguments.Elements()
+	for _, elem := range elems {
+		key := elem.Key()
+		val := elem.Value()
+
+		switch key {
+		case "database":
+			dbName = val.StringValue()
+		default:
+			return nil, fmt.Errorf("unrecognized dropDatabase option %q", key)
+		}
+	}
+	if dbName == "" {
+		return nil, newMissingArgumentError("database")
+	}
+
+	err = client.Database(dbName).Drop(ctx)
+	return newErrorResult(err), nil
+}
+
 func executeClientBulkWrite(ctx context.Context, operation *operation) (*operationResult, error) {
 	client, err := entities(ctx).client(operation.Object)
 	if err != nil {
